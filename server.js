4f43ed01c4f336b7b749942db33834dcb05468a1
generalhenry <htilford@gmail.com>
2013-06-07 15:37:22 -0700

    auto fork

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ if (cluster.isMaster) {
   }
   cluster.on('exit', function (worker, code, signal) {
     console.log('worker ' + worker.process.pid + ' died');
+    cluster.fork();
   });
 } else  {
   api_server.start();
