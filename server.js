8a8bfb2146e3aede3688fd479e658666bca31fdf
Enos Feedler <jeff.kingyens@gmail.com>
2013-06-07 14:07:27 -0700

    add cluster

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 var api_server = require('./lib');
-api_server.start();
+var cluster = require('cluster');
+var os = require('os');
 
+var numCPUs = os.cpus().length;
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', function (worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else  {
+  api_server.start();
+}
