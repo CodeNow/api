3777bea8924871eb1a11f61834e56f682924228d
prafulrana <prafulrana@gmail.com>
2013-07-08 17:38:02 -0700

    Node hello world app fix. Listen to only port 80

--- configs/runnables/node.js/src/server.js
+++ configs/runnables/node.js/src/server.js
@@ -2,4 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen();
+}).listen(80);
+
+console.log("Hello Kitty");
