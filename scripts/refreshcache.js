dda94c210f19c20f09f634ee08e4377fd627bd22
Enos Feedler <jeff.kingyens@gmail.com>
2013-11-11 17:25:11 -0800

    refresh published images sorted by vote cache every day at 12pm PST

--- scripts/refreshcache.js
+++ scripts/refreshcache.js
@@ -2,19 +2,23 @@ var configs = require('../lib/configs');
 var request = require('request');
 
 var refresh = function() {
-  request({
-    url: 'http://api.' + configs.domain + '/cache',
-    method: 'GET',
-    headers: {
-      'runnable-token': configs.adminToken
-    }
-  }, function (err, res) {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(res.statusCode + ': ' + res.body);
-    }
-  });
+  var now = new Date();
+  var hours = now.getUTCHours();
+  if (hours === 20) { // NOON PST
+    request({
+      url: 'http://api.' + configs.domain + '/cache',
+      method: 'GET',
+      headers: {
+        'runnable-token': configs.adminToken
+      }
+    }, function (err, res) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(res.statusCode + ': ' + res.body);
+      }
+    });
+  }
 }
 
 refresh();
