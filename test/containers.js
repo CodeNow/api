c11356156c42bbf0503aba04b3a9db91e1beb49a
generalhenry <htilford@gmail.com>
2014-01-07 13:02:19 -0800

    design

--- test/containers.js
+++ test/containers.js
@@ -14,20 +14,53 @@ describe('Containers', function () {
   after(helpers.cleanup);
 
   describe('GET /users/me/runnables', function () {
-    beforeEach(extendContextSeries({
-      user: users.createAnonymous,
-      container: ['user.createContainerFromFixture', 'node.js']
-    }));
+    
     afterEach(helpers.cleanupExcept('images'));
-    it('should query by image', function (done) {
-      this.user.specRequest({ parent: this.image._id })
-        .expect(200)
-        .expectArray(1)
-        .end(done);
+
+    describe('owner', function () {
+      beforeEach(extendContextSeries({
+        user: users.createAnonymous,
+        container: ['user.createContainerFromFixture', 'node.js']
+      }));
+      it('should query by image', function (done) {
+        this.user.specRequest({ parent: this.image._id })
+          .expect(200)
+          .expectArray(1)
+          .end(done);
+      });
     });
+    describe('not owner', function () {
+      beforeEach(extendContext({
+        user: users.createAnonymous,
+        container: containers.createContainerFromFixture('node.js')
+      }));
+      it('should not query by image', function (done) {
+        this.user.specRequest({ parent: this.image._id })
+          .expect(403)
+          .end(done);
+      });
+    });
+    describe('admin', function () {
+      beforeEach(extendContext({
+        user: users.createAdmin,
+        container: containers.createContainerFromFixture('node.js')
+      }));
+      it('should query by image', function (done) {
+        this.user.specRequest({ parent: this.image._id })
+          .expect(200)
+          .expectArray(1)
+          .end(done);
+      });
+    });
+    
   });
 
-  describe('GET /users/me/runnables/:id');
+  describe('GET /users/me/runnables/:id', function () {
+    afterEach(helpers.cleanupExcept('images'));
+    describe('owner');
+    describe('not owner');
+    describe('admin');
+  });
 
   describe('POST /users/me/runnables', function () {
     beforeEach(extendContext({
@@ -45,16 +78,35 @@ describe('Containers', function () {
     });
   });
 
-  describe('PUT /users/me/runnables/:id');
+  describe('PUT /users/me/runnables/:id', function () {
 
-  describe('PATCH /users/me/runnables/:id');
+    afterEach(helpers.cleanupExcept('images'));
+
+    describe('owner');
+    describe('not owner');
+    describe('admin');
+  });
+
+  describe('PATCH /users/me/runnables/:id', function () {
+
+    afterEach(helpers.cleanupExcept('images'));
+
+    describe('owner');
+    describe('not owner');
+    describe('admin');
+  });
 
   describe('DEL /users/me/runnables/:id', function () {
+
+    afterEach(helpers.cleanupExcept('images'));
+    
+    describe('owner');
+    describe('not owner');
+    describe('admin');
     beforeEach(extendContextSeries({
       user: users.createAnonymous,
       container: ['user.createContainerFromFixture', 'node.js']
     }));
-    afterEach(helpers.cleanupExcept('images'));
     it('should query by image', function (done) {
       this.user.specRequest(this.container._id)
         .expect(200)
