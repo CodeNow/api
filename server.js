ceecc22f3b26c053ffc7e8800effcdb2741a525b
generalhenry <htilford@gmail.com>
2014-01-08 16:29:35 -0800

    pruning

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var configs = require('./lib/configs');
-var cluster = require('cluster');
 
 if (configs.nodetime && cluster.isWorker) {
   var nodetime = require('nodetime');
@@ -8,40 +7,8 @@ if (configs.nodetime && cluster.isWorker) {
 
 var debug = require('debug')('master');
 
-require('source-map-support').install();
 var api_server = require('./lib');
 var os = require('os');
 
-if (cluster.isMaster) {
-
-  var create_worker = function () {
-    var worker = cluster.fork();
-    worker.on('message', function (msg) {
-      if (msg === 'exception') {
-        debug('spawning new worker to replace existing');
-        create_worker();
-      }
-    });
-  };
-
-  var numCPUs = os.cpus().length;
-  var numWorkers = numCPUs * 5;
-  debug('spawning initial ' + numWorkers + ' workers');
-  for (var i = 0; i < numWorkers; i++) {
-    create_worker();
-  }
-
-  cluster.on('fork', function (worker) {
-    debug('worker forked:', worker.process.pid);
-  });
-
-  cluster.on('online', function (worker) {
-    debug('worker online:', worker.process.pid);
-  });
-
-} else  {
-
-  var worker = new api_server(configs, null);
-  worker.start(function () { });
-
-}
+var worker = new api_server(configs, null);
+worker.start(function () { });
