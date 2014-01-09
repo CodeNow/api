f2c19e629d953a1bf65283480103f73246fc0a72
generalhenry <htilford@gmail.com>
2014-01-08 18:01:57 -0800

    linting

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 var configs = require('./lib/configs');
 
-if (configs.nodetime && cluster.isWorker) {
+if (configs.nodetime) {
   var nodetime = require('nodetime');
   nodetime.profile(configs.nodetime);
 }
 
-var debug = require('debug')('master');
-
 var api_server = require('./lib');
-var os = require('os');
 
 var worker = new api_server(configs, null);
 worker.start(function () { });
