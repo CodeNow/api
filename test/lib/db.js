9fee7747637b0476c6803a6cc9c62248e6ee65bc
tjmehta <tejesh.mehta@gmail.com>
2014-01-20 13:15:05 -0800

    drops -> removes so that indexes are not dropped btw tests

--- test/lib/db.js
+++ test/lib/db.js
@@ -11,13 +11,13 @@ var db = module.exports = {
       mongoose.connection.once('connected', callback);
     }
   },
-  dropCollection: function (collectionName) {
+  removeCollection: function (collectionName) {
     var collection = mongoose.connection.collections[collectionName];
     return function (callback) {
       if (!collection) {
         return callback();
       }
-      collection.drop(function(err) {
+      collection.remove(function(err) {
         if (err && err.message !== 'ns not found') {
           return callback(err);
         }
@@ -25,7 +25,7 @@ var db = module.exports = {
       });
     };
   },
-  dropCollectionsExcept: function (exclude) {
+  removeCollectionsExcept: function (exclude) {
     exclude = Array.isArray(exclude) ?
       exclude :
       Array.prototype.slice.call(arguments);
@@ -35,17 +35,23 @@ var db = module.exports = {
       names = _.difference(names, exclude);
       // var users = require('./userFactory');
       async.forEach(names, function(name, done) {
-        db.dropCollection(name)(done);
+        db.removeCollection(name)(done);
       }, callback);
     };
   },
-  dropCollections: function(callback) {
-    db.dropCollectionsExcept([])(callback);
+  removeCollections: function(callback) {
+    db.removeCollectionsExcept([])(callback);
   },
   dropDatabase: function (callback) {
     callback = callback || function () {};
     console.log('  drop db');
     mongoose.connection.db.dropDatabase(callback);
+  },
+  removeCollectionDocuments: function (callback) {
+    var names =  Object.keys(mongoose.connection.collections);
+    async.forEach(names, function (name, cb) {
+      mongoose.connection.collections[name].remove({}, cb);
+    }, callback);
   }
 };
 
