66d4a4d030008f083873c5d8f4fc54c2ab7cd759
generalhenry <htilford@gmail.com>
2014-01-22 15:02:04 -0800

    node path

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var configs = require('./lib/configs');
+var configs = require('configs');
 
 if (configs.nodetime) {
   var nodetime = require('nodetime');
   nodetime.profile(configs.nodetime);
 }
 
-var api_server = require('./lib');
+var api_server = require('index');
 
 var worker = new api_server();
 worker.start(function noop () {});
