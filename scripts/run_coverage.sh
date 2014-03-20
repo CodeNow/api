d5e31ab2b6a78cb50d9653dd7a17344047648d9b
Bryan Kendall <bryan@runnable.com>
2014-03-20 10:50:09 -0700

    Increasing timeouts for testing
    circleci be slow

--- scripts/run_coverage.sh
+++ scripts/run_coverage.sh
@@ -3,7 +3,7 @@ jscoverage lib lib-cov
 rm -rf lib
 mv lib-cov lib
 mkdir -p ./coverage
-NODE_ENV=testing NODE_PATH=./lib mocha --reporter html-cov > coverage/index.html 2> coverage.log
+NODE_ENV=testing NODE_PATH=./lib mocha -t 5000 --reporter html-cov > coverage/index.html 2> coverage.log
 rm -rf lib
 git checkout -- lib
 if [ "$CIRCLE_ARTIFACTS" != "" ]; then 
