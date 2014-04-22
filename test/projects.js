728a5958b3c7836b1dfd869a1380bb0ebd8e8ac8
Bryan Kendall <bryan@runnable.com>
2014-04-22 15:57:31 -0700

    continuing to add more routes
    - can get details of images
    - marking out some of the other paths
    - middleware-ing some parts as we go

--- test/projects.js
+++ test/projects.js
@@ -4,11 +4,25 @@ var users = require('./lib/userFactory');
 
 var extendContextSeries = helpers.extendContextSeries;
 
+var validProjectData = {
+  name: 'new project',
+  contexts: [{
+    'name': 'web server',
+    'dockerfile': 'FROM ubuntu\n' +
+      'WORKDIR /root\n' +
+      'RUN git clone https://github.com/heroku/node-js-sample\n' +
+      'WORKDIR /root/node-js-sample\n' +
+      'RUN npm install\n' +
+      'ENTRYPOINT ["node"]\n' +
+      'CMD ["web.js"]\n'
+  }]
+};
+
 describe('Projects', function () {
-  before(extendContextSeries({
+  beforeEach(extendContextSeries({
     admin: users.createAdmin
   }));
-  after(helpers.cleanup);
+  afterEach(helpers.cleanup);
 
   describe('creating projects', function () {
     it('should error when missing project parameters', function (done) {
@@ -29,24 +43,33 @@ describe('Projects', function () {
         .end(done);
     });
     it('should create a new project given no files', function (done) {
-      this.admin.post('/projects', {
-        name: 'new project',
-        contexts: [{
-          'name': 'web server',
-          'dockerfile': 'FROM ubuntu\n' +
-            'WORKDIR /root\n' +
-            'RUN git clone https://github.com/heroku/node-js-sample\n' +
-            'WORKDIR /root/node-js-sample\n' +
-            'RUN npm install\n' +
-            'ENTRYPOINT ["node"]\n' +
-            'CMD ["web.js"]\n'
-        }]
-      })
-        .expect(201)
-        .end(function (err, res) {
-          console.log(res.body);
-          done(err);
+      this.admin.post('/projects', validProjectData)
+        .expect(200)
+        .expectBody('name', 'new project')
+        .expectBody(function (body) {
+          body.contexts.length.should.equal(1);
+        })
+        .end(done);
+    });
+  });
+
+  describe('listing projects', function () {
+    before(extendContextSeries({
+      project: function (done) {
+        users.createAdmin(function (err, user) {
+          user.post('/projects', validProjectData).expect(200).end(done);
         });
+      }
+    }));
+
+    it('should give us details about a project', function (done) {
+      this.admin.get('/projects/' + this.project.body._id)
+        .expect(200)
+        .expectBody('name', 'new project')
+        .expectBody(function (body) {
+          body.contexts.length.should.equal(1);
+        })
+        .end(done);
     });
   });
 });
