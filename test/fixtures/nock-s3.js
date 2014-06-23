f6a82a214c9fc86cbe7b5cba8121ea07e3c0dc9d
Bryan Kendall <bryan@runnable.com>
2014-06-23 15:49:21 -0700

    twice built, plus more debugging

--- test/fixtures/nock-s3.js
+++ test/fixtures/nock-s3.js
@@ -12,6 +12,7 @@ module.exports = function () {
     .filteringPath(/\/containers\/[0-9a-f]+\/attach\?.+/,
       '/containers/284912fa2cf26d40cc262798ecbb483b58f222d42ab1551e818afe35744688f7/attach')
     .post('/containers/284912fa2cf26d40cc262798ecbb483b58f222d42ab1551e818afe35744688f7/attach')
+    .twice()
     .reply(200, 'Successfully built 15e17eedec196751ad15cdb1cef61f6022c19bee01b8079');
 
   /* PUTS */
