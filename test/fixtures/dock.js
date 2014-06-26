83c0abd75f364027d4ae235ee682eb418a897acf
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-06-25 17:11:01 -0700

    add simple rotating dock selection algo

--- test/fixtures/dock.js
+++ test/fixtures/dock.js
@@ -1,6 +1,7 @@
 var createCount = require('callback-count');
 var docker = require('./docker');
 var docklet = require('./docklet');
+var redis = require('models/redis');
 
 module.exports = {
   start: startDock,
@@ -12,11 +13,13 @@ function startDock (done) {
   var count = createCount(done);
   ctx.docker = docker.start(count.inc().next);
   ctx.docklet = docklet.start(count.inc().next);
+  redis.lpush("docks:active", "localhost", count.inc().next);
 }
 function stopDock (done) {
   var count = createCount(done);
   ctx.docker = docker.stop(count.inc().next);
   ctx.docklet = docklet.stop(count.inc().next);
+  redis.del("docks:active", count.inc().next);
   delete ctx.docker;
   delete ctx.docklet;
 }
