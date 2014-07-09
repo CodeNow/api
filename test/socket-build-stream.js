8ca6b3ec4c8cf9cd3ee200e4810c5a1a8d0b6d44
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-07-09 16:30:46 -0700

    use file stream for more robust build-stream test

--- test/socket-build-stream.js
+++ test/socket-build-stream.js
@@ -4,6 +4,8 @@ var it = Lab.test;
 var expect = Lab.expect;
 var before = Lab.before;
 var after = Lab.after;
+var testFile = './test.txt';
+var fs = require('fs');
 
 var createCount = require('callback-count');
 var configs = require('configs');
@@ -20,18 +22,23 @@ var primusClient = Primus.createSocket({
   parser: 'JSON'
 });
 
+
 describe('Build Stream', function () {
   var ctx = {};
 
   before(api.start.bind(ctx));
   after(api.stop.bind(ctx));
-
+  after(function(done) {
+    fs.unlink(testFile, done);
+  });
   // TODO: test multi client once API start is fixed
   it('should send data to clients', function (done) {
     var roomId = uuid();
     var testString = "this is yet another message";
     var numClients = 1;
-    var clientOpenCount = createCount(numClients, sendData);
+    var clientOpenCount = createCount(numClients, function() {
+      sendData(testString, roomId);
+    });
     var clientReadCount = createCount(numClients, done);
 
     var client = new primusClient('http://'+configs.ipaddress+':'+configs.port+"?type=build-stream&id="+roomId);
@@ -46,11 +53,9 @@ describe('Build Stream', function () {
       });
     });
 
-    function sendData () {
-      var sendStream = new require('stream').Readable();
-      sendStream.push(testString);
-      sendStream.push(null);
-      buildStream.sendBuildStream(roomId, sendStream);
+    function sendData (testString, roomId) {
+      fs.writeFileSync(testFile, testString);
+      buildStream.sendBuildStream(roomId, fs.createReadStream(testFile));
     }
   });
 });
