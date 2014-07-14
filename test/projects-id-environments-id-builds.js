e77d9aae4bf99dd0972dcb57a4ced0994998226b
Nathan219 <Nathan219@gmail.com>
2014-07-14 13:02:00 -0700

    Adding some changes.  Working on Tests

--- test/projects-id-environments-id-builds.js
+++ test/projects-id-environments-id-builds.js
@@ -24,18 +24,46 @@ describe('Builds - /projects/:id/environments/:id/builds', function () {
   afterEach(require('./fixtures/clean-mongo').removeEverything);
   afterEach(require('./fixtures/clean-ctx')(ctx));
 
-  // describe('POST', function () {
-  //   beforeEach(function (done) {
-  //     nockS3();
-  //     multi.createRegisteredUserAndProject(function (err, user, project) {
-  //       if (err) { return done(err); }
-  //       ctx.user = user;
-  //       ctx.project = project;
-  //       done();
-  //     });
-  //   });
-  //   // FIXME: create a build from a build (rebuild)
-  // });
+  /**
+   * This tests the rebuild functionality of a build.  We first create a user and environment, then
+   * we create a build.  Once finished, we should call the rebuild on the build
+   */
+  describe('POST:  Testing Build\'s rebuild functionality', function () {
+     beforeEach(function (done) {
+       nockS3();
+       multi.createRegisteredUserAndProject(function (err, user, project) {
+         if (err) { return done(err); }
+         ctx.user = user;
+         ctx.project = project;
+         var environments = ctx.project.fetchEnvironments(function (err) {
+           if (err) { return done(err); }
+
+           ctx.environment = environments.models[0];
+           done();
+         });
+       });
+     });
+    it('should create a new build from an existing one', function (done) {
+      var builds = ctx.environment.fetchBuilds(function (err, body, code) {
+        if (err) { return done(err); }
+
+        expect(code).to.equal(200);
+        expect(body).to.be.an('array');
+        // var testUser = body[0].createdBy;
+        expect(body[0].owner).to.be.a('string');
+        expect(body[0].createdBy).to.be.an('string');
+
+        var build = ctx.environment.fetchBuild(builds.models[0].id(), function (err) {
+          if (err) { return done(err); }
+          expect(build).to.be.okay;
+          // FIXME: build.createdBy()
+          // var buildCreator = build.createdBy();
+          // expect(buildCreator.toJSON()).to.equal(testUser);
+          done();
+        });
+      });
+    });
+  });
 
   describe('GET', function () {
     beforeEach(function (done) {
