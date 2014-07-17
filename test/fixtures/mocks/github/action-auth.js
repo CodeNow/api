80607f04715aaf56de2e22ef6773d43d9a1aa7b2
tjmehta <tj@init.me>
2014-07-17 09:51:15 -0700

    split up mocks

---
+++ test/fixtures/mocks/github/action-auth.js
@@ -0,0 +1,22 @@
+var isFunction = require('101/is-function');
+var findIndex = require('101/find-index');
+
+module.exports = function (userId, username, email, cb) {
+  var args = Array.prototype.slice.call(arguments);
+  var index = findIndex(args, isFunction);
+  args = args.slice(index+1);
+  userId   = args[0];
+  username = args[1];
+  email    = args[2];
+  cb       = args[3];
+
+  require('./user')(userId, username);
+  require('./user-emails')(email);
+
+  if (cb) {
+    cb(null, userId, username);
+  }
+  else {
+    return userId;
+  }
+};
