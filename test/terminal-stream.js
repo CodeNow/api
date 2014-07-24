614558964da0a6e5c271c3a4352fd1a611a35bb8
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-07-24 15:24:57 -0700

    update primus proxy to message style

--- test/terminal-stream.js
+++ test/terminal-stream.js
@@ -6,12 +6,8 @@ var beforeEach = Lab.beforeEach;
 var afterEach = Lab.afterEach;
 var after = Lab.after;
 var api = require('./fixtures/api-control');
-var dockerServer = require('./fixtures/docker.js');
-var Docker = require('dockerode');
-var docker = new Docker({
-  host: 'http://localhost',
-  port: 4242
-});
+var createCount = require('callback-count');
+
 var Primus = require('primus');
 var Socket = Primus.createSocket({
   transformer: process.env.PRIMUS_TRANSFORMER,
@@ -20,7 +16,7 @@ var Socket = Primus.createSocket({
   },
   parser: 'JSON'
 });
-var testServerPort = 3333;
+var testServerPort = 3111;
 var filibuster = require('Filibuster');
 var http = require('http');
 
@@ -29,17 +25,6 @@ describe('Socket Server', { timeout: 5000 }, function () {
 
   before(api.start.bind(ctx));
   after(api.stop.bind(ctx));
-
-  before(dockerServer.start.bind(ctx, 4242));
-
-  var container;
-  before(function (done) {
-    docker.createContainer({}, function (err, c) {
-      if (err) { return done(err); }
-      container = c;
-      container.start(done);
-    });
-  });
 
   beforeEach(function(done) {
     ctx.server = http.createServer();
@@ -53,13 +38,30 @@ describe('Socket Server', { timeout: 5000 }, function () {
     ctx.server.close(done);
   });
 
-  describe('Terminal test', function () {
+  describe('proxy test', function () {
+    var terminalStream, eventStream;
     var primus;
+    var containerId = '1c8feb1cc0e9';
     var pass = false;
     beforeEach(function (done) {
       pass = false;
-      primus = new Socket('http://localhost' +
-        ':'+testServerPort+'?type=filibuster&args={"containerId": "'+container.id+'"}');
+      primus = new Socket('http://'+process.env.IPADDRESS+':'+process.env.PORT);
+
+      terminalStream = primus.substream('terminalStream');
+      eventStream = primus.substream('eventStream');
+
+      primus.write({
+        id: 1,
+        event: 'terminal-stream',
+        data: {
+          dockPort: testServerPort,
+          dockHost: 'localhost',
+          type: 'filibuster',
+          containerId: containerId,
+          terminalStreamId: 'terminalStream',
+          eventStreamId: 'eventStream'
+        }
+      });
       done();
     });
     var check = function(errMsg, done) {
@@ -70,36 +72,79 @@ describe('Socket Server', { timeout: 5000 }, function () {
         return done(new Error(errMsg));
       });
     };
-    it('connect', function (done) {
+    it('should connect', function (done) {
       check('failed to connect', done);
-      primus.on('data', function (data) {
-        if(data.name === 'terminal'){
+      eventStream.on('data', function (data) {
+        if(data.event === 'connected'){
           pass = true;
-          return primus.end();
+          primus.end();
         }
       });
     });
-    it('send term command', function (done) {
-      var term = primus.substream('terminal');
-      check('echo failed to run', done);
-      term.on('data', function (data) {
-        if(~data.indexOf('TEST')) {
-          pass = true;
-          return primus.end();
-        }
-        term.write('echo TEST\n');
-      });
-    });
-    it('send clientEventsStream ping event', function (done) {
-      var cs = primus.substream('clientEvents');
+
+    it('should send event stream ping event', function (done) {
       check('echo failed to ping', done);
-      cs.on('data', function (data) {
+      eventStream.on('data', function (data) {
         if(data.event === 'pong') {
           pass = true;
           return primus.end();
         }
-        cs.write({
+        eventStream.write({
           event: 'ping'
+        });
+      });
+    });
+
+    it('should send test command', function (done) {
+      check('echo failed to run', done);
+      terminalStream.on('data', function (data) {
+        if(~data.indexOf('TEST')) {
+          pass = true;
+          return primus.end();
+        }
+        terminalStream.write('echo TEST\n');
+      });
+    });
+  });
+  describe('param validator', function() {
+    var primus;
+    var requiredParams = ['dockHost', 'dockPort', 'type', 'containerId',
+      'terminalStreamId', 'eventStreamId'];
+    beforeEach(function (done) {
+      pass = false;
+      primus = new Socket('http://'+process.env.IPADDRESS+':'+process.env.PORT);
+      done();
+    });
+    afterEach(function (done) {
+      primus.on('end', done);
+      primus.end();
+    });
+    it('should error if not all params sent', function (done) {
+      var doneCount = createCount(done);
+      var allParams = {
+        dockPort: 1111,
+        dockHost: 'localhost',
+        type: 'filibuster',
+        containerId: 'containerId',
+        terminalStreamId: 'terminalStream',
+        eventStreamId: 'eventStream'
+      };
+      requiredParams.forEach(function(param, i) {
+        doneCount.inc();
+        var testParams = JSON.parse(JSON.stringify(allParams));
+        delete testParams[param];
+        primus.write({
+          id: i,
+          event: 'terminal-stream',
+          data: testParams
+        });
+        primus.on('data', function(data) {
+          if(data.id === i) {
+            if(!data.error) {
+              done(data.error);
+            }
+            doneCount.next();
+          }
         });
       });
     });
