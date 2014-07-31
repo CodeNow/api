9e780e22d540c6f2cfb27db851bc886d2a6d0469
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-07-30 18:47:34 -0700

    initial mavis support

--- test/fixtures/dock.js
+++ test/fixtures/dock.js
@@ -2,27 +2,37 @@ var createCount = require('callback-count');
 var docker = require('./docker');
 var docklet = require('./docklet');
 var redis = require('models/redis');
-
+var mavisApp = require('mavis');
+var url = require('url');
 module.exports = {
   start: startDock,
   stop: stopDock
 };
-
+var testDockHost = 'http://localhost:4243';
 var ctx = {};
 function startDock (done) {
   var count = createCount(done);
   ctx.docker = docker.start(count.inc().next);
   ctx.docklet = docklet.start(count.inc().next);
-  redis.del("docks:http://localhost:4243", count.inc().next);
-  redis.del("docks:full", count.inc().next);
-  redis.lpush("docks:active", "http://localhost:4243", count.inc().next);
+  ctx.mavis = mavisApp.listen(url.parse(process.env.MAVIS_HOST).port);
+  ctx.mavis.on('listening', count.inc().next);
+  redis.lpush(process.env.REDIS_HOST_KEYS, testDockHost, count.inc().next);
+  redis.hmset(testDockHost,
+    'numContainers',
+    '0',
+    'numBuilds',
+    '0',
+    'host',
+    testDockHost,
+    count.inc().next);
 }
 function stopDock (done) {
   var count = createCount(done);
   ctx.docker = docker.stop(count.inc().next);
   ctx.docklet = docklet.stop(count.inc().next);
-  redis.del("docks:active", count.inc().next);
-  redis.del("docks:full", count.inc().next);
+  ctx.mavis = ctx.mavis.close(count.inc().next);
+  redis.del(process.env.REDIS_HOST_KEYS, count.inc().next);
+  redis.del(testDockHost, count.inc().next);
   delete ctx.docker;
   delete ctx.docklet;
 }
