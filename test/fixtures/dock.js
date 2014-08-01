a988d50b9f9e7e819b1cc998144f15c0f88d4615
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-08-01 14:44:28 -0700

    update api to use mavis. since we now look for a new dock host every create and build we moved the module into docker since this is the only place you should create and build i

--- test/fixtures/dock.js
+++ test/fixtures/dock.js
@@ -1,6 +1,5 @@
 var createCount = require('callback-count');
 var docker = require('./docker');
-var docklet = require('./docklet');
 var redis = require('models/redis');
 var mavisApp = require('mavis');
 var url = require('url');
@@ -13,7 +12,6 @@ var ctx = {};
 function startDock (done) {
   var count = createCount(done);
   ctx.docker = docker.start(count.inc().next);
-  ctx.docklet = docklet.start(count.inc().next);
   ctx.mavis = mavisApp.listen(url.parse(process.env.MAVIS_HOST).port);
   ctx.mavis.on('listening', count.inc().next);
   redis.lpush(process.env.REDIS_HOST_KEYS, testDockHost, count.inc().next);
@@ -29,10 +27,8 @@ function startDock (done) {
 function stopDock (done) {
   var count = createCount(done);
   ctx.docker = docker.stop(count.inc().next);
-  ctx.docklet = docklet.stop(count.inc().next);
   ctx.mavis = ctx.mavis.close(count.inc().next);
   redis.del(process.env.REDIS_HOST_KEYS, count.inc().next);
   redis.del(testDockHost, count.inc().next);
   delete ctx.docker;
-  delete ctx.docklet;
 }
