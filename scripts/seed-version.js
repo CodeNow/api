55d369b3da408ba73d5eff08d1a4dcdc398b8ce4
Bryan Kendall <bryan@runnable.com>
2014-08-07 15:07:25 -0700

    adding a blank template in seed script

--- scripts/seed-version.js
+++ scripts/seed-version.js
@@ -8,22 +8,54 @@ var async = require('async');
 var mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO);
-createFirstSourceContext();
+async.series([
+  function ensureMongooseIsConnected (cb) {
+    console.log('ensure');
+    if (mongoose.connection.readyState === 1) {
+      cb();
+    }
+    else {
+      mongoose.connection.once('connected', cb);
+    }
+  },
+  createFirstSourceContext,
+  createBlankSourceContext
+], function (err) {
+  console.log('done');
+  if (err) { console.error(err); }
+  process.exit(err ? 1 : 0);
+});
 
 var createdBy = { github: 160236 };
 
-function createFirstSourceContext () {
-
+function createBlankSourceContext (cb) {
   async.waterfall([
-    function ensureMongooseIsConnected (cb) {
-      console.log('ensure');
-      if (mongoose.connection.readyState === 1) {
-        cb();
-      }
-      else {
-        mongoose.connection.once('connected', cb);
-      }
+    function newContext (cb) {
+      console.log('newContext-blank');
+      var context = new Context({
+        owner: createdBy,
+        name: 'Blank',
+        description: 'An empty template!',
+        isSource: true
+      });
+      context.save(cb);
     },
+    function newICV (context, count, cb) {
+      console.log('newICV');
+      var icv = new InfraCodeVersion({
+        context: context._id
+      });
+      async.series([
+        icv.initWithDockerfile.bind(icv, ''),
+        icv.save.bind(icv)
+      ], function (err) { cb(err, context, icv); });
+    },
+    newCV,
+  ], cb);
+}
+
+function createFirstSourceContext (cb) {
+  async.waterfall([
     function newContext (cb) {
       console.log('newContext');
       var context = new Context({
@@ -44,25 +76,22 @@ function createFirstSourceContext () {
         icv.save.bind(icv)
       ], function (err) { cb(err, context, icv); });
     },
-    function newCV (context, icv, cb) {
-      console.log('newCV');
-      var d = new Date();
-      var cv = new ContextVersion({
-        createdBy: createdBy,
-        context: context._id,
-        environment: context._id,
-        infraCodeVersion: icv._id,
-        build: {
-          started: d,
-          completed: d
-        }
-      });
-      cv.save(cb);
-    },
-  ], function (err) {
-    console.log('done');
-    if (err) { console.error(err); }
-    process.exit(err ? 1 : 0);
-  });
+    newCV,
+  ], cb);
+}
 
+function newCV (context, icv, cb) {
+  console.log('newCV');
+  var d = new Date();
+  var cv = new ContextVersion({
+    createdBy: createdBy,
+    context: context._id,
+    environment: context._id,
+    infraCodeVersion: icv._id,
+    build: {
+      started: d,
+      completed: d
+    }
+  });
+  cv.save(cb);
 }
