5af08b3054b9e2d212f9efb3e6e42210f5df2930
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-09-08 13:40:37 -0700

    update container attach nock

--- test/fixtures/mocks/docker/container-id-attach.js
+++ test/fixtures/mocks/docker/container-id-attach.js
@@ -2,13 +2,17 @@ var nock = require('nock');
 
 module.exports = function () {
   nock('http://localhost:4243', { allowUnmocked: true })
-    .filteringPath(/\/containers\/[0-9a-f]+\/attach\?.+/,
-      '/containers/284912fa2cf26d40cc262798ecbb483b58f222d42ab1551e818afe35744688f7/attach')
-    .post('/containers/284912fa2cf26d40cc262798ecbb483b58f222d42ab1551e818afe35744688f7/attach')
-    .reply(200, 'Successfully built 15e17eedec196751ad15cdb1cef61f6022c19bee01b8079');
+    .filteringPath(/\/containers\/[0-9a-f]+\/logs\?.+/,
+      '/containers/284912fa2cf26d40cc262798ecbb483b58f222d42ab1551e818afe35744688f7/logs')
+    .get('/containers/284912fa2cf26d40cc262798ecbb483b58f222d42ab1551e818afe35744688f7/logs')
+    .reply(200, 'Successfully built d776bdb409ab783cea9b986170a2a496684c9a99a6f9c048080d32980521e743');
 
   nock('http://localhost:4243', { allowUnmocked: true })
     .filteringPath(/\/images\/.+\/push/, '/images/repo/push')
     .post('/images/repo/push')
+    .reply(200);
+
+  nock('http://localhost:4243', { allowUnmocked: true })
+    .post('/images/push')
     .reply(200);
 };
