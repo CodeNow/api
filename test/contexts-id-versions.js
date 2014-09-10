17e06fb4203a357e71c579eeea971b9487ae8ddb
Bryan Kendall <bryan@runnable.com>
2014-09-10 12:52:44 -0700

    adding awesome version searching via app code versions

--- test/contexts-id-versions.js
+++ test/contexts-id-versions.js
@@ -33,44 +33,67 @@ describe('Versions - /contexts/:contextid/versions', function () {
         done(err);
       });
     });
-    describe('via appCodeVersions.repo', function () {
+    describe('via appCodeVersions', function () {
       it('should return us our version', function (done) {
         var expected = [{
           _id: ctx.contextVersion.id()
         }];
         var query = {
-          appCodeVersions: {
-            repo: ctx.contextVersion.json().appCodeVersions[0].repo
-          }
+          appCodeVersions: [{
+            repo: ctx.contextVersion.json().appCodeVersions[0].repo,
+            branch: ctx.contextVersion.json().appCodeVersions[0].branch,
+            commit: ctx.contextVersion.json().appCodeVersions[0].commit
+          }]
+        };
+        ctx.context.fetchVersions(query, expects.success(200, expected, done));
+      });
+      it('should return us nothing (no version with both app code versions', function (done) {
+        var expected = [];
+        var query = {
+          appCodeVersions: [{
+            repo: ctx.contextVersion.json().appCodeVersions[0].repo,
+            branch: ctx.contextVersion.json().appCodeVersions[0].branch,
+            commit: ctx.contextVersion.json().appCodeVersions[0].commit
+          }, {
+            repo: ctx.contextVersion.json().appCodeVersions[0].repo,
+            branch: ctx.contextVersion.json().appCodeVersions[0].branch,
+            commit: ctx.contextVersion.json().appCodeVersions[0].commit
+          }]
         };
         ctx.context.fetchVersions(query, expects.success(200, expected, done));
       });
     });
-    describe('via appCodeVersions.commit', function () {
-      it('should return us our version', function (done) {
-        var expected = [{
-          _id: ctx.contextVersion.id()
-        }];
+    describe('via appCodeVersions.commit and .branch', function () {
+      it('should tell us repo is required', function (done) {
         var query = {
-          appCodeVersions: {
-            commit: ctx.contextVersion.json().appCodeVersions[0].commit
-          }
+          appCodeVersions: [{
+            commit: ctx.contextVersion.json().appCodeVersions[0].commit,
+            branch: ctx.contextVersion.json().appCodeVersions[0].branch
+          }]
         };
-        ctx.context.fetchVersions(query, expects.success(200, expected, done));
+        ctx.context.fetchVersions(query, expects.error(400, /repo.+required/, done));
       });
     });
     describe('via appCodeVersions.commit and .repo', function () {
-      it('should return us our version', function (done) {
-        var expected = [{
-          _id: ctx.contextVersion.id()
-        }];
+      it('should tell us branch is required', function (done) {
         var query = {
-          appCodeVersions: {
+          appCodeVersions: [{
             commit: ctx.contextVersion.json().appCodeVersions[0].commit,
             repo: ctx.contextVersion.json().appCodeVersions[0].repo
-          }
+          }]
         };
-        ctx.context.fetchVersions(query, expects.success(200, expected, done));
+        ctx.context.fetchVersions(query, expects.error(400, /branch.+required/, done));
+      });
+    });
+    describe('via appCodeVersions.branch and .repo', function () {
+      it('should tell us commit is required', function (done) {
+        var query = {
+          appCodeVersions: [{
+            branch: ctx.contextVersion.json().appCodeVersions[0].branch,
+            repo: ctx.contextVersion.json().appCodeVersions[0].repo
+          }]
+        };
+        ctx.context.fetchVersions(query, expects.error(400, /commit.+required/, done));
       });
     });
     describe('via infraCodeVersion', function () {
