f8bcbd6cdb7ce2bb6dbbfddc421ce71e26e10834
Tejesh Mehta <tejesh.mehta@gmail.com>
2014-09-12 16:18:28 -0700

    fixed deduping test

--- test/bdd-context-version-deduping.js
+++ test/bdd-context-version-deduping.js
@@ -10,13 +10,7 @@ var expect = Lab.expect;
 var api = require('./fixtures/api-control');
 var dock = require('./fixtures/dock');
 var multi = require('./fixtures/multi-factory');
-var expects = require('./fixtures/expects');
-var equals = require('101/equals');
-var clone = require('101/clone');
 var uuid = require('uuid');
-var not = require('101/not');
-var Build = require('models/mongo/build');
-var exists = require('101/exists');
 var createCount = require('callback-count');
 
 /**
@@ -55,38 +49,30 @@ describe('Building - Context Version Deduping', function () {
       });
     });
     it('should fork the instance, and both should be deployed when the build ' +
-      'is finished', { timeout: 10000000 }, function (done) {
+      'is finished', { timeout: 5000 }, function (done) {
       // start the build
       require('./fixtures/mocks/docker/container-id-attach')();
       require('./fixtures/mocks/github/repos-username-repo-branches-branch')(ctx.cv);
       require('./fixtures/mocks/github/user')(ctx.user);
       ctx.build.build({ message: uuid() }, function (err) {
-        if (err) {
-          return done(err);
-        }
+        if (err) { return done(err); }
         // Add it to an instance
         var json = { build: ctx.build.id(), name: uuid() };
         var instance = ctx.user.createInstance({ json: json }, function (err) {
-          if (err) {
-            done(err);
-          }
+          if (err) { return done(err); }
           // Now fork that instance
           require('./fixtures/mocks/github/user')(ctx.user);
           var forkedInstance = instance.copy(function(err) {
-            if (err) {
-              return done(err);
-            }
+            if (err) { return done(err); }
+            // Now tail both and make sure they both start
             var count = createCount(2, done);
-            function expected(err, instance) {
-              if (err) {
-                return count.next(err);
-              }
+            multi.tailInstance(ctx.user, instance, next);
+            multi.tailInstance(ctx.user, forkedInstance, next);
+            function next (err, instance) {
+              if (err) { return count.next(err); }
               expect(instance.attrs.containers[0].inspect.State.Running).to.be.okay;
               count.next();
             }
-            // Now tail both and make sure they both start
-            multi.tailInstance(ctx.user, instance, expected);
-            multi.tailInstance(ctx.user, forkedInstance, expected);
           });
         });
       });
