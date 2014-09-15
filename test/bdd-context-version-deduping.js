3e274739c46fcc39b42ec1ae3b916c6df6949a8f
Nathan219 <Nathan219@gmail.com>
2014-09-15 13:57:53 -0700

    Changing build to not use tail-build-stream Adding tests

--- test/bdd-context-version-deduping.js
+++ test/bdd-context-version-deduping.js
@@ -12,6 +12,8 @@ var dock = require('./fixtures/dock');
 var multi = require('./fixtures/multi-factory');
 var uuid = require('uuid');
 var createCount = require('callback-count');
+var Build = require('models/mongo/build');
+var tailBuildStream = require('./fixtures/tail-build-stream');
 
 /**
  * This tests many of the different possibilities that can happen during build, namely when deduping
@@ -23,11 +25,11 @@ describe('Building - Context Version Deduping', function () {
   /**
    * What needs testing
    *
-   * Create instance from in-progress build, should deploy when finished
-   * Fork instance with finished build, should deploy
-   * Fork instance with failed build, should not deploy
-   * Fork instance with in-progress build, should deploy both when successful
-   * Fork instance with in-progress build, shouldn't deploy when failed
+   * - Create instance from in-progress build, should deploy when finished
+   * - Fork instance with finished build, should deploy
+   * - Fork instance with failed build, should not deploy
+   * - Fork instance with in-progress build, should deploy both when successful
+   * - Fork instance with in-progress build, shouldn't deploy when failed
    */
 
   before(api.start.bind(ctx));
@@ -49,9 +51,9 @@ describe('Building - Context Version Deduping', function () {
       });
     });
     it('should fork the instance, and both should be deployed when the build ' +
-      'is finished', { timeout: 5000 }, function (done) {
+      'is finished', { timeout: 1000 }, function (done) {
       // start the build
-      require('./fixtures/mocks/docker/container-id-attach')();
+      require('./fixtures/mocks/docker/container-id-attach')(200);
       require('./fixtures/mocks/github/repos-username-repo-branches-branch')(ctx.cv);
       require('./fixtures/mocks/github/user')(ctx.user);
       ctx.build.build({ message: uuid() }, function (err) {
@@ -77,5 +79,126 @@ describe('Building - Context Version Deduping', function () {
         });
       });
     });
+    it('should fork the instance, and but not deploy since the build failed ' +
+      'is finished', { timeout: 1000 }, function (done) {
+      // start the build
+      require('./fixtures/mocks/docker/container-id-attach')(200, 'Failure');
+      require('./fixtures/mocks/github/repos-username-repo-branches-branch')(ctx.cv);
+      require('./fixtures/mocks/github/user')(ctx.user);
+      ctx.build.build({ message: uuid() }, function (err) {
+        if (err) { return done(err); }
+        // Add it to an instance
+        var json = { build: ctx.build.id(), name: uuid() };
+        var instance = ctx.user.createInstance({ json: json }, function (err) {
+          if (err) { return done(err); }
+          // Now fork that instance
+          require('./fixtures/mocks/github/user')(ctx.user);
+          var forkedInstance = instance.copy(function(err) {
+            if (err) { return done(err); }
+            // Now tail the buildstream so we can check if the instances do not deploy
+            tailBuildStream(ctx.cv.id(), function() {
+              checkInstance(ctx.user, instance, next);
+              checkInstance(ctx.user, forkedInstance, next);
+            });
+            var count = createCount(2, done);
+            function next (err, instance) {
+              if (err) { return count.next(err); }
+              expect(instance.containers.length).to.not.be.okay;
+              count.next();
+            }
+          });
+        });
+      });
+    });
+    it('should fork after failure, so the instance should not deploy', { timeout: 1000 }, function (done) {
+      // start the build
+      require('./fixtures/mocks/docker/container-id-attach')(200, 'Failure');
+      require('./fixtures/mocks/github/repos-username-repo-branches-branch')(ctx.cv);
+      require('./fixtures/mocks/github/user')(ctx.user);
+      ctx.build.build({ message: uuid() }, function (err) {
+        if (err) { return done(err); }
+        // Add it to an instance
+        var json = { build: ctx.build.id(), name: uuid() };
+        var instance = ctx.user.createInstance({ json: json }, function (err) {
+          if (err) {
+            return done(err);
+          }
+          // Now wait for the finished build
+          tailBuildStream(ctx.cv.id(), function () {
+            require('./fixtures/mocks/github/user')(ctx.user);
+            var forkedInstance = instance.copy(function (err) {
+              if (err) {
+                return done(err);
+              }
+              checkInstance(ctx.user, forkedInstance, next);
+              function next(err, instance) {
+                if (err) {
+                  return count.next(err);
+                }
+                expect(instance.containers.length).to.not.be.okay;
+                done();
+              }
+            });
+          });
+        });
+      });
+    });
+  });
+  describe('fork instance with finished build', function () {
+    beforeEach(function (done) {
+      multi.createBuiltBuild(function (err, build, user, modelArray) {
+        ctx.build = build;
+        ctx.user = user;
+        ctx.cv = modelArray[0];
+        done();
+      });
+    });
+    it('should deploy right after', { timeout: 1000 }, function (done) {
+      // start the build
+      require('./fixtures/mocks/docker/container-id-attach')();
+      require('./fixtures/mocks/github/repos-username-repo-branches-branch')(ctx.cv);
+      require('./fixtures/mocks/github/user')(ctx.user);
+      // Add it to an instance
+      var json = { build: ctx.build.id(), name: uuid() };
+      var instance = ctx.user.createInstance({ json: json }, function (err) {
+        if (err) {
+          return done(err);
+        }
+        // Now fork that instance
+        require('./fixtures/mocks/github/user')(ctx.user);
+        var forkedInstance = instance.copy(function (err) {
+          if (err) {
+            return done(err);
+          }
+          // Now tail both and make sure they both start
+          var count = createCount(2, done);
+          multi.tailInstance(ctx.user, instance, next);
+          multi.tailInstance(ctx.user, forkedInstance, next);
+          function next(err, instance) {
+            if (err) {
+              return count.next(err);
+            }
+            expect(instance.attrs.containers[0].inspect.State.Running).to.be.okay;
+            count.next();
+          }
+        });
+      });
+    });
   });
 });
+
+function checkInstance(user, instance, ownerId, cb) {
+  if (typeof ownerId === 'function') {
+    cb = ownerId;
+    ownerId = null;
+  }
+  setTimeout(function () {
+    require('./fixtures/mocks/github/user')(user);
+    if (ownerId) {
+      require('./fixtures/mocks/github/user-orgs')(ownerId, 'Runnable');
+      require('./fixtures/mocks/github/user-orgs')(ownerId, 'Runnable');
+    }
+    require('./fixtures/mocks/github/user')(user);
+    instance = user.fetchInstance(instance.id(), cb);
+  }, 100);
+}
--- test/fixtures/mocks/docker/container-id-attach.js
+++ test/fixtures/mocks/docker/container-id-attach.js
@@ -1,12 +1,13 @@
 var nock = require('nock');
+var Boom = require('dat-middleware').Boom;
 
-module.exports = function () {
+module.exports = function (delay, failure) {
   nock('http://localhost:4243', { allowUnmocked: true })
     .filteringPath(/\/containers\/[0-9a-f]+\/logs\?.+/,
       '/containers/284912fa2cf26d40cc262798ecbb483b58f222d42ab1551e818afe35744688f7/logs')
     .get('/containers/284912fa2cf26d40cc262798ecbb483b58f222d42ab1551e818afe35744688f7/logs')
-    .delayConnection(100)
-    .reply(200, 'Successfully built d776bdb409ab783cea9b986170a2a496684c9a99a6f9c048080d32980521e743');
+    .delayConnection(delay || 0)
+    .reply((failure) ? 403 : 200, failure || 'Successfully built d776bdb409ab783cea9b986170a2a496684c9a99a6f9c048080d32980521e743');
 
   nock('http://localhost:4243', { allowUnmocked: true })
     .filteringPath(/\/images\/.+\/push/, '/images/repo/push')
