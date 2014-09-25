b96798ab32c6f58e05107cfbbda6549f7974d22c
Bryan Kendall <bryan@runnable.com>
2014-09-25 10:17:52 -0700

    adding test for cache results in github proxy

--- test/github-proxy.js
+++ test/github-proxy.js
@@ -14,6 +14,9 @@ var generateKey = require('./fixtures/key-factory');
 var createCount = require('callback-count');
 var concat = require('concat-stream');
 var zlib = require('zlib');
+var redis = require('models/redis');
+var async = require('async');
+var nock = require('nock');
 
 describe('Github Proxy', function () {
   var ctx = {};
@@ -25,6 +28,14 @@ describe('Github Proxy', function () {
   afterEach(require('./fixtures/clean-mongo').removeEverything);
   afterEach(require('./fixtures/clean-ctx')(ctx));
 
+  beforeEach(function (done) {
+    redis.keys('github-proxy-cache:*', function (err, data) {
+      if (err) { return done(err); }
+      async.each(data, function (key, cb) {
+        redis.del(key, cb);
+      }, done);
+    });
+  });
   beforeEach(function (done) {
     multi.createUser(function (err, user) {
       ctx.user = user;
@@ -58,6 +69,32 @@ describe('Github Proxy', function () {
         expect(r.response.headers['access-control-allow-origin']).to.equal(undefined);
         expect(r.response.headers['access-control-allow-credentials']).to.equal('true');
         done();
+      }));
+    });
+    it('should have the correct headers when it comes from the cache', function (done) {
+      var r = ctx.user.client.get('/github/user');
+      r.on('error', done);
+      r.pipe(zlib.createGunzip()).pipe(concat(function (body) {
+        expect(body).to.be.okay;
+        // in this case, because of the test, we shouldn't have access-control-allow-origin
+        // (it would be the value set from github if it was here)
+        expect(r.response.headers['access-control-allow-origin']).to.equal(undefined);
+        expect(r.response.headers['access-control-allow-credentials']).to.equal('true');
+        // for a cache hit
+        nock('https://api.github.com:443')
+          .get('/user?access_token='+ctx.user.json().accounts.github.access_token)
+          .reply(304, null, {
+            'access-control-allow-origin': '*',
+            'access-control-allow-credentials': 'true'
+          });
+        r2 = ctx.user.client.get('/github/user');
+        r2.on('error', done);
+        r2.pipe(zlib.createGunzip()).pipe(concat(function (body) {
+          expect(body).to.be.okay;
+          expect(r.response.headers['access-control-allow-origin']).to.equal(undefined);
+          expect(r.response.headers['access-control-allow-credentials']).to.equal('true');
+          done();
+        }));
       }));
     });
   });
