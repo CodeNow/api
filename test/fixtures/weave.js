00251d6b899a967454f447c4d1d9910d38fe13ea
Tejesh Mehta <tejesh.mehta@gmail.com>
2014-10-27 15:51:13 -0700

    remove weave tracking from fixtures (we can verify states with sauron itself now)

--- test/fixtures/weave.js
+++ test/fixtures/weave.js
@@ -1,8 +1,6 @@
 var weave = require('sauron/lib/engines/weave-wrapper');
 var extend = require('extend');
 
-var attached = {};
-
 // Mocks for weave
 extend(weave, {
   status: function (opts, cb) {
@@ -12,25 +10,14 @@ extend(weave, {
     return cb(null, 'mock');
   },
   attach: function (opts, cb) {
-    attached[opts.containerId] = opts.ipaddr;
     return cb(null, 'mock');
   },
   detach: function (opts, cb) {
-    delete attached[opts.containerId];
     return cb(null, 'mock');
   },
   runCmd: function (opts, cb) {
     return cb(null, 'mock');
   },
 });
-
-// Test access for weave
-weave.hostIpForContainer = function (containerId) {
-  return attached[containerId];
-};
-weave.clean = function (cb) {
-  attached = {};
-  cb();
-};
 
 module.exports = weave;
