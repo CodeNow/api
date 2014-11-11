ac1fbb7777dee907dc716847261d544ac5819945
Anton Podviaznikov <podviaznikov@gmail.com>
2014-11-10 20:05:40 -0800

    refactor test-utils

--- test/fixtures/types-test-util.js
+++ test/fixtures/types-test-util.js
@@ -37,8 +37,12 @@ function errorMessageSuffix(paramType, type) {
   return suffixes[paramType];
 }
 
-function buildBodyWithRequiredParams(ctx, requiredParams) {
+function buildBodyWithRequiredParams(ctx, requiredParams, param, type) {
   var body = {};
+  if(param && type) {
+    body[param.name] = typeValue(ctx, type);
+  }
+  
   if(requiredParams) {
     requiredParams.forEach(function(requiredParam) {
       body[requiredParam.name] = typeValue(ctx, requiredParam.type);
@@ -47,9 +51,11 @@ function buildBodyWithRequiredParams(ctx, requiredParams) {
   return body;
 }
 
-function buildBodyForRequiredParams(ctx, requiredParams, param, paramIndex) {
+function buildBodyForRequiredParams(ctx, requiredParams, param, type, paramIndex) {
   var body = {};
-  body[param.name] = typeValue(ctx, param.type);
+  if(param && type) {
+    body[param.name] = typeValue(ctx, type);
+  }
   if(requiredParams) {
     requiredParams.forEach(function(requiredParam, index) {
       if(index < paramIndex) {
@@ -61,6 +67,58 @@ function buildBodyForRequiredParams(ctx, requiredParams, param, paramIndex) {
   return body;
 }
 
+function setupTests(ctx, handler, def, types, param, buildBodyFunction, index) {
+  var paramTypes = types.filter(function(type) {
+    return type !== param.type;
+  });
+  paramTypes.forEach(function(type) {
+    it('should not ' + def.action + ' when `' + param.name + '` param is ' + type, function(done) {
+      var body = buildBodyFunction(ctx, def.requiredParams, param, type, index);
+      var message = new RegExp('body parameter "' + param.name + '" ' + errorMessageSuffix(param.type, type));
+      var cb = expects.error(400, message, done);
+      handler(body, cb);
+    });
+  });
+}
+
+function setupArrayParamsTests(ctx, handler, def, types, param, buildBodyFunction, index) {
+  // handle array param
+  // TODO (anton) we should handle required array params too
+  if(param.type === 'array') {
+    var arrayItemTypes = types.filter(function(type) {
+      return type !== param.itemType;
+    });
+    arrayItemTypes.forEach(function(arrayItemType) {
+      var testName = 'should not ' + def.action + ' when `' + param.name +
+      '` param has ' + arrayItemType + ' items in the array';
+      it(testName, function(done) {
+        var body = buildBodyFunction(ctx, def.requiredParams, param);
+        body[param.name] = [];
+        body[param.name].push(typeValue(ctx, arrayItemType));
+        body[param.name].push(typeValue(ctx, arrayItemType));
+        body[param.name].push(typeValue(ctx, arrayItemType));
+        // e.g. body parameter "env" should be an array of strings
+        var regexp = 'body parameter "' + param.name + '" ' + errorMessageSuffix(param.type, arrayItemType) +
+        ' of ' + param.itemType + 's';
+        var message = new RegExp(regexp);
+        var cb = expects.error(400, message, done);
+        handler(body, cb);
+      }); 
+    });
+    param.itemValues.forEach(function(itemValue) {
+      var testName = 'should not ' + def.action + ' when `' + param.name +
+      '` param has invalid item value such as ' + itemValue;
+      it(testName, function(done) {
+        var body = buildBodyFunction(ctx, def.requiredParams);
+        body[param.name] = [itemValue];
+        // e.g. "env" should match 
+        var message = new RegExp('"' + param.name + '" should match ');
+        var cb = expects.error(400, message, done);
+        handler(body, cb);
+      }); 
+    });
+  }
+}
 
 exports.makeTestFromDef = function(def, ctx, handler) {
   // TODO (anton) null and undefined values are breaking code now. Investigate it
@@ -69,70 +127,14 @@ exports.makeTestFromDef = function(def, ctx, handler) {
 
   if(def.requiredParams) {
     def.requiredParams.forEach(function(param, index) {
-      var paramTypes = types.filter(function(type) {
-        return type !== param.type;
-      });
-      paramTypes.forEach(function(type) {
-        it('should not ' + def.action + ' when `' + param.name + '` param is ' + type, function(done) {
-          var body = buildBodyForRequiredParams(ctx, def.requiredParams, param, index);
-          body[param.name] = typeValue(ctx, type);
-          var message = new RegExp('body parameter "' + param.name + '" ' + errorMessageSuffix(param.type, type));
-          var cb = expects.error(400, message, done);
-          handler(body, cb);
-        });
-      });
+      setupTests(ctx, handler, def, types, param, buildBodyForRequiredParams, index);
+      setupArrayParamsTests(ctx, handler, def, types, param, buildBodyForRequiredParams, index);
     });
   }
   if(def.optionalParams) {
-    def.optionalParams.forEach(function(param) {
-      var paramTypes = types.filter(function(type) {
-        return type !== param.type;
-      });
-      paramTypes.forEach(function(type) {
-        it('should not ' + def.action + ' when `' + param.name + '` param is ' + type, function(done) {
-          var body = buildBodyWithRequiredParams(ctx, def.requiredParams);
-          body[param.name] = typeValue(ctx, type);
-          var message = new RegExp('body parameter "' + param.name + '" ' + errorMessageSuffix(param.type, type));
-          var cb = expects.error(400, message, done);
-          handler(body, cb);
-        });
-      });
-      // handle array param
-      // TODO (anton) we should handle required array params too
-      if(param.type === 'array') {
-        var arrayItemTypes = types.filter(function(type) {
-          return type !== param.itemType;
-        });
-        arrayItemTypes.forEach(function(arrayItemType) {
-          var testName = 'should not ' + def.action + ' when `' + param.name +
-          '` param has ' + arrayItemType + ' items in the array';
-          it(testName, function(done) {
-            var body = buildBodyWithRequiredParams(ctx, def.requiredParams);
-            body[param.name] = [];
-            body[param.name].push(typeValue(ctx, arrayItemType));
-            body[param.name].push(typeValue(ctx, arrayItemType));
-            body[param.name].push(typeValue(ctx, arrayItemType));
-            // e.g. body parameter "env" should be an array of strings
-            var regexp = 'body parameter "' + param.name + '" ' + errorMessageSuffix(param.type, arrayItemType) +
-            ' of ' + param.itemType + 's';
-            var message = new RegExp(regexp);
-            var cb = expects.error(400, message, done);
-            handler(body, cb);
-          }); 
-        });
-        param.itemValues.forEach(function(itemValue) {
-          var testName = 'should not ' + def.action + ' when `' + param.name +
-          '` param has invalid item value such as ' + itemValue;
-          it(testName, function(done) {
-            var body = buildBodyWithRequiredParams(ctx, def.requiredParams);
-            body[param.name] = [itemValue];
-            // e.g. "env" should match 
-            var message = new RegExp('"' + param.name + '" should match ');
-            var cb = expects.error(400, message, done);
-            handler(body, cb);
-          }); 
-        });
-      }
+    def.optionalParams.forEach(function(param, index) {
+      setupTests(ctx, handler, def, types, param, buildBodyWithRequiredParams, index);
+      setupArrayParamsTests(ctx, handler, def, types, param, buildBodyWithRequiredParams, index);
     });
   }
 };
