d481a9e2deb852dce6a51a9b0ba33bebe30fc144
Anton Podviaznikov <podviaznikov@gmail.com>
2014-11-24 17:18:08 -0800

    handling docker daemon down event in tests

--- test/events.js
+++ test/events.js
@@ -5,6 +5,7 @@ var it = Lab.test;
 var before = Lab.before;
 var after = Lab.after;
 var beforeEach = Lab.beforeEach;
+var afterEach = Lab.afterEach;
 
 var expect = Lab.expect;
 var api = require('./fixtures/api-control');
@@ -60,6 +61,10 @@ describe('Events handler', function () {
         });
       });
 
+      afterEach(require('./fixtures/clean-ctx')(ctx));
+      afterEach(require('./fixtures/clean-nock'));
+      afterEach(require('./fixtures/clean-mongo').removeEverything);
+
       it('should stop instance if die event received', function (done) {
         events.handleContainerDie({id: ctx.container.attrs.inspect.Id, ip: '192.0.0.1'}, function (err) {
           if (err) { return done(err); }
@@ -114,5 +119,68 @@ describe('Events handler', function () {
     });
 
   });
+
+  describe('docker daemon down', function () {
+
+
+    describe('started instance case', function () {
+      beforeEach(function (done) {
+        multi.createContainer(function (err, container, instance, build) {
+          if (err) { return done(err); }
+          ctx.build = build;
+          ctx.instance = instance;
+          ctx.container = container;
+          ctx.build = build;
+          expect(instance.attrs.container.inspect.State.Running).to.equal(true);
+          done();
+        });
+      });
+
+      afterEach(require('./fixtures/clean-ctx')(ctx));
+      afterEach(require('./fixtures/clean-nock'));
+      afterEach(require('./fixtures/clean-mongo').removeEverything);
+
+      it('should stop instances if docker down event received', function (done) {
+        events.handleDockerDaemonDown({ip: ctx.instance.attrs.container.dockerHost}, function (err) {
+          if (err) { return done(err); }
+          ctx.instance.fetch(function (err, newInstanceState) {
+            if (err) { return done(err); }
+            expect(newInstanceState.container.inspect.State.Running).to.equal(false);
+            done();
+          });
+        });
+      });
+
+      it('should try to stop stopped instance without error', function (done) {
+        ctx.instance.stop(function (err) {
+          if (err) { return done(err); }
+          ctx.instance.fetch(function (err, instance) {
+            if (err) { return done(err); }
+            expect(instance.container.inspect.State.Running).to.equal(false);
+            events.handleDockerDaemonDown({ip: ctx.instance.attrs.container.dockerHost}, function (err) {
+              if (err) { return done(err); }
+              ctx.instance.fetch(function (err, newInstanceState) {
+                if (err) { return done(err); }
+                expect(newInstanceState.container.inspect.State.Running).to.equal(false);
+                done();
+              });
+            });
+          });
+        });
+      });
+
+    });
+
+
+
+    it('should fail if event data has no ip', function (done) {
+      events.handleDockerDaemonDown({id: 'duasiduia213'}, function (err) {
+        expect(err.message).to.equal('Invalid data: ip is missing');
+        done();
+      });
+    });
+
+  });
+
 
 });
