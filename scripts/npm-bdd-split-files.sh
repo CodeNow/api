284d97cbc3682039907ddffa4ce75d0745bcb503
Bryan Kendall <bryan@runnable.com>
2014-12-04 13:54:39 -0800

    group by index, not file

--- scripts/npm-bdd-split-files.sh
+++ scripts/npm-bdd-split-files.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-i=0
-files=()
 for file in $(find ./test -path ./test/fixtures -prune -o -name "*.js" -print | sort)
 do
-  if [[ "$file" !=   "./test/fixtures" && $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]]
+  if [[ "$file" !=   "./test/fixtures" ]]
   then
     files+=" $file"
   fi
-  ((i++))
 done
 
-npm run _bdd -- ${files[@]}
+numTests=$(npm run _bdd -- -d ${files[@]} | grep -e '\d\+ tests complete' | awk '{split($0,r," "); print r[1];}')
+echo $numTests to run
+
+indexes=()
+for i in $(seq 1 $numTests)
+do
+  if [[ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]]
+  then
+    indexes+=" -i $i"
+  fi
+done
+
+npm run _bdd -- ${indexes[@]} ${files[@]}
