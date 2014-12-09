f2629db2c44d5ab1a67ea94b247bc36db477a2e2
Anton Podviaznikov <podviaznikov@gmail.com>
2014-12-09 11:09:14 -0800

    Bump runnable version. Starting work on events tests

--- test/events/docker-die-event.js
+++ test/events/docker-die-event.js
@@ -1,58 +1,59 @@
-// var Lab = require('lab');
-// var describe = Lab.experiment;
+var Lab = require('lab');
+var describe = Lab.experiment;
+var it = Lab.test;
 
-// var before = Lab.before;
-// var after = Lab.after;
-// var beforeEach = Lab.beforeEach;
+var before = Lab.before;
+var after = Lab.after;
+var beforeEach = Lab.beforeEach;
+var afterEach = Lab.afterEach;
 
-// var find = require('101/find');
-// var hasKeypaths = require('101/has-keypaths');
+var find = require('101/find');
+var hasKeypaths = require('101/has-keypaths');
 
-// var api = require('../../fixtures/api-control');
-// var dock = require('../../fixtures/dock');
-// var multi = require('../../fixtures/multi-factory');
-// var pubsub = require('models/redis/pubsub');
+var api = require('../fixtures/api-control');
+var dock = require('../fixtures/dock');
+var multi = require('../fixtures/multi-factory');
+var pubsub = require('models/redis/pubsub');
 
 
-// var typesTests = require('../../fixtures/types-test-util');
 
-// describe('EVENT runnable:docker:events:die', function () {
-//   var ctx = {};
+describe('EVENT runnable:docker:events:die', function () {
+  var ctx = {};
 
-//   before(api.start.bind(ctx));
-//   before(dock.start.bind(ctx));
-//   before(require('../../fixtures/mocks/api-client').setup);
-//   after(api.stop.bind(ctx));
-//   after(dock.stop.bind(ctx));
-//   after(require('../../fixtures/mocks/api-client').clean);
+  before(api.start.bind(ctx));
+  before(dock.start.bind(ctx));
+  before(require('../fixtures/mocks/api-client').setup);
+  after(api.stop.bind(ctx));
+  after(dock.stop.bind(ctx));
+  after(require('../fixtures/mocks/api-client').clean);
 
-//   describe('container dies naturally', function() {
-//     beforeEach(function (done) {
-//       multi.createContainer(function (err, container, instance) {
-//         if (err) { return done(err); }
-//         ctx.instance = instance;
-//         ctx.container = container;
-//         expect(instance.attrs.container.inspect.State.Running).to.equal(true);
-//         done();
-//       });
-//     });
-//     describe('container die event handler', function() {
-//       beforeEach(function (done) {
-//         ctx.origHandleDie = dockerEvents.events.die;
-//         done();
-//       });
-//       afterEach(function (done) {
-//         dockerEvents.events.die = ctx.origHandleDie;
-//         done();
-//       });
-//       it('should recieve the docker die event', function (done) {
-//         container.events.die = done;
-//         var docker = new Docker(instance.attrs.container.dockerHost);
-//         docker.stopContainer(instance.attrs.container, done);
-//       });
-//     });
-//   });
-//   describe('user stops the instance\'s container', function() {
+  describe('container dies naturally', function() {
+    beforeEach(function (done) {
+      multi.createContainer(function (err, container, instance) {
+        if (err) { return done(err); }
+        ctx.instance = instance;
+        ctx.container = container;
+        expect(instance.attrs.container.inspect.State.Running).to.equal(true);
+        done();
+      });
+    });
+    describe('container die event handler', function() {
+      beforeEach(function (done) {
+        ctx.origHandleDie = dockerEvents.events.die;
+        done();
+      });
+      afterEach(function (done) {
+        dockerEvents.events.die = ctx.origHandleDie;
+        done();
+      });
+      it('should receive the docker die event', function (done) {
+        container.events.die = done;
+        var docker = new Docker(instance.attrs.container.dockerHost);
+        docker.stopContainer(instance.attrs.container, done);
+      });
+    });
+  });
+  describe('user stops the instance\'s container', function() {
 
-//   });
-// });
+  });
+});
