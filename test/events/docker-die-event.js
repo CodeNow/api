47168e846bb330d7b945000f8bfc1acb2c9a0d98
Anton Podviaznikov <podviaznikov@gmail.com>
2014-12-09 14:38:44 -0800

    New tests for the docker events

--- test/events/docker-die-event.js
+++ test/events/docker-die-event.js
@@ -6,16 +6,15 @@ var before = Lab.before;
 var after = Lab.after;
 var beforeEach = Lab.beforeEach;
 var afterEach = Lab.afterEach;
-
-var find = require('101/find');
-var hasKeypaths = require('101/has-keypaths');
-
+var expect = Lab.expect;
 var api = require('../fixtures/api-control');
 var dock = require('../fixtures/dock');
 var multi = require('../fixtures/multi-factory');
-var pubsub = require('models/redis/pubsub');
-
-
+var dockerEvents = require('models/events/docker');
+var Docker = require('models/apis/docker');
+var createCount = require('callback-count');
+var UserStoppedContainer = require('models/redis/user-stopped-container');
+var Instance = require('models/mongo/instance');
 
 describe('EVENT runnable:docker:events:die', function () {
   var ctx = {};
@@ -40,20 +39,108 @@ describe('EVENT runnable:docker:events:die', function () {
     describe('container die event handler', function() {
       beforeEach(function (done) {
         ctx.origHandleDie = dockerEvents.events.die;
+        ctx.originalUserStoppedContainerUnLock = UserStoppedContainer.prototype.unlock;
         done();
       });
       afterEach(function (done) {
         dockerEvents.events.die = ctx.origHandleDie;
+        UserStoppedContainer.prototype.unlock = ctx.originalUserStoppedContainerUnLock;
         done();
       });
+      afterEach(dockerEvents.close.bind(dockerEvents));
       it('should receive the docker die event', function (done) {
-        container.events.die = done;
-        var docker = new Docker(instance.attrs.container.dockerHost);
-        docker.stopContainer(instance.attrs.container, done);
+        var count = createCount(3, done);
+        dockerEvents.events.die = function (data) {
+          expect(data.id).to.equal(ctx.instance.attrs.container.inspect.Id);
+          expect(data.status).to.equal('die');
+          expect(data.from).to.equal('ubuntu:latest');
+          count.next();
+        };
+        dockerEvents.listen(count.next);
+        var docker = new Docker(ctx.instance.attrs.container.dockerHost);
+        docker.stopContainer(ctx.instance.attrs.container, count.next);
+      });
+
+      it('should update instance state in the mongo', function (done) {
+        var count = createCount(3, done);
+        var userStoppedContainer = new UserStoppedContainer(ctx.instance.attrs.container.inspect.Id);
+        UserStoppedContainer.prototype.unlock = function(cb) {
+          ctx.originalUserStoppedContainerUnLock.bind(userStoppedContainer)(function (err, success) {
+            ctx.instance.fetch(function (err, instance) {
+              if (err) { return done(err); }
+              expect(instance.container.inspect.State.Running).to.equal(false);
+              expect(instance.container.inspect.State.Pid).to.equal(0);
+              cb(err, success);
+              count.next();
+            });
+          });
+        };
+        dockerEvents.listen(count.next);
+        var docker = new Docker(ctx.instance.attrs.container.dockerHost);
+        docker.stopContainer(ctx.instance.attrs.container, count.next);
+      });
+
+    });
+
+    describe('user stops the instance\'s container', {timeout: 2000}, function() {
+      beforeEach(function (done) {
+        ctx.origHandleDie = dockerEvents.events.die;
+        ctx.origHandleDieGetEventLock = dockerEvents.getEventLock;
+        ctx.originalUserStoppedContainerLock = UserStoppedContainer.prototype.lock;
+        done();
+      });
+      afterEach(function (done) {
+        dockerEvents.events.die = ctx.origHandleDie;
+        dockerEvents.getEventLock = ctx.origHandleDieGetEventLock;
+        UserStoppedContainer.prototype.lock = ctx.originalUserStoppedContainerLock;
+        done();
+      });
+      afterEach(dockerEvents.close.bind(dockerEvents));
+
+      it('should receive the docker die event', function (done) {
+        var count = createCount(3, done);
+        dockerEvents.events.die = function (data) {
+          expect(data.id).to.equal(ctx.instance.attrs.container.inspect.Id);
+          expect(data.status).to.equal('die');
+          expect(data.from).to.equal('ubuntu:latest');
+          count.next();
+        };
+        dockerEvents.listen(count.next);
+        ctx.instance.stop(count.next);
+      });
+
+      it('should acquire event lock', function (done) {
+        var count = createCount(3, done);
+        dockerEvents.getEventLock = function (eventId, cb) {
+          expect(eventId).to.be.ok();
+          count.next();
+        };
+        dockerEvents.listen(count.next);
+        ctx.instance.stop(count.next);
+      });
+
+      it('should acquire user stopped container lock on user action', function (done) {
+        var count = createCount(4, done);
+        var userStoppedContainer = new UserStoppedContainer(ctx.instance.attrs.container.inspect.Id);
+        var lockCounter = 0;
+        UserStoppedContainer.prototype.lock = function(cb) {
+          ctx.originalUserStoppedContainerLock.bind(userStoppedContainer)(function (err, success) {
+            if (lockCounter === 0) {
+              expect(success).to.equal(true);
+            }
+            if (lockCounter === 1) {
+              expect(success).to.equal(false);
+            }
+            lockCounter++;
+            cb(err, success);
+            count.next();
+          });
+        };
+        dockerEvents.listen(count.next);
+        ctx.instance.stop(count.next);
       });
     });
-  });
-  describe('user stops the instance\'s container', function() {
+
 
   });
 });
