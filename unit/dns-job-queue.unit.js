37196b90dbabb953c30f0abd21779f3cdce15461
Casey Flynn <cflynn.us@gmail.com>
2014-12-11 16:05:22 -0800

    seeding test file

---
+++ unit/dns-job-queue.unit.js
@@ -0,0 +1,12 @@
+var Lab = require('lab');
+var describe = Lab.experiment;
+var it = Lab.test;
+var expect = Lab.expect;
+var before = Lab.before;
+var afterEach = Lab.afterEach;
+
+var dnsJobQueue = require('models/dns-job-queue');
+
+describe('dnsJobQueue', function () {
+
+});
