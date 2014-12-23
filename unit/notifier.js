8c19259459fa983a2a973b212e6b1d66cefdc08d
Anton Podviaznikov <podviaznikov@gmail.com>
2014-12-22 18:37:55 -0800

    more unit tests for the notifier

--- unit/notifier.js
+++ unit/notifier.js
@@ -11,6 +11,16 @@ var Notifier = require('models/notifications/notifier');
 
 describe('Notifier', function () {
 
+  it('should throw an error name was not provided', function (done) {
+    try {
+      var slack = new Notifier();
+      done(new Error('should throw an error'));
+    } catch (e) {
+      expect(e.message).to.equal('Please provide name for the notifier');
+      done();
+    }
+  });
+
   it('should throw an error if send was not implemented', function (done) {
     var slack = new Notifier('slack', {});
     var sendMethod = slack.send.bind(slack, 'some-text');
@@ -18,4 +28,13 @@ describe('Notifier', function () {
     done();
   });
 
+  it('should throw an error if tpls were not found', function (done) {
+    try {
+      var facebook = new Notifier('facebook', {});
+      done(new Error('should throw an error'));
+    } catch (e) {
+      expect(e.message).to.contain(['ENOENT, no such file or directory']);
+      done();
+    }
+  });
 });
