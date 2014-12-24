aec1c23cfa0bf42556915f90b14fef6725c7b969
Anton Podviaznikov <podviaznikov@gmail.com>
2014-12-23 18:27:18 -0800

    seetings endpoint start

---
+++ test/settings/post/201.js
@@ -0,0 +1,39 @@
+'use strict';
+var Lab = require('lab');
+var describe = Lab.experiment;
+var it = Lab.test;
+var before = Lab.before;
+var after = Lab.after;
+var api = require('../../fixtures/api-control');
+var dock = require('../../fixtures/dock');
+var multi = require('../../fixtures/multi-factory');
+
+
+describe('201 POST /settings', {timeout:500}, function () {
+  var ctx = {};
+
+  before(api.start.bind(ctx));
+  before(dock.start.bind(ctx));
+  before(require('../../fixtures/mocks/api-client').setup);
+  after(api.stop.bind(ctx));
+  after(dock.stop.bind(ctx));
+  after(require('../../fixtures/mocks/api-client').clean);
+
+
+  describe('create new settings', function () {
+
+    it('should be possible to create settings', function (done) {
+      multi.createRunnableClient(function (err, runnable) {
+        if (err) { return done(err); }
+        runnable.client.request.post(runnable.host + '/settings', {owner: {github: 1}}, function (err, resp, body) {
+          if (err) { return done(err); }
+          console.log('seetingssss', body, resp.status);
+          done();
+        });
+      });
+    });
+
+  });
+
+
+});
