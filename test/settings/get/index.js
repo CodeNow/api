0d0edbaaabb30b1d821d3ffbe837535b494a00b6
Anton Podviaznikov <podviaznikov@gmail.com>
2015-01-06 16:22:21 -0800

    Add method for fetching settings by owner

--- test/settings/get/index.js
+++ test/settings/get/index.js
@@ -50,52 +50,98 @@ describe('GET /settings', {timeout:500}, function () {
       });
     });
 
+    describe('get by owner', function () {
 
-    it('should be possible to fetch settings that were just created', function (done) {
-      ctx.user.fetchSetting(settingsId, function (err, body) {
-        if (err) { return done(err); }
-        expect(body._id).to.exist();
-        expect(body.owner.github).to.equal(settings.owner.github);
-        expect(body.notifications.slack.webhookUrl).to.equal(settings.notifications.slack.webhookUrl);
-        expect(body.notifications.hipchat.authToken).to.equal(settings.notifications.hipchat.authToken);
-        expect(body.notifications.hipchat.roomId).to.equal(settings.notifications.hipchat.roomId);
-        done();
+      it('should be possible to fetch settings that were just created by owner', function (done) {
+        var st = ctx.user.newSettings([], {qs: {owner: {github: settings.owner.github}}});
+        st.fetch(function (err, body) {
+          if (err) { return done(err); }
+          var settings = body[0];
+          expect(settings._id).to.exist();
+          expect(settings.owner.github).to.equal(settings.owner.github);
+          expect(settings.notifications.slack.webhookUrl).to.equal(settings.notifications.slack.webhookUrl);
+          expect(settings.notifications.hipchat.authToken).to.equal(settings.notifications.hipchat.authToken);
+          expect(settings.notifications.hipchat.roomId).to.equal(settings.notifications.hipchat.roomId);
+          done();
+        });
       });
-    });
 
-    it('should fail if another user wanted to fetch settings without permissions', function (done) {
-      require('../../fixtures/mocks/github/user-orgs')(ctx.user);
-      multi.createUser(function (err, runnable) {
-        if (err) { return done(err); }
-        runnable.fetchSetting(settingsId, function (err) {
+
+      it('should fail if owner id is not matching', function (done) {
+        require('../../fixtures/mocks/github/user-orgs')(ctx.user);
+        var st = ctx.user.newSettings([], {qs: {owner: {github: 9999}}});
+        st.fetch(function (err, body) {
           expect(err.output.payload.statusCode).to.equal(403);
           expect(err.output.payload.message).to.equal('Access denied (!owner)');
           done();
         });
       });
+
+      it('should fail if permissions check failed', function (done) {
+        require('../../fixtures/mocks/github/user-orgs')(ctx.user);
+        multi.createUser(function (err, runnable) {
+          if (err) { return done(err); }
+          var st = runnable.newSettings([], {qs: {owner: {github: settings.owner.github}}});
+          st.fetch(function (err, body) {
+            expect(err.output.payload.statusCode).to.equal(403);
+            expect(err.output.payload.message).to.equal('Access denied (!owner)');
+            done();
+          });
+        });
+      });
+
     });
 
-    it('should return 404 for fake settings-id', function (done) {
-      multi.createUser(function (err, runnable) {
-        if (err) { return done(err); }
-        runnable.fetchSetting('507f1f77bcf86cd799439011', function (err) {
-          expect(err.data.statusCode).to.equal(404);
-          expect(err.data.message).to.equal('Setting not found');
+
+    describe('get by id', function () {
+
+      it('should be possible to fetch settings that were just created', function (done) {
+        ctx.user.fetchSetting(settingsId, function (err, body) {
+          if (err) { return done(err); }
+          expect(body._id).to.exist();
+          expect(body.owner.github).to.equal(settings.owner.github);
+          expect(body.notifications.slack.webhookUrl).to.equal(settings.notifications.slack.webhookUrl);
+          expect(body.notifications.hipchat.authToken).to.equal(settings.notifications.hipchat.authToken);
+          expect(body.notifications.hipchat.roomId).to.equal(settings.notifications.hipchat.roomId);
           done();
         });
       });
-    });
 
-    it('should return 400 for non-objectId settings-id', function (done) {
-      multi.createUser(function (err, runnable) {
-        if (err) { return done(err); }
-
-        runnable.fetchSetting('fake-id', function (err) {
-          expect(err.data.statusCode).to.equal(400);
-          expect(err.data.message).to.equal('url parameter \"id\" is not an ObjectId');
-          done();
+      it('should fail if another user wanted to fetch settings without permissions', function (done) {
+        require('../../fixtures/mocks/github/user-orgs')(ctx.user);
+        multi.createUser(function (err, runnable) {
+          if (err) { return done(err); }
+          runnable.fetchSetting(settingsId, function (err) {
+            expect(err.output.payload.statusCode).to.equal(403);
+            expect(err.output.payload.message).to.equal('Access denied (!owner)');
+            done();
+          });
         });
       });
+
+      it('should return 404 for fake settings-id', function (done) {
+        multi.createUser(function (err, runnable) {
+          if (err) { return done(err); }
+          runnable.fetchSetting('507f1f77bcf86cd799439011', function (err) {
+            expect(err.data.statusCode).to.equal(404);
+            expect(err.data.message).to.equal('Setting not found');
+            done();
+          });
+        });
+      });
+
+      it('should return 400 for non-objectId settings-id', function (done) {
+        multi.createUser(function (err, runnable) {
+          if (err) { return done(err); }
+
+          runnable.fetchSetting('fake-id', function (err) {
+            expect(err.data.statusCode).to.equal(400);
+            expect(err.data.message).to.equal('url parameter \"id\" is not an ObjectId');
+            done();
+          });
+        });
+      });
+
     });
 
   });
