86add17c16fc87ea04332ff136dc6da9b798d348
Casey Flynn <cflynn.us@gmail.com>
2015-01-08 16:08:58 -0800

    new mocks & tests

--- test/actions-analyze.js
+++ test/actions-analyze.js
@@ -13,6 +13,7 @@ var hooks = require('./fixtures/analyze-hooks');
 var multi = require('./fixtures/multi-factory');
 var nock = require('nock');
 var repoMock = require('./fixtures/mocks/github/repo');
+var repoContentsMock = require('./fixtures/mocks/github/repos-contents');
 
 before(function (done) {
   nock('http://runnable.com:80')
@@ -50,12 +51,28 @@ describe('Analyze - /actions/analyze', function () {
           done();
       });
     });
-
   });
 
   describe('Success conditions', function () {
-    it('Returns inferred suggestions for JavaScript/NodeJS repository', function (done) {
-      done();
+    it('Returns 0 inferred suggestions for JavaScript/NodeJS '+
+       'repository with no dependencies', function (done) {
+      var package = {
+        dependencies: []
+      };
+      repoContentsMock.repoContentsDirectory();
+      repoContentsMock.repoContentsFile({
+        name: 'package.json',
+        path: 'package.json',
+        content: (new Buffer(JSON.stringify(package).toString('base64')))
+      });
+      console.log('hooks.getSuccess', hooks.getSuccess);
+      ctx.request.get(
+        hooks.getSuccess,
+        function (err, res) {
+          expect(res.statusCode).to.equal(200);
+          done();
+        }
+      );
     });
   });
 });
