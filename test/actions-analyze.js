8adb9b467a05839c6b238ba6aeeb12f97cf9938b
Casey Flynn <cflynn.us@gmail.com>
2015-01-14 20:47:29 -0800

    use find instead of filter for improved performance (fewer operations)

--- test/actions-analyze.js
+++ test/actions-analyze.js
@@ -42,7 +42,6 @@ describe('Analyze - /actions/analyze', function () {
       done();
     });
   });
-  //afterEach(require('./fixtures/clean-mongo').removeEverything);
   afterEach(require('./fixtures/clean-ctx')(ctx));
 
   describe('Error conditions', function () {
@@ -91,7 +90,6 @@ describe('Analyze - /actions/analyze', function () {
           done();
         }
       );
-
     });
   });
 
