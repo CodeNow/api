6dec6a3b61466529e04d0d3e5409dc8459a0bd86
Casey Flynn <cflynn.us@gmail.com>
2015-01-21 15:24:34 -0800

    unused code block remove

--- test/actions-analyze-info/get/200.js
+++ test/actions-analyze-info/get/200.js
@@ -11,21 +11,6 @@ var api = require('../../fixtures/api-control');
 var generateKey = require('../../fixtures/key-factory');
 var hooks = require('../../fixtures/analyze-info-hooks');
 var multi = require('../../fixtures/multi-factory');
-var nock = require('nock');
-
-/*
-var supportedLanguageVersionsNode = require('../lib/routes/actions/analyze/data/supported-language-versions-nodejs');
-var supportedLanguageVersionsPython = require('../lib/routes/actions/analyze/data/supported-language-versions-python');
-var supportedLanguageVersionsRuby = require('../lib/routes/actions/analyze/data/supported-language-versions-ruby');
-*/
-
-before(function (done) {
-  nock('http://runnable.com:80')
-    .persist()
-    .get('/')
-    .reply(200);
-  done();
-});
 
 describe('Analyze - /actions/analyze/info', function () {
   var ctx = {};
