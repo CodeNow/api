b0b61cd9cb653b884096ac4a17d0792aa3b6fb40
Anton Podviaznikov <podviaznikov@gmail.com>
2015-02-10 11:49:09 -0800

    add more unit tests and mocks for github pr handling

--- unit/github-notifier.js
+++ unit/github-notifier.js
@@ -11,8 +11,8 @@ var repoMock = require('../test/fixtures/mocks/github/repo');
 var isCollaboratorMock =
   require('../test/fixtures/mocks/github/repos-username-repo-collaborators-collaborator');
 
-// var userMembershipMock =
-//   require('../test/fixtures/mocks/github/user-memberships-org');
+var userMembershipMock =
+  require('../test/fixtures/mocks/github/user-memberships-org');
 
 
 describe('GitHub Notifier',  function () {
@@ -146,6 +146,16 @@ describe('GitHub Notifier',  function () {
       });
     });
 
+    it('should be success for org public repo', function (done) {
+      var github = new GitHub();
+      repoMock.standardRepo({});
+      github._ensurePermissions('cflynn07/clubbingowl_brochure', 'cflynn07', function (err, resp) {
+        if (err) { return done(err); }
+        expect(resp).to.be.undefined();
+        done();
+      });
+    });
+
     it('should fail for for user\s private repo without configured collaborator', function (done) {
       var github = new GitHub();
       repoMock.privateRepo({});
@@ -163,6 +173,37 @@ describe('GitHub Notifier',  function () {
       repoMock.privateRepo({});
       isCollaboratorMock.isCollaborator('cflynn07', 'private_clubbingowl_brochure', 'runnabot');
       github._ensurePermissions('cflynn07/private_clubbingowl_brochure', null, function (err, resp) {
+        if (err) { return done(err); }
+        expect(resp).to.be.undefined();
+        done();
+      });
+    });
+
+    it('should try to accept membership for org private repo', function (done) {
+      var github = new GitHub();
+      repoMock.privateRepo({});
+      userMembershipMock.pendingMember(11, 'runnabot', 'cflynn07');
+      github._ensurePermissions('cflynn07/private_clubbingowl_brochure', 'cflynn07', function (err) {
+        expect(err.message).to.match(/No match for request patch/);
+        done();
+      });
+    });
+
+    it('should try to accept membership for org private repo', function (done) {
+      var github = new GitHub();
+      repoMock.privateRepo({});
+      userMembershipMock.notMember(11, 'runnabot', 'cflynn07');
+      github._ensurePermissions('cflynn07/private_clubbingowl_brochure', 'cflynn07', function (err) {
+        expect(err.message).to.match(/No match for request patch/);
+        done();
+      });
+    });
+
+    it('should work for org repo where runnabot is member', function (done) {
+      var github = new GitHub();
+      repoMock.privateRepo({});
+      userMembershipMock.isMember(11, 'runnabot', 'cflynn07');
+      github._ensurePermissions('cflynn07/private_clubbingowl_brochure', 'cflynn07', function (err, resp) {
         if (err) { return done(err); }
         expect(resp).to.be.undefined();
         done();
--- unit/github.js
+++ unit/github.js
@@ -13,6 +13,9 @@ var isCollaboratorMock =
 var userMembershipMock =
   require('../test/fixtures/mocks/github/user-memberships-org');
 
+var prsMock =
+  require('../test/fixtures/mocks/github/repos-username-repo-pulls');
+
 describe('GitHub API', function () {
 
 
@@ -20,6 +23,7 @@ describe('GitHub API', function () {
 
     it('should get one pr branch', function (done) {
       var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
+      prsMock.openPulls('podviaznikov', 'hellonode', 'test');
       github.listOpenPullRequestsForBranch('podviaznikov/hellonode', 'test', function (err, prs) {
         if (err) { return done(err); }
         expect(prs.length).to.equal(1);
@@ -30,6 +34,7 @@ describe('GitHub API', function () {
 
     it('should get 0 prs for master branch', function (done) {
       var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
+      prsMock.openPulls('podviaznikov', 'hellonode', 'test');
       github.listOpenPullRequestsForBranch('podviaznikov/hellonode', 'master', function (err, prs) {
         if (err) { return done(err); }
         expect(prs.length).to.equal(0);
@@ -39,92 +44,91 @@ describe('GitHub API', function () {
   });
 
 
-  describe('mocked API', function () {
 
-    describe('isPublicRepo', function () {
+  describe('isPublicRepo', function () {
 
-      it('should return true for the public repo', function (done) {
-        repoMock.standardRepo({});
-        var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
-        github.isPublicRepo('cflynn07/clubbingowl_brochure', function (err, isPublic) {
-          if (err) { return done(err); }
-          expect(isPublic).to.be.true();
-          done();
-        });
+    it('should return true for the public repo', function (done) {
+      repoMock.standardRepo({});
+      var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
+      github.isPublicRepo('cflynn07/clubbingowl_brochure', function (err, isPublic) {
+        if (err) { return done(err); }
+        expect(isPublic).to.be.true();
+        done();
       });
-
-      it('should return false for the private repo', function (done) {
-        repoMock.privateRepo({});
-        var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
-        github.isPublicRepo('cflynn07/private_clubbingowl_brochure', function (err, isPublic) {
-          if (err) { return done(err); }
-          expect(isPublic).to.be.false();
-          done();
-        });
-      });
-
     });
 
-    describe('isCollaborator', function () {
-
-      it('should return true if user is collaborator', function (done) {
-        isCollaboratorMock.isCollaborator('podviaznikov', 'hellonode', 'runnabot');
-        var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
-        github.isCollaborator('podviaznikov/hellonode', 'runnabot', function (err, isCollaborator) {
-          if (err) { return done(err); }
-          expect(isCollaborator).to.be.true();
-          done();
-        });
+    it('should return false for the private repo', function (done) {
+      repoMock.privateRepo({});
+      var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
+      github.isPublicRepo('cflynn07/private_clubbingowl_brochure', function (err, isPublic) {
+        if (err) { return done(err); }
+        expect(isPublic).to.be.false();
+        done();
       });
-
-      it('should return false if user is not collaborator', function (done) {
-        isCollaboratorMock.notCollaborator('podviaznikov', 'hellonode', 'tj');
-        var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
-        github.isCollaborator('podviaznikov/hellonode', 'tj', function (err, isCollaborator) {
-          if (err) { return done(err); }
-          expect(isCollaborator).to.be.false();
-          done();
-        });
-      });
-
-    });
-
-
-    describe('isOrgMember', function () {
-
-      it('should return true if user is an org members', function (done) {
-        userMembershipMock.isMember(1, 'runnabot', 'CodeNow');
-        var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
-        github.isOrgMember('CodeNow', function (err, isMember) {
-          if (err) { return done(err); }
-          expect(isMember).to.be.true();
-          done();
-        });
-      });
-
-      it('should return false if user is a pending org member', function (done) {
-        userMembershipMock.pendingMember(2, 'runnabot', 'Runnable');
-        var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
-        github.isOrgMember('Runnable', function (err, isMember) {
-          if (err) { return done(err); }
-          expect(isMember).to.be.false();
-          done();
-        });
-      });
-
-      it('should return false if user is not an org member', function (done) {
-        userMembershipMock.notMember(3, 'runnabot', 'hashobject');
-        var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
-        github.isOrgMember('hashobject', function (err, isMember) {
-          if (err) { return done(err); }
-          expect(isMember).to.be.false();
-          done();
-        });
-      });
-
     });
 
   });
+
+  describe('isCollaborator', function () {
+
+    it('should return true if user is collaborator', function (done) {
+      isCollaboratorMock.isCollaborator('podviaznikov', 'hellonode', 'runnabot');
+      var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
+      github.isCollaborator('podviaznikov/hellonode', 'runnabot', function (err, isCollaborator) {
+        if (err) { return done(err); }
+        expect(isCollaborator).to.be.true();
+        done();
+      });
+    });
+
+    it('should return false if user is not collaborator', function (done) {
+      isCollaboratorMock.notCollaborator('podviaznikov', 'hellonode', 'tj');
+      var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
+      github.isCollaborator('podviaznikov/hellonode', 'tj', function (err, isCollaborator) {
+        if (err) { return done(err); }
+        expect(isCollaborator).to.be.false();
+        done();
+      });
+    });
+
+  });
+
+
+  describe('isOrgMember', function () {
+
+    it('should return true if user is an org members', function (done) {
+      userMembershipMock.isMember(1, 'runnabot', 'CodeNow');
+      var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
+      github.isOrgMember('CodeNow', function (err, isMember) {
+        if (err) { return done(err); }
+        expect(isMember).to.be.true();
+        done();
+      });
+    });
+
+    it('should return false if user is a pending org member', function (done) {
+      userMembershipMock.pendingMember(2, 'runnabot', 'Runnable');
+      var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
+      github.isOrgMember('Runnable', function (err, isMember) {
+        if (err) { return done(err); }
+        expect(isMember).to.be.false();
+        done();
+      });
+    });
+
+    it('should return false if user is not an org member', function (done) {
+      userMembershipMock.notMember(3, 'runnabot', 'hashobject');
+      var github = new GitHub({token: process.env.RUNNABOT_GITHUB_ACCESS_TOKEN});
+      github.isOrgMember('hashobject', function (err, isMember) {
+        if (err) { return done(err); }
+        expect(isMember).to.be.false();
+        done();
+      });
+    });
+
+  });
+
+
 
 
 });
