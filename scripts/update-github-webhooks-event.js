96df07798ea9b3c9e74936128ebbda2bd86b7973
Anton Podviaznikov <podviaznikov@gmail.com>
2015-02-17 11:54:16 -0800

    updated migration script

--- scripts/update-github-webhooks-event.js
+++ scripts/update-github-webhooks-event.js
@@ -17,13 +17,37 @@ function findAllRepos(cb) {
 }
 
 
+var allErrors = [];
+
 function findUsersForRepos(repos, cb) {
   debug('findUsersForRepos', 'total repos num:', repos.length);
   async.map(repos, function (repo, callback) {
     User.findByGithubId(repo.creators[0], function (err, user) {
       if (err) { return callback(err); }
       repo.user = user;
-      callback(null, repo);
+      if (!user) {
+        if (!repo.creators[1]) {
+          return callback(null, repo);
+        }
+        User.findByGithubId(repo.creators[1], function (err, user) {
+          if (err) { return callback(err); }
+          repo.user = user;
+          if (!user) {
+            if (!repo.creators[2]) {
+              return callback(null, repo);
+            }
+            User.findByGithubId(repo.creators[2], function (err, user) {
+              if (err) { return callback(err); }
+              repo.user = user;
+              callback(null, repo);
+            });
+          } else {
+            callback(null, repo);
+          }
+        });
+      } else {
+        callback(null, repo);
+      }
     });
   }, cb);
 }
@@ -31,16 +55,35 @@ function findUsersForRepos(repos, cb) {
 
 function updateHooksEvents(repos, cb) {
   debug('updateHooksEvents', 'total repos num:', repos.length);
-  async.mapLimit(repos, function(repo, callback) {
+  async.mapLimit(repos, 50, function(repo, callback) {
+    console.log('processing repo', repo);
+    if (!repo.user) {
+      console.log('user not found for the repo', repo);
+      return callback();
+    }
     var github = new GitHub({token: repo.user.accounts.github.accessToken});
     // this will actually update hook (not just create if missing)
-    github.createRepoHookIfNotAlready(repo._id, callback);
-  }, 10, cb);
+    github.createRepoHookIfNotAlready(repo._id, function (err) {
+      if (err) {
+        allErrors.push(err);
+        if(err.output.statusCode === 404) {
+          console.log('repos not found. just skip it', repo);
+          callback(null);
+        } else if(err.output.statusCode === 502) {
+          console.log('access token removed. just skip it', repo);
+          callback(null);
+        } else {
+          callback(err);
+        }
+      }
+      callback(null);
+    });
+  }, cb);
 }
 
 function finish (err, results) {
   console.log('DONE: err?', err);
-  console.log('all results', results);
+  console.log('all errors', allErrors);
   process.exit();
 }
 async.waterfall([
