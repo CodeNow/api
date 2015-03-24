c2005892bc37fc7b768bf84879b0a26237d2c186
Bryan Kendall <bryan@runnable.com>
2015-03-24 15:46:44 -0700

    remove cayley from starting w/ tests

--- test/fixtures/api-control.js
+++ test/fixtures/api-control.js
@@ -3,7 +3,6 @@ var route53 = require('./route53');
 route53.start(); // must be before api require
 var api = require('../../app');
 var cleanMongo = require('./clean-mongo');
-var graph = require('./graph');
 var exec = require('child_process').exec;
 
 module.exports = {
@@ -35,17 +34,13 @@ var started = false;
 function startApi (done) {
   if(started) { return done(); }
   started = true;
-  var ctx = this;
-  ctx.graph = graph;
   route53.start(); // must be before api require, and here
   api.start(function (err) {
     if (err) { return done(err); }
-    graph.start(function (err) {
+    if (err) { return done(err); }
+    cleanMongo.removeEverything(function (err) {
       if (err) { return done(err); }
-      cleanMongo.removeEverything(function (err) {
-        if (err) { return done(err); }
-        ensureIndexes(done);
-      });
+      ensureIndexes(done);
     });
   });
 }
@@ -56,7 +51,7 @@ function stopApi (done) {
   route53.stop();
   api.stop(function (err) {
     if (err) { return done(err); }
-    graph.stop(done);
+    done();
     // cleanMongo.dropDatabase(done);
   });
 }
