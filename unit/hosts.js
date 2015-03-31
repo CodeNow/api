0413ad4845b33d22cc6632524860391190908b15
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-03-31 15:22:56 -0700

    GET instances by url->host, added githubUsername query support

--- unit/hosts.js
+++ unit/hosts.js
@@ -10,52 +10,64 @@ var expect = Code.expect;
 var Hosts = require('models/redis/hosts');
 
 describe('Hosts',  function () {
-  it('should parse a username from a url', function (done) {
-    var url = 'http://instance-name-org-name.'+process.env.USER_CONTENT_DOMAIN;
-    var hosts = new Hosts();
-    var name = 'instance-name';
-    hosts.parseUsernameFromUrl(url, name, function (err, username) {
-      if (err) { return done(err); }
-      expect(username).to.equal('org-name');
-      done();
+
+  describe('parseInstanceNameFromHostname', function() {
+
+    it('should parse a username from a hostname', function (done) {
+      var hostname = 'instance-name-org-name.'+process.env.USER_CONTENT_DOMAIN;
+      var hosts = new Hosts();
+      var username = 'org-name';
+      hosts.parseInstanceNameFromHostname(hostname, username, function (err, instanceName) {
+        if (err) { return done(err); }
+        expect(instanceName).to.equal('instance-name');
+        done();
+      });
     });
   });
-  describe('errors', function() {
-    describe('url is not url', function() {
-      expectError({
-        url: 'bogus',
-        name: 'name'
+
+  describe('parseUsernameFromHostname', function() {
+
+    it('should parse a username from a hostname', function (done) {
+      var hostname = 'instance-name-org-name.'+process.env.USER_CONTENT_DOMAIN;
+      var hosts = new Hosts();
+      var name = 'instance-name';
+      hosts.parseUsernameFromHostname(hostname, name, function (err, username) {
+        if (err) { return done(err); }
+        expect(username).to.equal('org-name');
+        done();
       });
     });
-    describe('url is does not end with user content domain', function() {
-      expectError({
-        url: 'http://hello.'+process.env.USER_CONTENT_DOMAIN+'.com',
-        name: 'name'
-      });
-    });
-    describe('url is does not contain a subdomain', function() {
-      expectError({
-        url: 'http://bogus.com',
-        name: 'name'
-      });
-    });
-    describe('url is does not contain name', function() {
-      expectError({
-        url: 'bogus.com',
-        name: 'name'
-      });
-    });
-    function expectError (args) {
-      it('should callback "invalid url" error', function (done) {
-        var url = args.url;
-        var name = args.name;
-        var hosts = new Hosts();
-        hosts.parseUsernameFromUrl(url, name, function (err) {
-          expect(err).to.exist();
-          expect(err.message).to.match(/invalid url/i);
-          done();
+    describe('errors', function() {
+      describe('hostname is does not end with user content domain', function() {
+        expectError({
+          hostname: 'hello-codenow.otherdomain.com',
+          name: 'name'
         });
       });
-    }
+      describe('hostname is does not contain a subdomain', function() {
+        expectError({
+          hostname: 'bogus.com',
+          name: 'name'
+        });
+      });
+      describe('hostname is does not contain name', function() {
+        expectError({
+          hostname: 'bogus.com',
+          name: 'name'
+        });
+      });
+      function expectError (args) {
+        it('should callback "invalid hostname" error', function (done) {
+          var hostname = args.hostname;
+          var name = args.name;
+          var hosts = new Hosts();
+          hosts.parseUsernameFromHostname(hostname, name, function (err) {
+            expect(err).to.exist();
+            expect(err.message).to.match(/invalid hostname/i);
+            done();
+          });
+        });
+      }
+    });
   });
 });
