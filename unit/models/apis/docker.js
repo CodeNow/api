32cba11dffad9ee82bd3039ecfabddf1cf25843b
Ryan Sandor Richards <ryan@runnable.com>
2015-04-03 16:32:02 -0700

    Fixed linting on new tests, use noop.

--- unit/models/apis/docker.js
+++ unit/models/apis/docker.js
@@ -2,19 +2,17 @@ var Lab = require('lab');
 var lab = exports.lab = Lab.script();
 var describe = lab.describe;
 var it = lab.it;
-var before = lab.before;
-var beforeEach = lab.beforeEach;
-var after = lab.after;
 var afterEach = lab.afterEach;
 var Code = require('code');
 var expect = Code.expect;
 var sinon = require('sinon');
+var noop = require('101/noop');
 
 require('loadenv')();
 var Docker = require('models/apis/docker.js');
 
 describe('docker', function() {
-  var model = new Docker('http://fake.host.com');;
+  var model = new Docker('http://fake.host.com');
 
   afterEach(function (done) {
     model.startContainer.restore();
@@ -22,21 +20,21 @@ describe('docker', function() {
   });
 
   it('should not include charon if env variable is not set', function (done) {
-    sinon.stub(model, 'startContainer', function (container, opts, cb) {
+    sinon.stub(model, 'startContainer', function (container, opts) {
       expect(opts.Dns.length).to.equal(2);
       done();
     });
-    model.startUserContainer({}, {}, function() {});
+    model.startUserContainer({}, {}, noop);
   });
 
   it('should include charon as the first dns when evn is set', function (done) {
     var host = process.env.CHARON_HOST = '10.10.10.10';
-    sinon.stub(model, 'startContainer', function (container, opts, cb) {
+    sinon.stub(model, 'startContainer', function (container, opts) {
       expect(opts.Dns.length).to.equal(3);
       expect(opts.Dns[0]).to.equal(host);
       delete process.env.CHARON_HOST;
       done();
     });
-    model.startUserContainer({}, {}, function() {});
+    model.startUserContainer({}, {}, noop);
   });
 });
