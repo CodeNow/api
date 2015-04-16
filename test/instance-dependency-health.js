8c8370ffaf9646031528e56d844be6acb2c42946
Bryan Kendall <bryan@runnable.com>
2015-04-16 16:20:07 -0700

    adding route to get node count of instances and serve as health check

---
+++ test/instance-dependency-health.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var before = lab.before;
+var beforeEach = lab.beforeEach;
+var after = lab.after;
+var afterEach = lab.afterEach;
+var Code = require('code');
+var expect = Code.expect;
+
+var api = require('./fixtures/api-control');
+var primus = require('./fixtures/primus');
+
+var request = require('request');
+
+describe('GET /instances', function () {
+  var ctx = {};
+
+  before(api.start.bind(ctx));
+  beforeEach(primus.connect);
+  afterEach(primus.disconnect);
+  after(api.stop.bind(ctx));
+
+  before({ timeout: 7000 }, function (done) {
+    var Graph = require('models/graph/neo4j');
+    var client = new Graph();
+    var err;
+    client.cypher('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n, r')
+      .on('error', function (e) { err = e; })
+      .on('end', function () { done(err); })
+      .on('data', function () {});
+  });
+
+  describe('GET /instance/_dependencies_health', function () {
+    it('should tell us how many instances are in the graph', function (done) {
+      request.get(process.env.FULL_API_DOMAIN + '/instances/_dependencies_health', function (err, res, body) {
+        expect(err).to.be.null();
+        expect(body).to.equal('0');
+
+        done(err);
+      });
+    });
+  });
+
+});
