ed9aa72249c3b135ebfd1378a0b40df5aaaf400a
Bryan Kendall <bryan@runnable.com>
2015-04-24 11:03:14 -0700

    adding token route test w/ whitelist logic

---
+++ test/auth-github/token.js
@@ -0,0 +1,122 @@
+'use strict';
+require('loadenv')();
+
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var expect = require('code').expect;
+var it = lab.it;
+var after = lab.after;
+var before = lab.before;
+var beforeEach = lab.beforeEach;
+
+var request = require('request');
+
+var api = require('../fixtures/api-control');
+var url = require('url');
+var querystring = require('querystring');
+var uuid = require('uuid');
+
+describe('/auth/github with whitelist', function () {
+  var ctx = {};
+  var baseUrl = 'http://' + process.env.ROOT_DOMAIN + '/auth/github/';
+  before(function (done) {
+    process.env.ENABLE_USER_WHITELIST = true;
+    done();
+  });
+  before(api.start.bind(ctx));
+  after(api.stop.bind(ctx));
+  after(function (done) {
+    delete process.env.ENABLE_USER_WHITELIST;
+    done();
+  });
+
+  describe('user not in the whitelist', function () {
+    var tokenUrl = baseUrl + 'token';
+    beforeEach(function (done) {
+      ctx.username = uuid();
+      ctx.testToken = uuid();
+      done();
+    });
+
+    it('should not let the user authenticate', function (done) {
+      require('../fixtures/mocks/github/user')(1000, ctx.username, ctx.testToken);
+      require('../fixtures/mocks/github/user-emails')();
+      request.post({
+        url: tokenUrl,
+        json: true,
+        body: { accessToken: ctx.testToken },
+        qs: { username: ctx.username },
+        followRedirect: false
+      }, function (err, res) {
+        if (err) { return done(err); }
+        expect(res.statusCode).to.equal(302);
+        var qs = querystring.parse(url.parse(res.headers.location).query);
+        expect(qs).to.contain({ whitelist: 'false' });
+        done();
+      });
+    });
+  });
+
+  describe('user in the whitelist', function () {
+    var tokenUrl = baseUrl + 'token';
+    before(function (done) {
+      ctx.username = uuid();
+      ctx.testToken = uuid();
+      var Whitelist = require('models/mongo/user-whitelist');
+      var w = new Whitelist({
+        name: ctx.username,
+        allowed: true
+      });
+      w.save(done);
+    });
+
+    it('should let the user authenticate', function (done) {
+      require('../fixtures/mocks/github/user')(1000, ctx.username, ctx.testToken);
+      require('../fixtures/mocks/github/user-emails')();
+      request.post({
+        url: tokenUrl,
+        json: true,
+        body: { accessToken: ctx.testToken },
+        qs: { username: ctx.username },
+        followRedirect: false
+      }, function (err, res) {
+        if (err) { return done(err); }
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+
+  describe('user in an org in the whitelist', function () {
+    var tokenUrl = baseUrl + 'token';
+    before(function (done) {
+      ctx.orgname = uuid();
+      ctx.username = uuid();
+      ctx.testToken = uuid();
+      var Whitelist = require('models/mongo/user-whitelist');
+      var w = new Whitelist({
+        name: ctx.orgname,
+        allowed: true
+      });
+      w.save(done);
+    });
+
+    it('should let the user authenticate', function (done) {
+      require('../fixtures/mocks/github/user')(1000, ctx.username, ctx.testToken);
+      require('../fixtures/mocks/github/user-emails')();
+      require('../fixtures/mocks/github/user-orgs')(1001, ctx.orgname);
+      request.post({
+        url: tokenUrl,
+        json: true,
+        body: { accessToken: ctx.testToken },
+        qs: { username: ctx.orgname },
+        followRedirect: false
+      }, function (err, res) {
+        if (err) { return done(err); }
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+});
