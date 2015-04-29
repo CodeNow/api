7898cc25a6640525a3cb93d4f95056515dcbd7da
Anton Podviaznikov <podviaznikov@gmail.com>
2015-04-29 16:35:43 -0700

    more tests

--- unit/sockets/messenger.js
+++ unit/sockets/messenger.js
@@ -10,6 +10,7 @@ var it = lab.it;
 var Code = require('code');
 var expect = Code.expect;
 
+var User = require('models/mongo/user');
 var Messenger = require('socket/messenger');
 
 describe('Messenger', function () {
@@ -62,6 +63,43 @@ describe('Messenger', function () {
         expect(canJoin).to.be.true();
         done();
       });
+    });
+  });
+  it('should return error if user search callbacks with error', function (done) {
+    var socket = {
+      request: {
+        session: {
+          passport: {
+            user: 'some-user-id'
+          }
+        }
+      }
+    };
+    sinon.stub(User, 'findByGithubId').yields(new Error('Mongoose error'));
+    Messenger.canJoin(socket, 'some-id', { name: 'some-org-id' }, function (err, canJoin) {
+      expect(err.message).to.equal('Mongoose error');
+      expect(canJoin).to.be.undefined();
+      User.findByGithubId.restore();
+      done();
+    });
+  });
+  it('should return error if user not found', function (done) {
+    var socket = {
+      request: {
+        session: {
+          passport: {
+            user: 'some-user-id'
+          }
+        }
+      }
+    };
+    sinon.stub(User, 'findByGithubId').yields(null, null);
+    Messenger.canJoin(socket, 'some-id', { name: 'some-org-id' }, function (err, canJoin) {
+      expect(err.output.statusCode).to.equal(404);
+      expect(canJoin).to.be.undefined();
+      expect(err.output.payload.message).to.equal('User not found');
+      User.findByGithubId.restore();
+      done();
     });
   });
 });
