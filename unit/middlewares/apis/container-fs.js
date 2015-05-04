2b70dfebee59ea704cb4249559098619d4ac0b29
Anton Podviaznikov <podviaznikov@gmail.com>
2015-05-04 15:52:44 -0700

    add tests

---
+++ unit/middlewares/apis/container-fs.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var sinon = require('sinon');
+var noop = require('101/noop');
+var Boom = require('dat-middleware').Boom;
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var Code = require('code');
+var expect = Code.expect;
+
+var containerFs = require('middlewares/apis/container-fs');
+
+describe('container-fs', function () {
+  describe('#parseBody', function () {
+    it('should parse pathname correctly for dir', function (done) {
+      var req = {
+        body: {
+          isDir: true,
+          name: 'hellonode',
+          path: '/'
+        },
+        params: {}
+      };
+      var res = {};
+      containerFs.parseBody(req, res, function () {
+        expect(req.params.path).to.equal('/hellonode/');
+        expect(req.params.content).to.equal('');
+        expect(req.params.isDir).to.equal(true);
+        done();
+      });
+    });
+
+  });
+});
