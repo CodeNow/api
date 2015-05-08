fa8ec56cb9161df9143b4632ed48ffe1af437685
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-05-08 14:31:29 -0700

    test for fetch instance by id

--- test/instances-id/get/index.js
+++ test/instances-id/get/index.js
@@ -83,6 +83,12 @@ describe('Instance - /instances/:id', function () {
       };
       ctx.instance.fetch(expects.success(200, expected, done));
     });
+    it('should fetch by _id', function(done) {
+      var expected = {
+        'containers[0].inspect.State.Running': true
+      };
+      ctx.user.fetchInstance(ctx.instance.attrs._id, expects.success(200, expected, done));
+    });
     describe('permissions', function () {
       describe('public', function () {
         beforeEach(function (done) {
