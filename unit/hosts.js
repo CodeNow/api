b0e2328d171bc86738b9bcbe3469202488360263
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-05-13 11:57:17 -0700

    fix unit test

--- unit/hosts.js
+++ unit/hosts.js
@@ -9,6 +9,8 @@ var afterEach = lab.afterEach;
 var Code = require('code');
 var expect = Code.expect;
 var keypather = require('keypather')();
+var sinon = require('sinon');
+var Dns = require('models/apis/dns');
 
 require('loadenv')();
 
@@ -21,13 +23,15 @@ describe('Hosts', function () {
       ctx.hosts = new Hosts();
       ctx.port = '80/tcp';
       ctx.instance = {};
+      sinon.stub(Dns.prototype, 'putEntryForInstance').yieldsAsync();
+      sinon.stub(Dns.prototype, 'deleteEntryForInstance').yieldsAsync();
       keypather.set(ctx.instance, 'container.dockerHost', 'http://10.0.0.1:4242');
       keypather.set(ctx.instance, 'container.ports["80/tcp"][0].HostPort', 49201);
       keypather.set(ctx.instance, 'network.hostIp', '10.6.4.1');
-      ctx.branch = 'somebranch';
+      ctx.branch = 'some-branch';
       keypather.set(ctx.instance, 'contextVersion.appCodeVersions[0].lowerBranch', ctx.branch);
 
-      ctx.instanceName = 'instance-name';
+      ctx.instanceName = 'some-branch-instance-name';
       ctx.username = 'user-name';
       ctx.hosts.upsertHostsForInstance(
         ctx.username, ctx.instance, ctx.instanceName, ctx.instance.container, done);
@@ -36,15 +40,20 @@ describe('Hosts', function () {
       ctx.hosts.removeHostsForInstance(
         ctx.username, ctx.instance, ctx.instanceName, ctx.instance.container, done);
     });
+    afterEach(function (done) {
+      Dns.prototype.putEntryForInstance.restore();
+      Dns.prototype.deleteEntryForInstance.restore();
+      done();
+    });
 
     it('should parse a username from a hostname', function (done) {
       var hostname = [
-        ctx.branch, '-', ctx.instanceName, '-staging-', ctx.username, '.',
+        ctx.instanceName, '-staging-', ctx.username, '.',
         process.env.USER_CONTENT_DOMAIN
       ].join('');
       ctx.hosts.parseHostname(hostname, function (err, parsed) {
         if (err) { return done(err); }
-        expect(parsed.instanceName).to.equal('instance-name');
+        expect(parsed.instanceName).to.equal(ctx.instanceName);
         expect(parsed.username).to.equal('user-name');
         done();
       });
