1ef8a0383feef1c146a9cf367b3af37ee3bfa37d
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-07-24 14:16:03 -0700

    add push to registy as fallback, update test

--- unit/models/apis/docker.js
+++ unit/models/apis/docker.js
@@ -13,30 +13,31 @@ var noop = require('101/noop');
 require('loadenv')();
 var Docker = require('models/apis/docker');
 
-describe('docker', function () {
+describe('docker unit test', function () {
   var model = new Docker('http://fake.host.com');
-
-  afterEach(function (done) {
-    model.startContainer.restore();
-    done();
-  });
-
-  it('should not include charon if env variable is not set', function (done) {
-    sinon.stub(model, 'startContainer', function (container, opts) {
-      expect(opts.Dns.length).to.equal(1);
+  describe('startUserContainer', function() {
+    afterEach(function (done) {
+      model.startContainer.restore();
       done();
     });
-    model.startUserContainer({}, {}, noop);
-  });
 
-  it('should include charon as the first dns when evn is set', function (done) {
-    var host = process.env.CHARON_HOST = '10.10.10.10';
-    sinon.stub(model, 'startContainer', function (container, opts) {
-      expect(opts.Dns.length).to.equal(2);
-      expect(opts.Dns[0]).to.equal(host);
-      delete process.env.CHARON_HOST;
-      done();
+    it('should not include charon if env variable is not set', function (done) {
+      sinon.stub(model, 'startContainer', function (container, opts) {
+        expect(opts.Dns.length).to.equal(1);
+        done();
+      });
+      model.startUserContainer({}, {}, noop);
     });
-    model.startUserContainer({}, {}, noop);
+
+    it('should include charon as the first dns when evn is set', function (done) {
+      var host = process.env.CHARON_HOST = '10.10.10.10';
+      sinon.stub(model, 'startContainer', function (container, opts) {
+        expect(opts.Dns.length).to.equal(2);
+        expect(opts.Dns[0]).to.equal(host);
+        delete process.env.CHARON_HOST;
+        done();
+      });
+      model.startUserContainer({}, {}, noop);
+    });
   });
 });
