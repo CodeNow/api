e3d27ec431f77f214b1c17e55164e9882210574d
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-08-03 15:27:00 -0700

    loadenv and bind functions in master

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 'use strict';
+'use strict';
+require('loadenv')();
+
+if (process.env.NEW_RELIC_LICENSE_KEY) {
+  require('newrelic');
+}
 var cluster = require('cluster');
 var log = require('middlewares/logger')(__filename).log;
 
@@ -8,6 +14,7 @@ if (cluster.isMaster) {
   var master = process.env.ENABLE_CLUSTERING ?
     new Master()  :
     new Master(1) ; // disabled clustering means 1 worker
+  console.log(master.numWorkers, process.env.WORKERS_PER_CPU);
   master.start(function (err) {
     if (err) {
       log.fatal({err:err}, 'master failed to start');
