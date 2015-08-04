f9453f9b36c5bef28cd62c951ef66bfff60851b4
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-08-03 17:51:46 -0700

    wip fix api in tests

--- app.js
+++ app.js
@@ -6,30 +6,49 @@ if (process.env.NEW_RELIC_LICENSE_KEY) {
 }
 var cluster = require('cluster');
 var log = require('middlewares/logger')(__filename).log;
+var noop = require('101/noop');
 
-if (cluster.isMaster) {
-  // leave require in here.
-  var Master = require('process/master');
-  var master = process.env.ENABLE_CLUSTERING ?
-    new Master()  :
-    new Master(1) ; // disabled clustering means 1 worker
-  console.log(master.numWorkers, process.env.WORKERS_PER_CPU);
-  master.start(function (err) {
-    if (err) {
-      log.fatal({err:err}, 'master failed to start');
-      throw err;
-    }
-    log.info('master started');
-  });
-} else { // cluster.isWorker
-  // leave require in here.
-  var Worker = require('process/worker');
-  var worker = new Worker();
-  worker.start(function (err) {
-    if (err) {
-      log.fatal({err:err}, 'worker failed to start');
-      throw err;
-    }
-    log.info('worker started');
-  });
+function Api () {}
+
+Api.prototype.start = function (cb) {
+  cb = cb || noop;
+  if (process.env.ENABLE_CLUSTERING && cluster.isMaster) {
+    // leave require in here.
+    var Master = require('process/master');
+    var master = this.taskManager = new Master();
+    master.start(function (err) {
+      if (err) {
+        log.fatal({err:err}, 'master failed to start');
+        return cb(err);
+      }
+      log.info('master started');
+      cb();
+    });
+  }
+  else {
+    // leave require in here.
+    var Worker = require('process/worker');
+    var worker = this.taskManager = new Worker();
+    worker.start(function (err) {
+      if (err) {
+        log.fatal({err:err}, 'worker failed to start');
+        return cb(err);
+      }
+      log.info('worker started');
+      cb();
+    });
+  }
+};
+
+Api.prototype.stop = function (cb) {
+  cb = cb || noop;
+  if (process.env.ENABLE_CLUSTERING) {
+    this.taskManager.stop(cb);
+  }
+};
+
+var api = module.exports = new Api();
+
+if (!module.parent) { // npm start
+  api.start();
 }
