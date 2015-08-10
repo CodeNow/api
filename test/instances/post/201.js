a16ea7a58dd4064b12008bdaa7664a9f42e31f1d
Anton Podviaznikov <podviaznikov@gmail.com>
2015-08-10 15:52:08 -0700

    fixing tests

--- test/instances/post/201.js
+++ test/instances/post/201.js
@@ -219,7 +219,8 @@ describe('201 POST /instances', function () {
               creatorGithubId: ctx.user.attrs.accounts.github.id.toString(),
               ownerGithubId: ctx.user.attrs.accounts.github.id.toString(),
               sessionUserUsername: ctx.user.attrs.accounts.github.login,
-              sessionUserId: ctx.user.attrs.accounts.github.id,
+              sessionUserId: ctx.user.id.toString(),
+              sessionUserGitHubId: ctx.user.attrs.accounts.github.id,
               type: 'user-container',
               tid: process.env.TID_POST_INSTANCES
             }
