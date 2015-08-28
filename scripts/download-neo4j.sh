fe50fc664031cedd8237401a52d0e5e687f182dd
Bryan Kendall <bryan@runnable.com>
2015-08-27 17:25:22 -0700

    travis: cache puts the dir in place; check for bin

--- scripts/download-neo4j.sh
+++ scripts/download-neo4j.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-if [[ ! -e /tmp/neo4j-community-2.1.8 ]]; then
+if [[ ! -d /tmp/neo4j-community-2.1.8/bin ]]; then
+  echo "Downloading neo4j"
   wget http://neo4j.com/artifact.php?name=neo4j-community-2.1.8-unix.tar.gz -O /tmp/neo4j-community-2.1.8-unix.tar.gz
   cd /tmp; tar -xzvf neo4j-community-2.1.8-unix.tar.gz; cd -;
+else
+  echo "Neo4j exists!"
+  ls -l /tmp/neo4j-community-2.1.8
 fi
