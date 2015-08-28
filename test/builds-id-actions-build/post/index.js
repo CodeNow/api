90d84dbda4fd48917834826a3745887e581466e9
Nathan219 <Nathan219@gmail.com>
2015-08-28 15:11:05 -0700

    maybe this will help?

--- test/builds-id-actions-build/post/index.js
+++ test/builds-id-actions-build/post/index.js
@@ -31,9 +31,6 @@ describe('Build - /builds/:id/actions/build', function () {
   beforeEach(require('./../../fixtures/mocks/github/login'));
   beforeEach(require('./../../fixtures/mocks/github/login'));
   beforeEach(primus.connect);
-
-  before(require('../../fixtures/mocks/api-client').setup);
-  after(require('../../fixtures/mocks/api-client').clean);
   afterEach(primus.disconnect);
   afterEach(require('./../../fixtures/clean-mongo').removeEverything);
   afterEach(require('./../../fixtures/clean-ctx')(ctx));
