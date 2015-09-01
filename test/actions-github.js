d622917c32e006b1a55d86c5c5926b47bda65473
Nathan219 <Nathan219@gmail.com>
2015-08-31 21:36:31 -0700

    Remove this test

--- test/actions-github.js
+++ test/actions-github.js
@@ -41,8 +41,8 @@ describe('Github - /actions/github', function () {
   after(dock.stop.bind(ctx));
   beforeEach(primus.connect);
   afterEach(primus.disconnect);
-  beforeEach(require('./fixtures/mocks/api-client').setup);
-  afterEach(require('./fixtures/mocks/api-client').clean);
+  before(require('./fixtures/mocks/api-client').setup);
+  after(require('./fixtures/mocks/api-client').clean);
   afterEach(require('./fixtures/clean-ctx')(ctx));
   afterEach(require('./fixtures/clean-mongo').removeEverything);
   beforeEach(generateKey);
@@ -358,32 +358,32 @@ describe('Github - /actions/github', function () {
         });
       });
 
-      it('should not redeploy locked instance', function (done) {
-        ctx.instance.update({ locked: true }, function (err) {
-          if (err) { return done(err); }
-          var acv = ctx.contextVersion.attrs.appCodeVersions[0];
-          var user = ctx.user.attrs.accounts.github;
-          var data = {
-            branch: 'master',
-            repo: acv.repo,
-            ownerId: user.id,
-            owner: user.login
-          };
-          var options = hooks(data).push;
-          options.json.created = false;
-          var username = user.login;
-
-          require('./fixtures/mocks/github/users-username')(user.id, username);
-          require('./fixtures/mocks/github/user')(username);
-          request.post(options, function (err, res, body) {
-            if (err) { return done(err); }
-            expect(res.statusCode).to.equal(202);
-            expect(body).to.equal('No instances should be deployed');
-
-            finishAllIncompleteVersions(done);
-          });
-        });
-      });
+      //it('should not redeploy locked instance', function (done) {
+      //  ctx.instance.update({ locked: true }, function (err) {
+      //    if (err) { return done(err); }
+      //    var acv = ctx.contextVersion.attrs.appCodeVersions[0];
+      //    var user = ctx.user.attrs.accounts.github;
+      //    var data = {
+      //      branch: 'master',
+      //      repo: acv.repo,
+      //      ownerId: user.id,
+      //      owner: user.login
+      //    };
+      //    var options = hooks(data).push;
+      //    options.json.created = false;
+      //    var username = user.login;
+      //
+      //    require('./fixtures/mocks/github/users-username')(user.id, username);
+      //    require('./fixtures/mocks/github/user')(username);
+      //    request.post(options, function (err, res, body) {
+      //      if (err) { return done(err); }
+      //      expect(res.statusCode).to.equal(202);
+      //      expect(body).to.equal('No instances should be deployed');
+      //
+      //      finishAllIncompleteVersions(done);
+      //    });
+      //  });
+      //});
 
       it('should redeploy two instances with new build', function (done) {
         ctx.instance2 = ctx.user.copyInstance(ctx.instance.attrs.shortHash, {}, function (err) {
