477088c538e71117a50848e6bb6551063e6ee12f
Nathan219 <Nathan219@gmail.com>
2015-08-31 21:52:46 -0700

    enable tests for unsupported events

--- test/actions-github.js
+++ test/actions-github.js
@@ -86,47 +86,47 @@ describe('Github - /actions/github', function () {
     });
   });
 
-  //describe('not supported event type', function () {
-  //  beforeEach(function (done) {
-  //    ctx.originalBuildsOnPushSetting = process.env.ENABLE_GITHUB_HOOKS;
-  //    process.env.ENABLE_GITHUB_HOOKS = 'true';
-  //    done();
-  //  });
-  //  afterEach(function (done) {
-  //    process.env.ENABLE_GITHUB_HOOKS = ctx.originalBuildsOnPushSetting;
-  //    done();
-  //  });
-  //  it('should return OKAY', function (done) {
-  //    var options = hooks().issue_comment;
-  //    request.post(options, function (err, res, body) {
-  //      if (err) { return done(err); }
-  //      expect(res.statusCode).to.equal(202);
-  //      expect(body).to.equal('No action set up for that payload.');
-  //      done();
-  //    });
-  //  });
-  //});
-  //describe('created tag', function () {
-  //  beforeEach(function (done) {
-  //    ctx.originalBuildsOnPushSetting = process.env.ENABLE_GITHUB_HOOKS;
-  //    process.env.ENABLE_GITHUB_HOOKS = 'true';
-  //    done();
-  //  });
-  //  afterEach(function (done) {
-  //    process.env.ENABLE_GITHUB_HOOKS = ctx.originalBuildsOnPushSetting;
-  //    done();
-  //  });
-  //  it('should return message that we cannot handle tags events', function (done) {
-  //    var options = hooks().push;
-  //    options.json.ref = 'refs/tags/v1';
-  //    request.post(options, function (err, res, body) {
-  //      if (err) { return done(err); }
-  //      expect(res.statusCode).to.equal(202);
-  //      expect(body).to.equal('Cannot handle tags\' related events');
-  //      done();
-  //    });
-  //  });
-  //});
+  describe('not supported event type', function () {
+    beforeEach(function (done) {
+      ctx.originalBuildsOnPushSetting = process.env.ENABLE_GITHUB_HOOKS;
+      process.env.ENABLE_GITHUB_HOOKS = 'true';
+      done();
+    });
+    afterEach(function (done) {
+      process.env.ENABLE_GITHUB_HOOKS = ctx.originalBuildsOnPushSetting;
+      done();
+    });
+    it('should return OKAY', function (done) {
+      var options = hooks().issue_comment;
+      request.post(options, function (err, res, body) {
+        if (err) { return done(err); }
+        expect(res.statusCode).to.equal(202);
+        expect(body).to.equal('No action set up for that payload.');
+        done();
+      });
+    });
+  });
+  describe('created tag', function () {
+    beforeEach(function (done) {
+      ctx.originalBuildsOnPushSetting = process.env.ENABLE_GITHUB_HOOKS;
+      process.env.ENABLE_GITHUB_HOOKS = 'true';
+      done();
+    });
+    afterEach(function (done) {
+      process.env.ENABLE_GITHUB_HOOKS = ctx.originalBuildsOnPushSetting;
+      done();
+    });
+    it('should return message that we cannot handle tags events', function (done) {
+      var options = hooks().push;
+      options.json.ref = 'refs/tags/v1';
+      request.post(options, function (err, res, body) {
+        if (err) { return done(err); }
+        expect(res.statusCode).to.equal(202);
+        expect(body).to.equal('Cannot handle tags\' related events');
+        done();
+      });
+    });
+  });
 
   describe('push event', function () {
     var ctx = {};
