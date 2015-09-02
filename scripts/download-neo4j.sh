5eb5a647da19b34743af859e562b4ed62e7fdbe9
Bryan Kendall <bryan@runnable.com>
2015-09-02 14:48:32 -0700

    travis: only cache tarball for neo4j

--- scripts/download-neo4j.sh
+++ scripts/download-neo4j.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-if [[ ! -d /tmp/neo4j-community-2.1.8/bin ]]; then
+if [[ ! -e /tmp/downloads/neo4j-community-2.1.8-unix.tar.gz ]]; then
   echo "Downloading neo4j"
-  wget http://neo4j.com/artifact.php?name=neo4j-community-2.1.8-unix.tar.gz -O /tmp/neo4j-community-2.1.8-unix.tar.gz
-  cd /tmp; tar -xzvf neo4j-community-2.1.8-unix.tar.gz; cd -;
+  mkdir -p /tmp/downloads
+  wget http://neo4j.com/artifact.php?name=neo4j-community-2.1.8-unix.tar.gz -O /tmp/downloads/neo4j-community-2.1.8-unix.tar.gz
 else
-  echo "Neo4j exists!"
-  ls -l /tmp/neo4j-community-2.1.8
+  echo "Neo4j tarball exists!"
+  ls -l /tmp/downloads
 fi
+
+rm -rf /tmp/neo4j-community-2.1.8
+tar -C /tmp -xzf /tmp/downloads/neo4j-community-2.1.8-unix.tar.gz;
