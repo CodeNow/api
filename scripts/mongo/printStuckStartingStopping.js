955b87dd4dd90f17ef01ad6aa784dcf318ae64e0
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-09-10 15:31:21 -0700

    mongo script descriptions and better stuck starting stopping script

--- scripts/mongo/printStuckStartingStopping.js
+++ scripts/mongo/printStuckStartingStopping.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var console = { log: print };
 var $startingOrStopping = {
@@ -10,9 +11,56 @@ var $startingOrStopping = {
     }
   ]
 };
-console.log(new ISODate());
+var timeout = 2*60*1000; // 2m
+
+console.log('Started ' + new ISODate());
+var instanceIds1 = [];
+var instanceIds2 = [];
 db.instances
   .find($startingOrStopping)
   .forEach(function (instance) {
-    console.log(instance._id.toString());
+    instanceIds1.push(instance._id.toString());
   });
+
+
+console.log('Waiting %s min and checking if any instances are stuck'.replace('%s', timeout/60000));
+setTimeout(function () {
+  db.instances
+    .find($startingOrStopping)
+    .forEach(function (instance) {
+      instanceIds2.push(instance._id.toString());
+    });
+  var stuckInstances = intersection(instanceIds1, instanceIds2);
+
+  console.log('Completed ' + new ISODate());
+  console.log('Stuck instances');
+  printjson(stuckInstances);
+}, timeout);
+
+
+/* Utils */
+function intersection (arr1, arr2) {
+  return arr1.map(function (id) {
+    var index = findIndex(arr2, equals(id));
+    if (~index) {
+      instanceIds2.splice(index, 1);
+      return true;
+    }
+  });
+}
+function equals (id1) {
+  return function (id2) {
+    return id1 === id2;
+  };
+}
+function findIndex (arr, fn) {
+  var index = -1;
+  arr.some(function (item, i) {
+    if (fn(item)) {
+      index = i;
+      return true;
+    }
+  });
+
+  return index;
+}
