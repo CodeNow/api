83d8d5d3faad57edc92aec6b039be86bc17b9790
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-09-11 03:12:50 -0700

    removed setting error early, fixed lint/eslint errors

--- unit/workers/create-instance-container.js
+++ unit/workers/create-instance-container.js
@@ -5,6 +5,8 @@ var Lab = require('lab');
 var lab = exports.lab = Lab.script();
 var describe = lab.describe;
 var it = lab.it;
+var beforeEach = lab.beforeEach;
+var afterEach = lab.afterEach;
 var Code = require('code');
 var expect = Code.expect;
 var sinon = require('sinon');
@@ -15,8 +17,8 @@ var Docker = require('models/apis/docker');
 var Instance = require('models/mongo/instance');
 var ContextVersion = require('models/mongo/context-version');
 
-describe('Worker: create-instance-container', function () {
 
+describe('Worker: create-instance-container', function () {
   describe('#_handleAppError', function () {
     it('should return error if instance.findId returned error', function (done) {
       var worker = new CreateInstanceContainer();
@@ -30,6 +32,7 @@ describe('Worker: create-instance-container', function () {
         done();
       });
     });
+
     it('should return error if instance was not found', function (done) {
       var worker = new CreateInstanceContainer();
       sinon.stub(Instance, 'findById', function (id, cb) {
@@ -38,11 +41,13 @@ describe('Worker: create-instance-container', function () {
       worker._handleAppError('some-instance-id', 'some-cv-id', {}, function (err) {
         expect(err).to.exist();
         expect(err.output.statusCode).to.equal(404);
-        expect(err.output.payload.message).to.equal('Instance was not found inside create container job');
+        expect(err.output.payload.message)
+          .to.equal('Instance was not found inside create container job');
         Instance.findById.restore();
         done();
       });
     });
+
     it('should call instance.modifyContainerCreateErr ', function (done) {
       var worker = new CreateInstanceContainer();
       var error = Boom.badRequest('Some error');
@@ -62,47 +67,82 @@ describe('Worker: create-instance-container', function () {
       worker._handleAppError('some-instance-id', 'some-cv-id', error, function (err) {
         expect(err).to.exist();
         expect(err.output.statusCode).to.equal(404);
-        expect(err.output.payload.message).to.equal('Instance was not found inside create container job');
+        expect(err.output.payload.message)
+          .to.equal('Instance was not found inside create container job');
       });
     });
   });
-  describe('#_handle404', function () {
-    it('should return error if finding instance returned error', function (done) {
+
+  describe.only('#_handle404', function () {
+    beforeEach(function (done) {
+      sinon.stub(Docker.prototype, 'pullImage');
+      sinon.stub(Docker.prototype, 'createUserContainer');
+      done();
+    });
+
+    afterEach(function (done) {
+      Docker.prototype.pullImage.restore();
+      Docker.prototype.createUserContainer.restore();
+      done();
+    });
+
+
+    it('should return error if pull returned error', function (done) {
+      Docker.prototype.pullImage.yieldsAsync('explode');
       var worker = new CreateInstanceContainer();
-      sinon.stub(Instance, 'findById', function (id, cb) {
-        cb(new Error('Some error'));
-      });
-      var data = {
-        instanceId: 'some-instance-id',
-        userId: 'user-id'
+      var cv = {
+        build: {
+          dockerTag: 'test/tag'
+        }
       };
-      var error = Boom.notFound('Docker error');
-      worker._handle404({}, data, error, function (err) {
+      var data = {
+        dockerHost: 'http://localhost:4242'
+      };
+      worker._handle404(cv, data, function (err) {
         expect(err).to.exist();
-        expect(err.message).to.equal('Some error');
-        Instance.findById.restore();
         done();
       });
     });
-    it('should return error if instance was not found', function (done) {
+
+
+    it('should return error if createUserContainer returned error', function (done) {
+      Docker.prototype.pullImage.yieldsAsync();
+      Docker.prototype.createUserContainer.yieldsAsync('explode');
       var worker = new CreateInstanceContainer();
-      sinon.stub(Instance, 'findById', function (id, cb) {
-        cb(null, null);
-      });
-      var data = {
-        instanceId: 'some-instance-id',
-        userId: 'user-id'
+      var cv = {
+        build: {
+          dockerTag: 'test/tag'
+        }
       };
-      var error = Boom.notFound('Docker error');
-      worker._handle404({}, data, error, function (err) {
+      var data = {
+        dockerHost: 'http://localhost:4242'
+      };
+      worker._handle404(cv, data, function (err) {
         expect(err).to.exist();
-        expect(err.output.statusCode).to.equal(404);
-        expect(err.output.payload.message).to.equal('Instance was not found inside create container job');
-        Instance.findById.restore();
+        done();
+      });
+    });
+
+
+    it('should return', function (done) {
+      Docker.prototype.pullImage.yieldsAsync();
+      Docker.prototype.createUserContainer.yieldsAsync();
+      var worker = new CreateInstanceContainer();
+      var cv = {
+        build: {
+          dockerTag: 'test/tag'
+        }
+      };
+      var data = {
+        dockerHost: 'http://localhost:4242'
+      };
+      worker._handle404(cv, data, function (err) {
+        expect(err).to.not.exist();
         done();
       });
     });
   });
+
   describe('#handle', function () {
     it('should return nothing if context version was not found because of error', function (done) {
       var worker = new CreateInstanceContainer({});
@@ -120,6 +160,7 @@ describe('Worker: create-instance-container', function () {
       });
     });
 
+
     it('should return nothing if context version was not found', function (done) {
       var worker = new CreateInstanceContainer({});
       sinon.stub(ContextVersion, 'findById', function (id, cb) {
@@ -136,7 +177,8 @@ describe('Worker: create-instance-container', function () {
       });
     });
 
-    it('should call Docker.createUserContainer and return nothing if everything was fine', function (done) {
+
+    it('should call Docker.createUserContainer and return nothing', function (done) {
       var data = {
         cvId: 'some-cv-id',
         sessionUserId: 'some-user-id',
@@ -144,18 +186,18 @@ describe('Worker: create-instance-container', function () {
         dockerHost: 'http://localhost:4242',
         instanceEnvs: ['RUNNABLE_CONTAINER_ID=yd3as6'],
         labels: {
-          contextVersionId : 'some-cv-id',
-          instanceId       : 'some-instance-id',
-          instanceName     : 'master',
+          contextVersionId: 'some-cv-id',
+          instanceId: 'some-instance-id',
+          instanceName: 'master',
           instanceShortHash: 'yd3as6',
-          ownerUsername    : 'anton',
-          creatorGithubId  : 123123,
-          ownerGithubId    : 812933
+          ownerUsername: 'anton',
+          creatorGithubId: 123123,
+          ownerGithubId: 812933
         }
       };
       var worker = new CreateInstanceContainer(data);
       sinon.stub(ContextVersion, 'findById', function (id, cb) {
-        cb(null, {_id: 'some-cv-id' });
+        cb(null, { _id: 'some-cv-id' });
       });
       sinon.stub(Docker.prototype, 'createUserContainer', function (cv, payload, cb) {
         expect(cv._id).to.equal(data.cvId);
@@ -179,6 +221,7 @@ describe('Worker: create-instance-container', function () {
       });
     });
 
+
     it('should call _handle404 if we got 404 from docker', function (done) {
       var data = {
         cvId: 'some-cv-id',
@@ -187,18 +230,18 @@ describe('Worker: create-instance-container', function () {
         dockerHost: 'http://localhost:4242',
         instanceEnvs: ['RUNNABLE_CONTAINER_ID=yd3as6'],
         labels: {
-          contextVersionId : 'some-cv-id',
-          instanceId       : 'some-instance-id',
-          instanceName     : 'master',
+          contextVersionId: 'some-cv-id',
+          instanceId: 'some-instance-id',
+          instanceName: 'master',
           instanceShortHash: 'yd3as6',
-          ownerUsername    : 'anton',
-          creatorGithubId  : 123123,
-          ownerGithubId    : 812933
+          ownerUsername: 'anton',
+          creatorGithubId: 123123,
+          ownerGithubId: 812933
         }
       };
       var worker = new CreateInstanceContainer(data);
       sinon.stub(ContextVersion, 'findById', function (id, cb) {
-        cb(null, {_id: 'some-cv-id' });
+        cb(null, { _id: 'some-cv-id' });
       });
       sinon.stub(Docker.prototype, 'createUserContainer', function (cv, payload, cb) {
         expect(cv._id).to.equal(data.cvId);
@@ -206,7 +249,7 @@ describe('Worker: create-instance-container', function () {
         expect(payload.Labels).to.deep.equal(data.labels);
         cb(Boom.notFound('Docker error'));
       });
-      sinon.stub(worker, '_handle404', function (contextVersion, data, appError, cb) {
+      sinon.stub(worker, '_handle404', function (contextVersion, d, cb) {
         cb(null);
       });
       sinon.spy(worker, '_handleAppError');
@@ -224,6 +267,7 @@ describe('Worker: create-instance-container', function () {
       });
     });
 
+
     it('should call _handleAppError if we got not 404 or 504 from docker', function (done) {
       var data = {
         cvId: 'some-cv-id',
@@ -232,18 +276,18 @@ describe('Worker: create-instance-container', function () {
         dockerHost: 'http://localhost:4242',
         instanceEnvs: ['RUNNABLE_CONTAINER_ID=yd3as6'],
         labels: {
-          contextVersionId : 'some-cv-id',
-          instanceId       : 'some-instance-id',
-          instanceName     : 'master',
+          contextVersionId: 'some-cv-id',
+          instanceId: 'some-instance-id',
+          instanceName: 'master',
           instanceShortHash: 'yd3as6',
-          ownerUsername    : 'anton',
-          creatorGithubId  : 123123,
-          ownerGithubId    : 812933
+          ownerUsername: 'anton',
+          creatorGithubId: 123123,
+          ownerGithubId: 812933
         }
       };
       var worker = new CreateInstanceContainer(data);
       sinon.stub(ContextVersion, 'findById', function (id, cb) {
-        cb(null, {_id: 'some-cv-id' });
+        cb(null, { _id: 'some-cv-id' });
       });
       sinon.stub(Docker.prototype, 'createUserContainer', function (cv, payload, cb) {
         expect(cv._id).to.equal(data.cvId);
@@ -269,6 +313,7 @@ describe('Worker: create-instance-container', function () {
       });
     });
 
+
     it('should return error if 504 occured 5 times', function (done) {
       var data = {
         cvId: 'some-cv-id',
@@ -277,18 +322,18 @@ describe('Worker: create-instance-container', function () {
         dockerHost: 'http://localhost:4242',
         instanceEnvs: ['RUNNABLE_CONTAINER_ID=yd3as6'],
         labels: {
-          contextVersionId : 'some-cv-id',
-          instanceId       : 'some-instance-id',
-          instanceName     : 'master',
+          contextVersionId: 'some-cv-id',
+          instanceId: 'some-instance-id',
+          instanceName: 'master',
           instanceShortHash: 'yd3as6',
-          ownerUsername    : 'anton',
-          creatorGithubId  : 123123,
-          ownerGithubId    : 812933
+          ownerUsername: 'anton',
+          creatorGithubId: 123123,
+          ownerGithubId: 812933
         }
       };
       var worker = new CreateInstanceContainer(data);
       sinon.stub(ContextVersion, 'findById', function (id, cb) {
-        cb(null, {_id: 'some-cv-id' });
+        cb(null, { _id: 'some-cv-id' });
       });
       sinon.stub(Docker.prototype, 'createUserContainer', function (cv, payload, cb) {
         expect(cv._id).to.equal(data.cvId);
@@ -313,6 +358,5 @@ describe('Worker: create-instance-container', function () {
         done();
       });
     });
-
   });
 });
