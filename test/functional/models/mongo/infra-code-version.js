078cc878adc4da7cc106ae06b452e0d9c7337eec
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-09-15 16:41:24 -0700

    functional tests for context-version

---
+++ test/functional/models/mongo/infra-code-version.js
@@ -0,0 +1,12 @@
+var InfraCodeVersion = require('models/mongo/infra-code-version.js');
+
+describe('InfraCodeVersion Model Functional Tests', function() {
+
+  describe('methods', function() {
+
+  });
+
+  describe('statics', function() {
+
+  });
+});
