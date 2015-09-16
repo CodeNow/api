74431825d328c9ac112f6f32478ca6921355fc82
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-09-15 17:04:43 -0700

    comment out infra-code tests for now

--- test/functional/models/mongo/infra-code-version.js
+++ test/functional/models/mongo/infra-code-version.js
@@ -1,12 +1,15 @@
-var InfraCodeVersion = require('models/mongo/infra-code-version.js');
+// var InfraCodeVersion = require('models/mongo/infra-code-version.js');
 
-describe('InfraCodeVersion Model Functional Tests', function() {
+// describe('InfraCodeVersion Model Functional Tests', function() {
 
-  describe('methods', function() {
+//   describe('methods', function() {
 
-  });
+//     describe('findByIdAndGetHash', function() {
 
-  describe('statics', function() {
+//     });
+//   });
 
-  });
-});
+//   describe('statics', function() {
+
+//   });
+// });
