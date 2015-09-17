6e6aba37c68f7ed99cf445e4336da20f69760dcc
Anton Podviaznikov <podviaznikov@gmail.com>
2015-09-17 13:06:27 -0700

    remove old code

--- unit/models/apis/build-files.js
+++ unit/models/apis/build-files.js
@@ -17,33 +17,6 @@ var BuildFiles = require('models/apis/build-files');
 describe('build-files', function() {
   var model = new BuildFiles('some-context-id');
 
-  // TODO Update s3-stream-upload past 0.0.6 and we can more easily test this
-  // describe('putFileStream', function() {
-  //   var stream = { pipe: noop, on: noop };
-  //
-  //   beforeEach(function (done) {
-  //     sinon.stub(stream, 'pipe').returns({
-  //       on: noop,
-  //       _uploader: {
-  //         once: noop
-  //       }
-  //     });
-  //     sinon.spy(stream, 'on');
-  //     done();
-  //   });
-  //
-  //   afterEach(function (done) {
-  //     stream.on.restore();
-  //     done();
-  //   });
-  //
-  //   it('should handle errors on streams', function(done) {
-  //     model.putFileStream('key', stream, noop);
-  //     expect(stream.on.calledWith('error')).to.be.true();
-  //     done();
-  //   });
-  // });
-
   describe('copyObject', function() {
     var readStream = { pipe: noop };
 
