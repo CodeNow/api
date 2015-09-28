d24d410e18a29f5ce26002f0684a31ffc65ce003
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-09-27 23:28:09 -0700

    add query and fetch of instance

--- scripts/rebuild-instances-no-cache.js
+++ scripts/rebuild-instances-no-cache.js
@@ -21,8 +21,13 @@ var runnableClient = new Runnable(process.env.FULL_API_DOMAIN, {
 });
 
 // all instances found with this query will be rebuild
+// var query = { 'container.error.data.err.reason': 'runnable error please rebuild' };
+
+// hung builds
 var query = {
-  'container.error.data.err.reason': 'runnable error please rebuild'
+  container: { $exists: false },
+  'contextVersion.build.completed': { $exists: true },
+  'contextVersion.build.failed': false
 };
 
 runnableClient.githubLogin(process.env.HELLO_RUNNABLE_GITHUB_TOKEN, function (err0) {
@@ -33,7 +38,7 @@ runnableClient.githubLogin(process.env.HELLO_RUNNABLE_GITHUB_TOKEN, function (er
   var c = 0;
   Instance.find(query, function (err1, a) {
     // use me to do a single instance
-    // a = [{ shortHash: '17kxj2' }];
+    // a = [{ shortHash: 'e5wdne' }];
     if (err1) {
       console.log('find failed');
       throw err1;
@@ -68,31 +73,38 @@ runnableClient.githubLogin(process.env.HELLO_RUNNABLE_GITHUB_TOKEN, function (er
               console.log('error logging in', err4, i.shortHash);
               return cb();
             }
-            instanceModel.build.deepCopy(function (err5, build) {
+            instanceModel = runnableClient2.newInstance(i.shortHash);
+            instanceModel.fetch(function (err5) {
               if (err5) {
-                console.log('failed to deep copy', i.shortHash, err5);
+                console.log('failed fetch 2', err5, i.shortHash);
                 return cb();
               }
-              build = runnableClient.newBuild(build);
-              build.build({
-                message: 'Manual build',
-                noCache: true
-              }, function (err6, nbuild) {
+              instanceModel.build.deepCopy(function (err6, build) {
                 if (err6) {
-                  console.log('failed to build', i.shortHash, err6);
+                  console.log('failed to deep copy', i.shortHash, err6);
                   return cb();
                 }
-                instanceModel.update({
-                  build: nbuild._id,
-                  env: instanceModel.attrs.env
-                }, function (err7) {
-                  // ignore errors for now
+                build = runnableClient.newBuild(build);
+                build.build({
+                  message: 'Manual build',
+                  noCache: true
+                }, function (err7, nbuild) {
                   if (err7) {
-                    console.log('failed to update', i.shortHash, err7);
-                  } else {
-                    console.log('done', i.shortHash, c, '/', a.length);
+                    console.log('failed to build', i.shortHash, err7);
+                    return cb();
                   }
-                  cb();
+                  instanceModel.update({
+                    build: nbuild._id,
+                    env: instanceModel.attrs.env
+                  }, function (err8) {
+                    // ignore errors for now
+                    if (err8) {
+                      console.log('failed to update', i.shortHash, err8);
+                    } else {
+                      console.log('done', i.shortHash, c, '/', a.length);
+                    }
+                    cb();
+                  });
                 });
               });
             });
