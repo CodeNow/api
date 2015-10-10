d7605d10808cf4f16df2568f2ce79bc89148bf2c
Anton Podviaznikov <podviaznikov@gmail.com>
2015-10-09 17:37:51 -0700

    add one more describe

--- unit/middlewares/apis/jobs.js
+++ unit/middlewares/apis/jobs.js
@@ -19,58 +19,61 @@ var path = require('path');
 var moduleName = path.relative(process.cwd(), __filename);
 
 describe('lib/middlewares/apis/jobs.js unit test: '+moduleName, function () {
-  var testReq = {
-    body: {
-      name: 'nemo'
-    }
-  };
-  beforeEach(function (done) {
-    sinon.stub(Github.prototype, 'getUserByUsername');
-    sinon.stub(rabbitMQ, 'publishClusterProvision');
-    done();
-  });
 
-  afterEach(function (done) {
-    Github.prototype.getUserByUsername.restore();
-    rabbitMQ.publishClusterProvision.restore();
-    done();
-  });
-
-  describe('getUserByUsername errs', function() {
-    it('should return error', function(done) {
-      var testErr = new Error('ice storm');
-      Github.prototype.getUserByUsername.yieldsAsync(testErr);
-      jobs.publishClusterProvision(testReq, {}, function (err) {
-        expect(err).to.deep.equal(testErr);
-        done();
-      });
-    });
-    it('should return badRequest', function(done) {
-      Github.prototype.getUserByUsername.yieldsAsync(null, null);
-      jobs.publishClusterProvision(testReq, {}, function (err) {
-        expect(err.output.statusCode).to.equal(400);
-        done();
-      });
-    });
-  }); // end getUserByUsername errs
-  describe('getUserByUsername successful', function() {
-    var testId = 24182934;
-    var testData = {
-      id: testId
+  describe('publishClusterProvision', function () {
+    var testReq = {
+      body: {
+        name: 'nemo'
+      }
     };
-    beforeEach(function(done) {
-      Github.prototype.getUserByUsername.yieldsAsync(null, testData);
+    beforeEach(function (done) {
+      sinon.stub(Github.prototype, 'getUserByUsername');
+      sinon.stub(rabbitMQ, 'publishClusterProvision');
       done();
     });
-    it('should publish job', function(done) {
-      jobs.publishClusterProvision(testReq, {}, function (err) {
-        expect(err).to.not.exist();
-        expect(rabbitMQ.publishClusterProvision
-          .withArgs({
-            githubId: testId
-          }).called).to.be.true();
+
+    afterEach(function (done) {
+      Github.prototype.getUserByUsername.restore();
+      rabbitMQ.publishClusterProvision.restore();
+      done();
+    });
+
+    describe('getUserByUsername errs', function() {
+      it('should return error', function(done) {
+        var testErr = new Error('ice storm');
+        Github.prototype.getUserByUsername.yieldsAsync(testErr);
+        jobs.publishClusterProvision(testReq, {}, function (err) {
+          expect(err).to.deep.equal(testErr);
+          done();
+        });
+      });
+      it('should return badRequest', function(done) {
+        Github.prototype.getUserByUsername.yieldsAsync(null, null);
+        jobs.publishClusterProvision(testReq, {}, function (err) {
+          expect(err.output.statusCode).to.equal(400);
+          done();
+        });
+      });
+    }); // end getUserByUsername errs
+    describe('getUserByUsername successful', function() {
+      var testId = 24182934;
+      var testData = {
+        id: testId
+      };
+      beforeEach(function(done) {
+        Github.prototype.getUserByUsername.yieldsAsync(null, testData);
         done();
       });
-    });
-  }); // end getUserByUsername successful
+      it('should publish job', function(done) {
+        jobs.publishClusterProvision(testReq, {}, function (err) {
+          expect(err).to.not.exist();
+          expect(rabbitMQ.publishClusterProvision
+            .withArgs({
+              githubId: testId
+            }).called).to.be.true();
+          done();
+        });
+      });
+    }); // end getUserByUsername successful
+  }); // end publishClusterProvision successful
 });
