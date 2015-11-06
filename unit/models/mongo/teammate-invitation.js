3c57081b01bce7f07e34260f6b8a355d27637073
thejsj <jorge.silva@thejsj.com>
2015-11-06 13:04:16 -0800

    Change test code from promises to callbacks

--- unit/models/mongo/teammate-invitation.js
+++ unit/models/mongo/teammate-invitation.js
@@ -6,7 +6,7 @@
 var Code = require('code');
 var Lab = require('lab');
 var path = require('path');
-var Promise = require('bluebird');
+var async = require('async');
 
 var lab = exports.lab = Lab.script();
 var Faker = require('faker');
@@ -14,12 +14,13 @@ var Faker = require('faker');
 var describe = lab.describe;
 var it = lab.it;
 var before = lab.before;
+var beforeEach = lab.afterEach;
 var afterEach = lab.afterEach;
 var expect = Code.expect;
 
 var validation = require('../../fixtures/validation')(lab);
 
-var TeammateInvitation = Promise.promisifyAll(require('models/mongo/teammate-invitation'));
+var TeammateInvitation = require('models/mongo/teammate-invitation');
 
 var moduleName = path.relative(process.cwd(), __filename);
 describe('TeammateInvitation: ' + moduleName, function () {
@@ -27,33 +28,37 @@ describe('TeammateInvitation: ' + moduleName, function () {
   before(require('../../fixtures/mongo').connect);
   afterEach(require('../../../test/functional/fixtures/clean-mongo').removeEverything);
 
-  function createNewInviteAndSave (orgName) {
-    var invite = new TeammateInvitation({
-      githubUserId: validation.VALID_GITHUB_ID,
-      createdBy: validation.VALID_OBJECT_ID,
-      created: Date.now(),
-      email: Faker.Internet.email(),
-      orgName: orgName || 'CodeNow'
-    });
-    invite = Promise.promisifyAll(invite);
-    return invite.save();
-  }
+  beforeEach(function (done) {
+    function createNewInvite (orgName) {
+      return new TeammateInvitation({
+        githubUserId: validation.VALID_GITHUB_ID,
+        createdBy: validation.VALID_OBJECT_ID,
+        created: Date.now(),
+        email: Faker.Internet.email(),
+        orgName: orgName || 'CodeNow'
+      });
+    }
+    var runnableInvite = createNewInvite('Runnable');
+    var codeNowInvite = createNewInvite('CodeNow');
+    async.series([
+      runnableInvite.save.bind(runnableInvite),
+      codeNowInvite.save.bind(codeNowInvite),
+    ], done);
+  });
 
   describe('findByGithubOrgName', function () {
 
     it('should fetch all inivitations within a particular org', function (done) {
-      Promise.all([ createNewInviteAndSave('CodeNow'), createNewInviteAndSave('Runnable') ])
-        .then(function () {
-          return TeammateInvitation.findByGithubOrgNameAsync('Runnable');
-        })
-        .then(function (result) {
-          expect(result).to.have.length(1);
-          expect(result[0]).to.be.an.object();
-          expect(result[0].orgName).to.be.a.string();
-          expect(result[0].orgName).to.equal('Runnable');
-          done();
-        })
-        .catch(done);
+      TeammateInvitation.findByGithubOrgName('Runnable', function (err, result) {
+        if (err) {
+          done(err);
+        }
+        expect(result).to.have.length(1);
+        expect(result[0]).to.be.an.object();
+        expect(result[0].orgName).to.be.a.string();
+        expect(result[0].orgName).to.equal('Runnable');
+        done();
+      });
     });
 
   });
