21a071f5f157ee6b9d69e116f8105a895d54b1f3
Myztiq <kahn@runnable.com>
2015-11-09 17:22:52 -0800

    Revert "Fixed some BDD tests."
    This reverts commit 9808148a0f36d3cc6dcb0f5bdf69013c1398f639.

--- test/functional/actions-github.js
+++ test/functional/actions-github.js
@@ -55,15 +55,11 @@ describe('Github - /actions/github', function () {
     // Prevent worker creation and github event publishing by rabbit
     sinon.stub(rabbitMQ, 'deleteInstance', function () {});
     sinon.stub(rabbitMQ, 'publishGithubEvent');
-    sinon.stub(rabbitMQ, 'instanceCreated');
-    sinon.stub(rabbitMQ, 'instanceUpdated');
     done();
   });
   afterEach(function (done) {
     rabbitMQ.deleteInstance.restore();
     rabbitMQ.publishGithubEvent.restore();
-    rabbitMQ.instanceCreated.restore();
-    rabbitMQ.instanceUpdated.restore();
     done();
   });
   beforeEach(function (done) {
