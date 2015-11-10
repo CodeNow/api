dbe76e572669fd283c0cb2cdc132cf826bf9015d
Anton Podviaznikov <podviaznikov@gmail.com>
2015-11-10 11:41:10 -0800

    adding sauron mock to dock mock

--- test/functional/fixtures/dock.js
+++ test/functional/fixtures/dock.js
@@ -15,33 +15,80 @@ var put = require('101/put');
 
 var Hermes = require('runnable-hermes');
 
-var publishedEvents = [
-  'container.network.attached',
-  'container.network.attach-failed'
-];
 
-var subscribedEvents = [
-  'container.life-cycle.died',
-  'container.life-cycle.started'
-];
+// Sauron mock listens for `container.life-cycle.started` event and
+// publsihes `container.network.attached`
+var sauronMock = {
+  start: function (cb) {
+    var publishedEvents = [
+      'container.network.attached',
+      'container.network.attach-failed'
+    ];
 
-var opts = {
-    hostname: process.env.RABBITMQ_HOSTNAME,
-    password: process.env.RABBITMQ_PASSWORD,
-    port: process.env.RABBITMQ_PORT,
-    username: process.env.RABBITMQ_USERNAME,
-    name: '10.12.13.11.sauron'
-  };
+    var subscribedEvents = [
+      'container.life-cycle.died',
+      'container.life-cycle.started'
+    ];
 
-var rabbitPublisher = new Hermes(put({
-    publishedEvents: publishedEvents,
-  }, opts))
-  .on('error', console.log.bind(console));
+    var opts = {
+      hostname: process.env.RABBITMQ_HOSTNAME,
+      password: process.env.RABBITMQ_PASSWORD,
+      port: process.env.RABBITMQ_PORT,
+      username: process.env.RABBITMQ_USERNAME,
+      name: '10.12.13.11.sauron'
+    };
+    var rabbitPublisher = new Hermes(put({
+      publishedEvents: publishedEvents,
+    }, opts))
+    .on('error', function (err) {
+      console.log('rabbit publisher error', err);
+    });
+    this.rabbitPublisher = rabbitPublisher;
 
-var rabbitSubscriber = new Hermes(put({
-    subscribedEvents: subscribedEvents,
-  }, opts))
-  .on('error', console.log.bind(console));
+    var rabbitSubscriber = new Hermes(put({
+      subscribedEvents: subscribedEvents,
+    }, opts))
+    .on('error', function (err) {
+      console.log('rabbit subscriber error', err);
+    });
+    this.rabbitSubscriber = rabbitSubscriber;
+
+    var count = createCount(2, cb);
+    rabbitPublisher.connect(count.next);
+    rabbitSubscriber.connect(function (err) {
+      if (err) {
+        return count.next(err);
+      }
+      rabbitSubscriber.subscribe('container.life-cycle.started', function (data, jobCb) {
+        data.containerIp = '10.12.10.121';
+        rabbitPublisher.publish('container.network.attached', data);
+        jobCb();
+      });
+      count.next();
+    });
+  },
+  stop: function (cb) {
+    var count = createCount(3, cb);
+    this.rabbitSubscriber.unsubscribe('container.life-cycle.started', null, function(err) {
+      if (err) {
+        console.log('dock sauronMock unsubscribe error', err);
+      }
+      count.next();
+    });
+    this.rabbitSubscriber.close(function (err) {
+      if (err) {
+        console.log('dock sauronMock subscriber close error', err);
+      }
+      count.next();
+    });
+    this.rabbitPublisher.close(function (err) {
+      if (err) {
+        console.log('dock sauronMock publisher close error', err);
+      }
+      count.next();
+    });
+  }
+};
 
 module.exports = {
   start: startDock,
@@ -55,20 +102,9 @@ function startDock (done) {
   // FIXME: hack because docker-mock does not add image to its store for image-builder creates
   sinon.stub(dockerModel.prototype, 'pullImage').yieldsAsync();
   started = true;
-  var count = createCount(4, done);
+  var count = createCount(3, done);
   dockerModuleMock.setup(count.next);
-  rabbitPublisher.connect(count.next);
-  rabbitSubscriber.connect(function (err) {
-    if (err) {
-      return count.next(err);
-    }
-    rabbitSubscriber.subscribe('container.life-cycle.started', function (data, jobCb) {
-      data.containerIp = '10.12.10.121';
-      rabbitPublisher.publish('container.network.attached', data);
-      jobCb();
-    });
-    count.next();
-  });
+  sauronMock.start(count.next);
   ctx.docker = docker.start(function (err) {
     if (err) { return count.next(err); }
     ctx.mavis = mavisApp.listen(url.parse(process.env.MAVIS_HOST).port);
@@ -85,11 +121,9 @@ function stopDock (done) {
   if(!started) { return done(); }
   dockerModel.prototype.pullImage.restore();
   started = false;
-  var count = createCount(6, done);
+  var count = createCount(4, done);
   ctx.mavis.close(count.next);
-  rabbitSubscriber.unsubscribe('container.life-cycle.started', null, count.next);
-  rabbitSubscriber.close(count.next);
-  rabbitPublisher.close(count.next);
+  sauronMock.stop(count.next);
   redis.del(process.env.REDIS_HOST_KEYS, count.inc().next);
   dockerModuleMock.clean(count.next);
   dockerListener.stop(function(err) {
