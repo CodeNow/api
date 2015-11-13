eb85a10d0c896aa61eff00621ba26fadfc8146d9
test <test>
2015-11-13 05:56:19 +0000

    Script tweaks

--- scripts/populate-link.js
+++ scripts/populate-link.js
@@ -5,6 +5,11 @@ require('loadenv')();
 // Load all the things!
 require('express-app');
 
+// dummy port
+process.env.PORT = 7777;
+var Server = require('server');
+var server = new Server();
+
 var mongoose = require('mongoose');
 var async = require('async');
 
@@ -15,34 +20,40 @@ if (!process.env.MONGO) {
   throw 'process.env.MONGO does not exist!'
 }
 
-console.log('Connecting to ', process.env.MONGO, ' in 10 seconds');
-console.log('Connecting...');
-mongoose.connect(process.env.MONGO, function () {
-  console.log('Connected.');
+server.start(function () {
+  console.log('server started', arguments);
+	console.log('Connecting to ', process.env.MONGO, ' in 10 seconds');
+	console.log('Connecting...');
+	mongoose.connect(process.env.MONGO, function () {
+	  console.log('Connected.');
 
-  console.log('Fetching Instances');
-  Instance.find({}, function (err, instances) {
-    if (err) {
-      throw err
-    }
-    console.log(instances.length + ' instances fetched');
-    console.log('About to go through each one and update them in 10 seconds. Now is your last chance to stop the onslaught!');
-    setTimeout(function () {
-      console.log('TOO LATE! Populating....');
-      async.eachSeries(instances, function (instance, index) {
-        instance.emitInstanceUpdate('update', function (err) {
-          if (err) {
-            throw err
-          }
-          if (!instance.owner.username || !instance.createdBy.username ) {
-            console.log('Instance did not populate owner username and createdBy username', instance._id);
-          }
-          console.log('Updated', index, Math.floor(index/instances.length * 100));
-        })
-      }, function () {
-        console.log('DONE!');
-        process.exit(1);
-      })
-    }, 10000);
-  })
-})
+	  console.log('Fetching Instances');
+	  Instance.find({}, function (err, instances) {
+	    if (err) {
+	      throw err
+	    }
+	    console.log(instances.length + ' instances fetched');
+	    console.log('About to go through each one and update them in 10 seconds. Now is your last chance to stop the onslaught!');
+	    setTimeout(function () {
+	      console.log('TOO LATE! Populating....');
+	      async.eachSeries(instances, function (instance, cb) {
+		instance.emitInstanceUpdate('update', function (err) {
+		  if (err) {
+		    throw err
+		  }
+		  if (!instance.owner.username || !instance.createdBy.username ) {
+		    console.log('Instance did not populate owner username and createdBy username', instance._id);
+		  }
+//		  console.log('Updated', index, Math.floor(index/instances.length * 100));
+                  console.log('updated: ', instance._id);
+                  cb();
+		})
+	      }, function () {
+		console.log('DONE!');
+		process.exit(1);
+	      })
+	    }, 10000);
+	  })
+	})
+});
+
