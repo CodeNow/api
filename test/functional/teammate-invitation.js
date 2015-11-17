26c583b5172311e34825788c54d9da7e06a13f6c
thejsj <jorge.silva@thejsj.com>
2015-11-17 13:19:52 -0800

    Adding isOwnerOf check

--- test/functional/teammate-invitation.js
+++ test/functional/teammate-invitation.js
@@ -12,29 +12,30 @@ var Code = require('code')
 var expect = Code.expect
 var request = require('request')
 var randStr = require('randomstring').generate
+var githubUserOrgsMock = require('./fixtures/mocks/github/user-orgs.js')
 
 var api = require('./fixtures/api-control')
 var ctx = {
   githubUserId: 1,
-  orgGithubId: 2
+  orgGithubId: 999
 }
 
 describe('TeammateInvitation', function () {
   before(api.start.bind(ctx))
   before(function (done) {
     ctx.name = randStr(5)
-    done()
-  })
-  before(function (done) {
     ctx.j = request.jar()
     require('./fixtures/multi-factory').createUser({
       requestDefaults: { jar: ctx.j }
     }, function (err, user) {
+      if (err) {
+        done(err)
+      }
       ctx.user = user
-      done(err)
+      githubUserOrgsMock(ctx.user, ctx.orgGithubId, 'org-3')
+      done()
     })
   })
-
   after(api.stop.bind(ctx))
   after(function (done) {
     require('./fixtures/clean-mongo').removeEverything()
@@ -54,7 +55,7 @@ describe('TeammateInvitation', function () {
       }
       ctx.user.createTeammateInvitation(opts, function (err, res, statusCode) {
         if (err) {
-          done(err)
+          return done(err)
         }
         expect(statusCode).to.equal(201)
         expect(res).to.be.an.object()
@@ -72,22 +73,20 @@ describe('TeammateInvitation', function () {
   })
 
   describe('GET /teammate-invitation/', function () {
-    it('should get no results for an org that has no invitations', function (done) {
-      ctx.user.fetchTeammateInvitations({ orgGithubId: 777 }, function (err, res, statusCode) {
+    it('should deny a user querying an org it doesnt belong to', function (done) {
+      ctx.user.fetchTeammateInvitations({ orgGithubId: 2 }, function (err, res, statusCode) {
         if (err) {
-          done(err)
+          expect(err).to.be.an.object()
+          expect(err.message).to.match(/access denied/ig)
+          return done()
         }
-        expect(statusCode).to.equal(200)
-        expect(res).to.be.an.array()
-        expect(res.length).to.equal(0)
-        done()
       })
     })
 
     it('should get the results for an org that has invitations', function (done) {
       ctx.user.fetchTeammateInvitations({ orgGithubId: ctx.orgGithubId }, function (err, res, statusCode) {
         if (err) {
-          done(err)
+          return done(err)
         }
         expect(statusCode).to.equal(200)
         expect(res).to.be.an.array()
