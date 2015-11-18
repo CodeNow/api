72f93870153585e196140ed0d2a23977b4569cb7
thejsj <jorge.silva@thejsj.com>
2015-11-17 16:57:45 -0800

    Improving tests by adding more test cases
    Adding a test case for when there are no results, for when users and
    unauthorized to delete and Github API don't get cached

--- test/functional/teammate-invitation.js
+++ test/functional/teammate-invitation.js
@@ -5,7 +5,9 @@ var lab = exports.lab = Lab.script()
 var describe = lab.describe
 var it = lab.it
 var before = lab.before
+var beforeEach = lab.beforeEach
 var after = lab.after
+var afterEach = lab.afterEach
 
 var async = require('async')
 var Code = require('code')
@@ -13,16 +15,29 @@ var expect = Code.expect
 var request = require('request')
 var randStr = require('randomstring').generate
 var githubUserOrgsMock = require('./fixtures/mocks/github/user-orgs.js')
+var nock = require('nock')
 
 var api = require('./fixtures/api-control')
 var ctx = {
   githubUserId: 1,
   orgGithubId: 999
 }
+var createInvitation = function (done) {
+  var opts = {
+    organization: {
+      github: ctx.orgGithubId
+    },
+    recipient: {
+      email: ctx.user.attrs.email,
+      github: ctx.githubUserId
+    }
+  }
+  ctx.user.createTeammateInvitation(opts, done)
+}
 
 describe('TeammateInvitation', function () {
   before(api.start.bind(ctx))
-  before(function (done) {
+  beforeEach(function (done) {
     ctx.name = randStr(5)
     ctx.j = request.jar()
     require('./fixtures/multi-factory').createUser({
@@ -32,12 +47,16 @@ describe('TeammateInvitation', function () {
         done(err)
       }
       ctx.user = user
-      githubUserOrgsMock(ctx.user, ctx.orgGithubId, 'org-3')
+      githubUserOrgsMock(ctx.user, ctx.orgGithubId, 'super-org')
       done()
     })
   })
+  afterEach(function (done) {
+    nock.cleanAll()
+    done()
+  })
   after(api.stop.bind(ctx))
-  after(function (done) {
+  afterEach(function (done) {
     require('./fixtures/clean-mongo').removeEverything()
     done()
   })
@@ -102,27 +121,45 @@ describe('TeammateInvitation', function () {
       })
     })
 
-    it('should get the results for an org that has invitations', function (done) {
+    it('should return an empty array if there are no invitations', function (done) {
       ctx.user.fetchTeammateInvitations({ orgGithubId: ctx.orgGithubId }, function (err, res, statusCode) {
         if (err) {
           return done(err)
         }
         expect(statusCode).to.equal(200)
         expect(res).to.be.an.array()
-        expect(res.length).to.equal(1)
-        expect(res[0]).to.be.an.object()
-        expect(res[0].recipient.github).to.equal(ctx.githubUserId)
-        expect(res[0].recipient.email).to.equal(ctx.user.attrs.email)
-        expect(res[0].organization.github).to.equal(ctx.orgGithubId)
-        expect(res[0].owner).to.be.an.object()
-        expect(res[0].owner.github).to.be.a.number()
-        expect(res[0].owner.github).to.equal(ctx.user.attrs.accounts.github.id)
+        expect(res.length).to.equal(0)
         done()
+      })
+    })
+
+    describe('Getting Invitations', function () {
+      beforeEach(createInvitation)
+
+      it('should get the results for an org that has invitations', function (done) {
+        ctx.user.fetchTeammateInvitations({ orgGithubId: ctx.orgGithubId }, function (err, res, statusCode) {
+          if (err) {
+            return done(err)
+          }
+          expect(statusCode).to.equal(200)
+          expect(res).to.be.an.array()
+          expect(res.length).to.equal(1)
+          expect(res[0]).to.be.an.object()
+          expect(res[0].recipient.github).to.equal(ctx.githubUserId)
+          expect(res[0].recipient.email).to.equal(ctx.user.attrs.email)
+          expect(res[0].organization.github).to.equal(ctx.orgGithubId)
+          expect(res[0].owner).to.be.an.object()
+          expect(res[0].owner.github).to.be.a.number()
+          expect(res[0].owner.github).to.equal(ctx.user.attrs.accounts.github.id)
+          done()
+        })
       })
     })
   })
 
   describe('DELETE /teammate-invitation/:orgName', function () {
+    beforeEach(createInvitation)
+
     it('should delete invitations from the database', function (done) {
       async.waterfall([ function (cb) {
         ctx.user.fetchTeammateInvitations({ orgGithubId: ctx.orgGithubId }, cb)
@@ -139,6 +176,39 @@ describe('TeammateInvitation', function () {
         expect(collection.length).to.equal(0)
         cb()
       }], done)
+    })
+
+    describe('Unathorized Delete', function () {
+      var unauthorizedUser
+      before(function (done) {
+        ctx.name = randStr(5)
+        ctx.j = request.jar()
+        require('./fixtures/multi-factory').createUser({
+          requestDefaults: { jar: ctx.j }
+        }, function (err, user) {
+          if (err) {
+            done(err)
+          }
+          unauthorizedUser = user
+          githubUserOrgsMock(unauthorizedUser, 777, 'not-super-org')
+          done()
+        })
+      })
+
+      it('should delete invitations from the database', function (done) {
+        ctx.user.fetchTeammateInvitations({ orgGithubId: ctx.orgGithubId }, function (err, collection) {
+          if (err) {
+            done(err)
+          }
+          unauthorizedUser.destroyTeammateInvitation(collection[0]._id, {}, function (err, res) {
+            if (err) {
+              expect(err).to.be.an.object()
+              expect(err.message).to.match(/access denied/ig)
+              return done()
+            }
+          })
+        })
+      })
     })
   })
 })
