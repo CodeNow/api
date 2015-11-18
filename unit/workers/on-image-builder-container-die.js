0f94d18a892f47f3ffd3a00657d5af0605086b0e
Anton Podviaznikov <podviaznikov@gmail.com>
2015-11-17 17:39:43 -0800

    remove obsolete code, add more logs

--- unit/workers/on-image-builder-container-die.js
+++ unit/workers/on-image-builder-container-die.js
@@ -249,16 +249,28 @@ describe('OnImageBuilderContainerDie: ' + moduleName, function () {
     })
 
     it('should emit instance update events', function (done) {
+      sinon.stub(ctx.worker, '_baseWorkerUpdateInstanceFrontend').yieldsAsync(null)
+      ctx.worker.data = {
+        inspectData: {
+          Name: '/507f1f77bcf86cd799439011',
+          Config: {
+            Labels: {
+              sessionUserGithubId: '1274567'
+            }
+          }
+        }
+      }
       ctx.worker._emitInstanceUpdateEvents(function (err) {
         if (err) { return done(err) }
-        sinon.assert.calledWith(User.findByGithubId, ctx.data.inspectData.Config.Labels.sessionUserGithubId)
-        sinon.assert.calledWith(Instance.findAndPopulate, ctx.mockUser)
-        var query = Instance.findAndPopulate.firstCall.args[1]
-        expect(query['contextVersion.build._id'].toString())
-          .to.deep.equal(ctx.data.inspectData.Name.slice(1))
-        ctx.mockInstances.forEach(function (mockInstance, i) {
-          expect(messenger.emitInstanceUpdate.args[i][0]).to.equal(mockInstance)
-        })
+        expect(ctx.worker._baseWorkerUpdateInstanceFrontend.calledOnce).to.be.true()
+        var args = ctx.worker._baseWorkerUpdateInstanceFrontend.getCall(0).args
+        expect(Object.keys(args[0]).length).to.equal(1)
+        console.log('aaaa', args[0]);
+        var buildId = args[0]['contextVersion.build._id']
+        expect(buildId.toString()).to.equal('507f1f77bcf86cd799439011')
+        expect(args[1]).to.equal('1274567')
+        expect(args[2]).to.equal('patch')
+        ctx.worker._baseWorkerUpdateInstanceFrontend.restore()
         done()
       })
     })
