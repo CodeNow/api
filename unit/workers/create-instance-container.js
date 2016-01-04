18297df3d805c8fb5da6a2d2fcb97c075e793675
Myztiq <kahn@runnable.com>
2016-01-04 11:01:30 -0800

    Added logic to trigger re-builds when we get a image not found when creating a container.

--- unit/workers/create-instance-container.js
+++ unit/workers/create-instance-container.js
@@ -3,9 +3,14 @@ var path = require('path')
 
 var Lab = require('lab')
 var Boom = require('dat-middleware').Boom
+var Docker = require('models/apis/docker')
 var Code = require('code')
+var ContextVersion = require('models/mongo/context-version')
 var createInstanceContainer = require('workers/create-instance-container')
 var InstanceService = require('models/services/instance-service')
+var moment = require('moment')
+var Promise = require('bluebird')
+var rabbitmq = require('models/rabbitmq')
 var sinon = require('sinon')
 
 var expect = Code.expect
@@ -79,6 +84,43 @@ describe('Worker: create-instance-container: ' + moduleName, function () {
         createInstanceContainer(ctx.job)
           .asCallback(function (err) {
             expect(err.data.originalError.cause).to.equal(ctx.err)
+            done()
+          })
+      })
+    })
+
+    describe('Image not found create err', function () {
+      beforeEach(function (done) {
+        ctx.err = new Error('image not found')
+        ctx.contextVersion = {
+          build: {
+            completed: moment().subtract(3, 'minutes').format()
+          }
+        }
+        sinon.stub(Docker, 'isImageNotFoundForCreateErr').returns(true)
+        sinon.stub(ContextVersion, 'findById').returns(Promise.resolve(ctx.contextVersion))
+        sinon.stub(rabbitmq, 'publishInstanceRebuild')
+        InstanceService.createContainer.yieldsAsync(ctx.err)
+        done()
+      })
+      afterEach(function (done) {
+        Docker.isImageNotFoundForCreateErr.restore()
+        ContextVersion.findById.restore()
+        rabbitmq.publishInstanceRebuild.restore()
+        done()
+      })
+
+      it('should trigger a re-build of the instance', function (done) {
+        createInstanceContainer(ctx.job)
+          .asCallback(function (err) {
+            expect(err).to.not.exist()
+            sinon.assert.calledOnce(Docker.isImageNotFoundForCreateErr)
+            sinon.assert.calledOnce(ContextVersion.findById)
+            sinon.assert.calledWith(ContextVersion.findById, ctx.job.contextVersionId)
+            sinon.assert.calledOnce(rabbitmq.publishInstanceRebuild)
+            sinon.assert.calledWith(rabbitmq.publishInstanceRebuild, {
+              instanceId: ctx.job.instanceId
+            })
             done()
           })
       })
