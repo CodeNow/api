2720497ae507bd1becf9db51707b19c127942421
Ryan Sandor Richards <ryan@runnable.com>
2016-01-06 11:47:07 -0800

    Removed accidental file addition.

--- test.js
+++
@@ -1,9 +0,0 @@
-var Github = require('github');
-
-var github = new Github({
-        version: '3.0.0',
-        debug: true,
-        protocol: 'https',
-        requestMedia: 'application/json'
-});
-
