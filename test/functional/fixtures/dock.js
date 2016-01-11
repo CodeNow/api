4faa501fcfdb9aacb503761d67d24d7194ada380
thejsj <jorge.silva@thejsj.com>
2016-01-11 10:43:09 -0800

    Remove pullImage stubs from dock mock

--- test/functional/fixtures/dock.js
+++ test/functional/fixtures/dock.js
@@ -1,11 +1,9 @@
 
 var async = require('async')
-var dockerModel = require('models/apis/docker')
 var createCount = require('callback-count')
 var docker = require('./docker')
 var redis = require('models/redis')
 var dockerModuleMock = require('./mocks/docker-model')
-var sinon = require('sinon')
 
 process.env.AUTO_RECONNECT = false // needed for test
 process.env.HOST_TAGS = 'default' // needed for test
@@ -83,7 +81,6 @@ var started = false
 function startDock (done) {
   if (started) { return done() }
   // FIXME: hack because docker-mock does not add image to its store for image-builder creates
-  sinon.stub(dockerModel.prototype, 'pullImage').yieldsAsync()
   started = true
   var count = createCount(3, done)
   dockerModuleMock.setup(count.next)
@@ -99,7 +96,6 @@ function startDock (done) {
 }
 function stopDock (done) {
   if (!started) { return done() }
-  dockerModel.prototype.pullImage.restore()
   started = false
   var count = createCount(4, done)
   sauronMock.stop(count.next)
