c0f94a5353c59ac22e78aedfa642768c9a2fa987
Nathan219 <Nathan219@gmail.com>
2016-01-14 18:08:32 -0800

    Finishing the rest of the unit tests

--- unit/socket/terminal-stream.js
+++ unit/socket/terminal-stream.js
@@ -11,14 +11,15 @@ var Code = require('code')
 var expect = Code.expect
 
 var sinon = require('sinon')
-var Docker = require('models/apis/docker')
 var stream = require('stream')
 var EventEmitter = require('events').EventEmitter
 var util = require('util')
 
-var logStream = require('socket/log-stream')
+var terminalStream = require('socket/terminal-stream')
 var Instance = require('models/mongo/instance')
+var DebugContainer = require('models/mongo/debug-container')
 
+var Primus = require('primus')
 var Promise = require('bluebird')
 var commonStream = require('socket/common-stream')
 
@@ -39,7 +40,7 @@ var moduleName = path.relative(process.cwd(), __filename)
 var error
 var rejectionPromise
 
-describe('log stream: ' + moduleName, function () {
+describe('terminal stream: ' + moduleName, function () {
   beforeEach(function (done) {
     error = new Error('not owner')
     rejectionPromise = Promise.reject(error)
@@ -56,17 +57,22 @@ describe('log stream: ' + moduleName, function () {
         request: {
           sessionUser: ctx.sessionUser
         },
+        addListener: sinon.spy(),
         write: sinon.spy(),
         substream: sinon.stub().returns({
           on: sinon.spy(),
+          addListener: sinon.spy(),
           setEncoding: sinon.spy()
         })
       }
       ctx.id = 4
       ctx.data = {
         id: 4,
-        dockHost: 1233,
-        containerId: 1231231232
+        type: 'hello',
+        dockHost: '127.0.0.1',
+        containerId: 1231231232,
+        eventStreamId: 23413,
+        terminalStreamId: 2341324
       }
 
       ctx.instance = {
@@ -80,61 +86,108 @@ describe('log stream: ' + moduleName, function () {
           dockerContainer: ctx.data.containerId
         }
       }
+      ctx.debugContainer = {
+        createdBy: {
+          github: 123
+        },
+        owner: {
+          github: 123
+        },
+        inspect: {
+          dockerContainer: ctx.data.containerId
+        }
+      }
       done()
     })
     afterEach(function (done) {
-      Instance.findOne.restore()
       commonStream.checkOwnership.restore()
       done()
     })
 
     describe('Faliures', function () {
-      describe('Instance fetch failures', function () {
+      describe('model fetch failures', function () {
         beforeEach(function (done) {
           sinon.stub(commonStream, 'checkOwnership').returns(Promise.resolve(true))
           done()
         });
-        it('should do nothing if the instance fetch returns nothing', function (done) {
-          sinon.stub(Instance, 'findOne').yields()
-          logStream.logStreamHandler(ctx.socket, ctx.id, ctx.data)
-            .catch(function (err) {
-              expect(err.message).to.equal('Missing instance')
-              sinon.assert.calledOnce(ctx.socket.write)
-              sinon.assert.calledWith(ctx.socket.write, {
-                id: ctx.socket.id,
-                error: 'You don\'t have access to this stream'
+        describe('DebugContainer', function () {
+          beforeEach(function (done) {
+            ctx.data.isDebugContainer = true
+            done()
+          });
+          afterEach(function (done) {
+            DebugContainer.findOne.restore()
+            done()
+          });
+
+          it('should do nothing if the DebugContainer fetch returns nothing', function (done) {
+            sinon.stub(DebugContainer, 'findOne').yields()
+            terminalStream.proxyStreamHandler(ctx.socket, ctx.id, ctx.data)
+              .catch(function (err) {
+                expect(err.message).to.equal('Missing model')
+                sinon.assert.calledOnce(ctx.socket.write)
+                sinon.assert.calledWith(ctx.socket.write, {
+                  id: ctx.socket.id,
+                  error: 'You don\'t have access to this stream'
+                })
+                done()
               })
-              done()
-            })
+          })
         })
-        it('should do nothing if the instance fetch returns an error', function (done) {
-          sinon.stub(Instance, 'findOne').yields(error)
-          logStream.logStreamHandler(ctx.socket, ctx.id, ctx.data)
-            .catch(function (err) {
-              expect(err.message).to.equal(error.message)
-              sinon.assert.calledOnce(ctx.socket.write)
-              sinon.assert.calledWith(ctx.socket.write, {
-                id: ctx.socket.id,
-                error: 'You don\'t have access to this stream'
+
+        describe('Instance', function () {
+          afterEach(function (done) {
+            Instance.findOne.restore()
+            done()
+          })
+          it('should do nothing if the instance fetch returns nothing', function (done) {
+            sinon.stub(Instance, 'findOne').yields()
+            terminalStream.proxyStreamHandler(ctx.socket, ctx.id, ctx.data)
+              .catch(function (err) {
+                expect(err.message).to.equal('Missing model')
+                sinon.assert.calledOnce(ctx.socket.write)
+                sinon.assert.calledWith(ctx.socket.write, {
+                  id: ctx.socket.id,
+                  error: 'You don\'t have access to this stream'
+                })
+                done()
               })
-              done()
-            })
+          })
+          it('should do nothing if the model fetch returns an error', function (done) {
+            sinon.stub(Instance, 'findOne').yields(error)
+            terminalStream.proxyStreamHandler(ctx.socket, ctx.id, ctx.data)
+              .catch(function (err) {
+                expect(err.message).to.equal(error.message)
+                sinon.assert.calledOnce(ctx.socket.write)
+                sinon.assert.calledWith(ctx.socket.write, {
+                  id: ctx.socket.id,
+                  error: 'You don\'t have access to this stream'
+                })
+                done()
+              })
+          })
         })
       })
+
       describe('Other failures', function () {
         beforeEach(function (done) {
           sinon.stub(Instance, 'findOne').yields(null, ctx.instance)
           done()
-        });
+        })
+        afterEach(function (done) {
+          Instance.findOne.restore()
+          done()
+        })
         it('should do nothing if the args are invalid', function (done) {
           sinon.stub(commonStream, 'checkOwnership').returns(rejectionPromise)
-          logStream.logStreamHandler(ctx.socket, ctx.id, {})
+          terminalStream.proxyStreamHandler(ctx.socket, ctx.id, {})
             .catch(function (err) {
               expect(err.message).to.equal('invalid args')
               sinon.assert.calledOnce(ctx.socket.write)
               sinon.assert.calledWith(ctx.socket.write, {
                 id: ctx.id,
-                error: 'dockHost and containerId are required',
+                error: 'dockHost, type, containerId, ' +
+                'terminalStreamId, clientStreamId, are required',
                 data: {}
               })
               done()
@@ -142,7 +195,7 @@ describe('log stream: ' + moduleName, function () {
         })
         it('should do nothing if the ownership check fails', function (done) {
           sinon.stub(commonStream, 'checkOwnership').returns(rejectionPromise)
-          logStream.logStreamHandler(ctx.socket, ctx.id, ctx.data)
+          terminalStream.proxyStreamHandler(ctx.socket, ctx.id, ctx.data)
             .catch(function (err) {
               expect(err).to.equal(error)
               sinon.assert.calledOnce(ctx.socket.write)
@@ -159,38 +212,29 @@ describe('log stream: ' + moduleName, function () {
       beforeEach(function (done) {
         sinon.stub(Instance, 'findOne').yields(null, ctx.instance)
         sinon.stub(commonStream, 'checkOwnership').returns(Promise.resolve(true))
-        sinon.stub(Docker.prototype, 'getLogs').yields(null, {
-          on: sinon.spy(),
-          setEncoding: sinon.spy()
+        sinon.stub(Primus, 'createSocket').returns(function () {
+          this.substream = sinon.stub().returns({
+            on: sinon.spy(),
+            addListener: sinon.spy(),
+            setEncoding: sinon.spy()
+          })
         })
         done()
       })
       afterEach(function (done) {
-        Docker.prototype.getLogs.restore()
+        Instance.findOne.restore()
         done()
       })
       it('should allow logs when check ownership passes', function (done) {
-        logStream.logStreamHandler(ctx.socket, ctx.id, ctx.data)
+        terminalStream.proxyStreamHandler(ctx.socket, ctx.id, ctx.data)
           .then(function () {
             sinon.assert.calledOnce(commonStream.checkOwnership)
             sinon.assert.calledWith(commonStream.checkOwnership, ctx.sessionUser, ctx.instance)
-            sinon.assert.calledOnce(ctx.socket.substream)
-            sinon.assert.calledWith(ctx.socket.substream, ctx.data.containerId)
-            sinon.assert.calledOnce(Docker.prototype.getLogs)
-            sinon.assert.calledWith(
-              Docker.prototype.getLogs,
-              ctx.data.containerId,
-              sinon.match.any,
-              sinon.match.func
-            )
-            sinon.assert.calledOnce(ctx.socket.write)
-            sinon.assert.calledWith(ctx.socket.write, {
-              id: ctx.id,
-              event: 'LOG_STREAM_CREATED',
-              data: {
-                substreamId: ctx.data.containerId
-              }
-            })
+
+            sinon.assert.calledTwice(ctx.socket.substream)
+            sinon.assert.calledWith(ctx.socket.substream, ctx.data.terminalStreamId)
+            sinon.assert.calledWith(ctx.socket.substream, ctx.data.eventStreamId)
+
             done()
           })
       })
