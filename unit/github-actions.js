11e7049ef03661cb8d2ce8e142f1258afe344a04
Anton Podviaznikov <podviaznikov@gmail.com>
2016-01-20 14:57:01 -0800

    more unit tests

--- unit/github-actions.js
+++ unit/github-actions.js
@@ -29,7 +29,16 @@ describe('GitHub Actions: ' + moduleName, function () {
     it('should parse headCommit as {} if req.body.head_commit is null', function (done) {
       var req = {
         body: {
-          repository: 'podviaznikov/hellonode',
+          repository: {
+            id: 20736018,
+            name: 'api',
+            full_name: 'CodeNow/api',
+            owner: {
+              name: 'CodeNow',
+              email: 'live@codenow.com'
+            },
+            private: true
+          },
           ref: 'refs/heads/feature-1'
         }
       }
@@ -59,18 +68,83 @@ describe('GitHub Actions: ' + moduleName, function () {
     })
 
     it('should parse branch and default to [] for commmitLog', function (done) {
+      var headCommit = {
+        id: '77485a1a3c2fcf1a6db52e72bf1c05f40336d244',
+        distinct: true,
+        message: 'add whitelist check back to hooks',
+        timestamp: '2016-01-20T14:40:39-08:00',
+        url: 'https://github.com/CodeNow/api/commit/77485a1a3c2fcf1a6db52e72bf1c05f40336d244',
+        author: {
+          name: 'Anton Podviaznikov',
+          email: 'podviaznikov@gmail.com',
+          username: 'podviaznikov'
+        },
+        committer: {
+          name: 'Anton Podviaznikov',
+          email: 'podviaznikov@gmail.com',
+          username: 'podviaznikov'
+        },
+        added: [
+
+        ],
+        removed: [
+
+        ],
+        modified: [
+          'lib/routes/actions/github.js'
+        ]
+      }
+      var sender =  {
+        login: 'podviaznikov',
+        id: 429706,
+        avatar_url: 'https://avatars.githubusercontent.com/u/429706?v=3',
+        gravatar_id: '',
+        url: 'https://api.github.com/users/podviaznikov',
+        html_url: 'https://github.com/podviaznikov',
+        followers_url: 'https://api.github.com/users/podviaznikov/followers',
+        following_url: 'https://api.github.com/users/podviaznikov/following{/other_user}',
+        gists_url: 'https://api.github.com/users/podviaznikov/gists{/gist_id}',
+        starred_url: 'https://api.github.com/users/podviaznikov/starred{/owner}{/repo}',
+        subscriptions_url: 'https://api.github.com/users/podviaznikov/subscriptions',
+        organizations_url: 'https://api.github.com/users/podviaznikov/orgs',
+        repos_url: 'https://api.github.com/users/podviaznikov/repos',
+        events_url: 'https://api.github.com/users/podviaznikov/events{/privacy}',
+        received_events_url: 'https://api.github.com/users/podviaznikov/received_events',
+        type: 'User',
+        site_admin: false
+      }
       var req = {
         body: {
-          repository: 'podviaznikov/hellonode',
           ref: 'refs/heads/feature-1',
-          head_commit: {}
+          head_commit: headCommit,
+          commits: [headCommit],
+          sender: sender,
+          repository: {
+            id: 20736018,
+            name: 'api',
+            full_name: 'CodeNow/api',
+            owner: {
+              name: 'CodeNow',
+              email: 'live@codenow.com'
+            },
+            private: true
+          },
         }
       }
+
       var res = {}
       githubActions.parseGitHubPushData(req, res, function (err) {
         if (err) { return done(err) }
         expect(req.githubPushInfo.branch).to.equal('feature-1')
-        expect(req.githubPushInfo.commitLog.length).to.equal(0)
+        expect(req.githubPushInfo.repo).to.equal('CodeNow/api')
+        expect(req.githubPushInfo.repoName).to.equal('api')
+        expect(req.githubPushInfo.repoOwnerOrgName).to.equal('CodeNow')
+        expect(req.githubPushInfo.ref).to.equal(req.body.ref)
+        expect(req.githubPushInfo.headCommit).to.equal(headCommit)
+        expect(req.githubPushInfo.commit).to.equal(headCommit.id)
+        expect(req.githubPushInfo.commitLog.length).to.equal(1)
+        expect(req.githubPushInfo.commitLog[0]).to.equal(headCommit)
+        expect(req.githubPushInfo.user).to.equal(sender)
         done()
       })
     })
