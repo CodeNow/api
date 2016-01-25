049b047c7644bca339091d5ec77b167c8708615a
Bryan Kendall <bryan@runnable.com>
2016-01-25 13:44:05 -0800

    fix job schema validation to require the object

--- unit/workers/instance.delete.js
+++ unit/workers/instance.delete.js
@@ -90,7 +90,7 @@ describe('InstanceDelete: ' + moduleName, function () {
           expect(err).to.be.instanceOf(TaskFatalError)
           expect(err.data.validationError).to.exist()
           expect(err.data.validationError.message)
-            .to.contain('Value does not exist')
+            .to.match(/job.+required/)
           done()
         })
       })
