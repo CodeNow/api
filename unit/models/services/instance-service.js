19eaea171b1f65d585f0feae4112778abe934a5c
Anton Podviaznikov <podviaznikov@gmail.com>
2016-02-10 15:00:02 -0800

    use .markAsStopping

--- unit/models/services/instance-service.js
+++ unit/models/services/instance-service.js
@@ -8,6 +8,7 @@ var sinon = require('sinon')
 var Boom = require('dat-middleware').Boom
 var Code = require('code')
 var Promise = require('bluebird')
+require('sinon-as-promised')(Promise)
 
 var cleanMongo = require('../../../test/functional/fixtures/clean-mongo.js')
 var ContextVersion = require('models/mongo/context-version')
@@ -930,14 +931,14 @@ describe('InstanceService: ' + moduleName, function () {
   describe('stopInstance', function () {
     beforeEach(function (done) {
       sinon.stub(Instance.prototype, 'isNotStartingOrStoppingAsync').returns(Promise.resolve())
-      sinon.stub(Instance.prototype, 'setContainerStateToStoppingAsync').returns(Promise.resolve())
+      sinon.stub(Instance, 'markAsStoppingAsync').returns(Promise.resolve())
       sinon.stub(rabbitMQ, 'stopInstanceContainer').returns()
       done()
     })
 
     afterEach(function (done) {
       Instance.prototype.isNotStartingOrStoppingAsync.restore()
-      Instance.prototype.setContainerStateToStoppingAsync.restore()
+      Instance.markAsStoppingAsync.restore()
       rabbitMQ.stopInstanceContainer.restore()
       done()
     })
@@ -946,7 +947,7 @@ describe('InstanceService: ' + moduleName, function () {
       InstanceService.stopInstance({}, 21331).asCallback(function (err) {
         expect(err.message).to.equal('Instance does not have a container')
         sinon.assert.notCalled(Instance.prototype.isNotStartingOrStoppingAsync)
-        sinon.assert.notCalled(Instance.prototype.setContainerStateToStoppingAsync)
+        sinon.assert.notCalled(Instance.markAsStoppingAsync)
         sinon.assert.notCalled(rabbitMQ.stopInstanceContainer)
         done()
       })
@@ -961,22 +962,22 @@ describe('InstanceService: ' + moduleName, function () {
       InstanceService.stopInstance(instance, 21331).asCallback(function (err) {
         expect(err.message).to.equal(testErr.message)
         sinon.assert.calledOnce(Instance.prototype.isNotStartingOrStoppingAsync)
-        sinon.assert.notCalled(Instance.prototype.setContainerStateToStoppingAsync)
+        sinon.assert.notCalled(Instance.markAsStoppingAsync)
         sinon.assert.notCalled(rabbitMQ.stopInstanceContainer)
         done()
       })
     })
 
-    it('should fail setContainerStateToStoppingAsync failed', function (done) {
+    it('should fail markAsStoppingAsync failed', function (done) {
       var testErr = new Error('Mongo error')
       var rejectionPromise = Promise.reject(testErr)
       rejectionPromise.suppressUnhandledRejections()
-      Instance.prototype.setContainerStateToStoppingAsync.returns(rejectionPromise)
+      Instance.markAsStoppingAsync.returns(rejectionPromise)
       var instance = createNewInstance('testy', {})
       InstanceService.stopInstance(instance, 21331).asCallback(function (err) {
         expect(err.message).to.equal(testErr.message)
         sinon.assert.calledOnce(Instance.prototype.isNotStartingOrStoppingAsync)
-        sinon.assert.calledOnce(Instance.prototype.setContainerStateToStoppingAsync)
+        sinon.assert.calledOnce(Instance.markAsStoppingAsync)
         sinon.assert.notCalled(rabbitMQ.stopInstanceContainer)
         done()
       })
@@ -985,10 +986,12 @@ describe('InstanceService: ' + moduleName, function () {
     it('should pass', function (done) {
       var instance = createNewInstance('testy', {})
       var sessionUserGithubId = 21331
+      Instance.markAsStoppingAsync.returns(Promise.resolve(instance))
       InstanceService.stopInstance(instance, sessionUserGithubId).asCallback(function (err) {
         expect(err).to.not.exist()
         sinon.assert.calledOnce(Instance.prototype.isNotStartingOrStoppingAsync)
-        sinon.assert.calledOnce(Instance.prototype.setContainerStateToStoppingAsync)
+        sinon.assert.calledOnce(Instance.markAsStoppingAsync)
+        sinon.assert.calledWith(Instance.markAsStoppingAsync, instance._id, instance.container.dockerContainer)
         sinon.assert.calledOnce(rabbitMQ.stopInstanceContainer)
         sinon.assert.calledWith(rabbitMQ.stopInstanceContainer, {
           containerId: instance.container.dockerContainer,
