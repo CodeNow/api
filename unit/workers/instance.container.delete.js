0eacbbf0985de9976d0521df5a07c54ca94e3650
Bryan Kendall <bryan@runnable.com>
2016-02-11 10:05:20 -0800

    break up unit tests

--- unit/workers/instance.container.delete.js
+++ unit/workers/instance.container.delete.js
@@ -22,6 +22,7 @@ var expect = Code.expect
 var it = lab.it
 
 describe('instance.container.delete unit test', function () {
+  var testJob
   var testJobData = {
     container: {
       dockerContainer: 'dockerContainerTest'
@@ -34,227 +35,234 @@ describe('instance.container.delete unit test', function () {
     ownerGithubUsername: 'ownerGithubUsernameTest'
   }
 
-  var testJob
   beforeEach(function (done) {
     testJob = clone(testJobData)
+    sinon.stub(Docker.prototype, 'stopContainer').yieldsAsync()
+    sinon.stub(Docker.prototype, 'removeContainer').yieldsAsync()
+    sinon.stub(Hosts.prototype, 'removeHostsForInstance').yieldsAsync()
     done()
   })
 
-  describe('job validation', function () {
-    it('should throw if missing container', function (done) {
-      delete testJob.container
+  afterEach(function (done) {
+    Docker.prototype.stopContainer.restore()
+    Docker.prototype.removeContainer.restore()
+    Hosts.prototype.removeHostsForInstance.restore()
+    done()
+  })
 
-      InstanceContainerDelete(testJob).asCallback(function (err) {
-        expect(err).to.be.an.instanceof(TaskFatalError)
-        expect(err.message).to.match(/Invalid Job Data/)
+  describe('errors', function () {
+    describe('job validation', function () {
+      it('should throw if missing container', function (done) {
+        delete testJob.container
+
+        InstanceContainerDelete(testJob).asCallback(function (err) {
+          expect(err).to.be.an.instanceof(TaskFatalError)
+          expect(err.message).to.match(/Invalid Job Data/)
+          done()
+        })
+      })
+
+      it('should throw if missing dockerContainer', function (done) {
+        delete testJob.container.dockerContainer
+
+        InstanceContainerDelete(testJob).asCallback(function (err) {
+          expect(err).to.be.an.instanceof(TaskFatalError)
+          expect(err.message).to.match(/Invalid Job Data/)
+          done()
+        })
+      })
+
+      it('should throw if instanceMasterBranch not a string', function (done) {
+        testJob.instanceMasterBranch = 1234
+
+        InstanceContainerDelete(testJob).asCallback(function (err) {
+          expect(err).to.be.an.instanceof(TaskFatalError)
+          expect(err.message).to.match(/Invalid Job Data/)
+          done()
+        })
+      })
+
+      it('should throw if missing instanceMasterPod', function (done) {
+        delete testJob.instanceMasterPod
+
+        InstanceContainerDelete(testJob).asCallback(function (err) {
+          expect(err).to.be.an.instanceof(TaskFatalError)
+          expect(err.message).to.match(/Invalid Job Data/)
+          done()
+        })
+      })
+
+      it('should throw if missing instanceName', function (done) {
+        delete testJob.instanceName
+
+        InstanceContainerDelete(testJob).asCallback(function (err) {
+          expect(err).to.be.an.instanceof(TaskFatalError)
+          expect(err.message).to.match(/Invalid Job Data/)
+          done()
+        })
+      })
+
+      it('should throw if missing instanceShortHash', function (done) {
+        delete testJob.instanceShortHash
+
+        InstanceContainerDelete(testJob).asCallback(function (err) {
+          expect(err).to.be.an.instanceof(TaskFatalError)
+          expect(err.message).to.match(/Invalid Job Data/)
+          done()
+        })
+      })
+
+      it('should throw if missing ownerGithubId', function (done) {
+        delete testJob.ownerGithubId
+
+        InstanceContainerDelete(testJob).asCallback(function (err) {
+          expect(err).to.be.an.instanceof(TaskFatalError)
+          expect(err.message).to.match(/Invalid Job Data/)
+          done()
+        })
+      })
+
+      it('should throw if missing ownerGithubUsername', function (done) {
+        delete testJob.ownerGithubUsername
+
+        InstanceContainerDelete(testJob).asCallback(function (err) {
+          expect(err).to.be.an.instanceof(TaskFatalError)
+          expect(err.message).to.match(/Invalid Job Data/)
+          done()
+        })
+      })
+    }) // end job validation
+
+    describe('behavior errors', function () {
+      var testErr
+
+      beforeEach(function (done) {
+        testErr = new Error('zed')
         done()
       })
-    })
 
-    it('should throw if missing dockerContainer', function (done) {
-      delete testJob.container.dockerContainer
+      it('should throw error if removeHostsForInstance failed', function (done) {
+        Hosts.prototype.removeHostsForInstance.yieldsAsync(testErr)
+        InstanceContainerDelete(testJob).asCallback(function (err) {
+          expect(err.cause).to.deep.equal(testErr)
+          done()
+        })
+      })
 
-      InstanceContainerDelete(testJob).asCallback(function (err) {
-        expect(err).to.be.an.instanceof(TaskFatalError)
-        expect(err.message).to.match(/Invalid Job Data/)
-        done()
+      it('should throw error if stopContainer failed', function (done) {
+        Docker.prototype.stopContainer.yieldsAsync(testErr)
+        InstanceContainerDelete(testJob).asCallback(function (err) {
+          expect(err.cause).to.deep.equal(testErr)
+          done()
+        })
+      })
+
+      it('should throw error if stopContainer failed', function (done) {
+        Docker.prototype.removeContainer.yieldsAsync(testErr)
+        InstanceContainerDelete(testJob).asCallback(function (err) {
+          expect(err.cause).to.deep.equal(testErr)
+          done()
+        })
+      })
+
+      it('should throw task fatal if 404', function (done) {
+        testErr.output = { statusCode: 404 }
+        Docker.prototype.removeContainer.yieldsAsync(testErr)
+        InstanceContainerDelete(testJob).asCallback(function (err) {
+          expect(err).to.be.an.instanceof(TaskFatalError)
+          expect(err.message).to.match(/container not found/)
+          done()
+        })
       })
     })
-
-    it('should throw if instanceMasterBranch not a string', function (done) {
-      testJob.instanceMasterBranch = 1234
-
-      InstanceContainerDelete(testJob).asCallback(function (err) {
-        expect(err).to.be.an.instanceof(TaskFatalError)
-        expect(err.message).to.match(/Invalid Job Data/)
-        done()
-      })
-    })
-
-    it('should throw if missing instanceMasterPod', function (done) {
-      delete testJob.instanceMasterPod
-
-      InstanceContainerDelete(testJob).asCallback(function (err) {
-        expect(err).to.be.an.instanceof(TaskFatalError)
-        expect(err.message).to.match(/Invalid Job Data/)
-        done()
-      })
-    })
-
-    it('should throw if missing instanceName', function (done) {
-      delete testJob.instanceName
-
-      InstanceContainerDelete(testJob).asCallback(function (err) {
-        expect(err).to.be.an.instanceof(TaskFatalError)
-        expect(err.message).to.match(/Invalid Job Data/)
-        done()
-      })
-    })
-
-    it('should throw if missing instanceShortHash', function (done) {
-      delete testJob.instanceShortHash
-
-      InstanceContainerDelete(testJob).asCallback(function (err) {
-        expect(err).to.be.an.instanceof(TaskFatalError)
-        expect(err.message).to.match(/Invalid Job Data/)
-        done()
-      })
-    })
-
-    it('should throw if missing ownerGithubId', function (done) {
-      delete testJob.ownerGithubId
-
-      InstanceContainerDelete(testJob).asCallback(function (err) {
-        expect(err).to.be.an.instanceof(TaskFatalError)
-        expect(err.message).to.match(/Invalid Job Data/)
-        done()
-      })
-    })
-
-    it('should throw if missing ownerGithubUsername', function (done) {
-      delete testJob.ownerGithubUsername
-
-      InstanceContainerDelete(testJob).asCallback(function (err) {
-        expect(err).to.be.an.instanceof(TaskFatalError)
-        expect(err.message).to.match(/Invalid Job Data/)
-        done()
-      })
-    })
-  }) // end job validation
+  })
 
   describe('valid job', function () {
-    beforeEach(function (done) {
-      sinon.stub(Docker.prototype, 'stopContainer')
-      sinon.stub(Docker.prototype, 'removeContainer')
-      sinon.stub(Hosts.prototype, 'removeHostsForInstance')
-      done()
-    })
-
-    afterEach(function (done) {
-      Docker.prototype.stopContainer.restore()
-      Docker.prototype.removeContainer.restore()
-      Hosts.prototype.removeHostsForInstance.restore()
-      done()
-    })
-
-    it('should throw error if removeHostsForInstance failed', function (done) {
-      var testErr = new Error('zed')
-      Hosts.prototype.removeHostsForInstance.yieldsAsync(testErr)
-
+    it('should call removeHostsForInstance', function (done) {
       InstanceContainerDelete(testJob).asCallback(function (err) {
-        expect(testErr).to.deep.equal(err.cause)
+        expect(err).to.not.exist()
 
         sinon.assert.calledOnce(Hosts.prototype.removeHostsForInstance)
-        sinon.assert.calledWith(Hosts.prototype.removeHostsForInstance, {
-          ownerUsername: testJobData.ownerGithubUsername,
-          ownerGithub: testJobData.ownerGithubId,
-          branch: testJobData.instanceMasterBranch,
-          masterPod: testJobData.instanceMasterPod,
-          instanceName: testJobData.instanceName,
-          shortHash: testJobData.instanceShortHash
-        })
-
+        sinon.assert.calledWithExactly(
+          Hosts.prototype.removeHostsForInstance,
+          {
+            ownerUsername: testJobData.ownerGithubUsername,
+            ownerGithub: testJobData.ownerGithubId,
+            branch: testJobData.instanceMasterBranch,
+            masterPod: testJobData.instanceMasterPod,
+            instanceName: testJobData.instanceName,
+            shortHash: testJobData.instanceShortHash
+          },
+          testJob.container,
+          sinon.match.func
+        )
         done()
       })
     })
 
-    it('should throw error if stopContainer failed', function (done) {
-      var testErr = new Error('is')
-      Hosts.prototype.removeHostsForInstance.yieldsAsync()
-      Docker.prototype.stopContainer.yieldsAsync(testErr)
-
+    it('should call stopContainer', function (done) {
       InstanceContainerDelete(testJob).asCallback(function (err) {
-        expect(testErr).to.deep.equal(err.cause)
+        expect(err).to.not.exist()
 
         sinon.assert.calledOnce(Docker.prototype.stopContainer)
-        sinon.assert.calledWith(Docker.prototype.stopContainer, testJobData.container.dockerContainer)
-
+        sinon.assert.calledWithExactly(
+          Docker.prototype.stopContainer,
+          testJobData.container.dockerContainer,
+          true,
+          sinon.match.func
+        )
         done()
       })
     })
 
-    it('should throw error if stopContainer failed', function (done) {
-      var testErr = new Error('dead')
-      Hosts.prototype.removeHostsForInstance.yieldsAsync()
-      Docker.prototype.stopContainer.yieldsAsync()
-      Docker.prototype.removeContainer.yieldsAsync(testErr)
-
+    it('should call removeContainer', function (done) {
       InstanceContainerDelete(testJob).asCallback(function (err) {
-        expect(testErr).to.deep.equal(err.cause)
+        expect(err).to.not.exist()
 
         sinon.assert.calledOnce(Docker.prototype.removeContainer)
-        sinon.assert.calledWith(Docker.prototype.removeContainer, testJobData.container.dockerContainer)
-
+        sinon.assert.calledWithExactly(
+          Docker.prototype.removeContainer,
+          testJobData.container.dockerContainer,
+          sinon.match.func
+        )
         done()
       })
     })
 
-    it('should throw task fatal if 404', function (done) {
-      var testErr = {
-        output: {
-          statusCode: 404
-        }
-      }
-      Hosts.prototype.removeHostsForInstance.yieldsAsync()
-      Docker.prototype.stopContainer.yieldsAsync()
-      Docker.prototype.removeContainer.yieldsAsync(testErr)
+    it('should resolve (with whatever removeContainer returns)', function (done) {
+      var someObject = { some: 'object' }
+      Docker.prototype.removeContainer.yieldsAsync(null, someObject)
+      InstanceContainerDelete(testJob).asCallback(function (err, result) {
+        expect(err).to.not.exist()
 
-      InstanceContainerDelete(testJob).asCallback(function (err) {
-        expect(err).to.be.an.instanceof(TaskFatalError)
-        expect(err.message).to.match(/container not found/)
-
+        expect(result).to.equal(someObject)
         done()
       })
     })
 
-    it('should resolve', function (done) {
-      Hosts.prototype.removeHostsForInstance.yieldsAsync()
-      Docker.prototype.stopContainer.yieldsAsync()
-      Docker.prototype.removeContainer.yieldsAsync()
-
+    it('should call all these things in order', function (done) {
       InstanceContainerDelete(testJob).asCallback(function (err) {
-        if (err) { return done(err) }
+        expect(err).to.not.exist()
 
-        sinon.assert.calledOnce(Docker.prototype.removeContainer)
-        sinon.assert.calledWith(Docker.prototype.removeContainer, testJobData.container.dockerContainer)
-
-        sinon.assert.calledOnce(Docker.prototype.stopContainer)
-        sinon.assert.calledWith(Docker.prototype.stopContainer, testJobData.container.dockerContainer)
-
-        sinon.assert.calledOnce(Hosts.prototype.removeHostsForInstance)
-        sinon.assert.calledWith(Hosts.prototype.removeHostsForInstance, {
-          ownerUsername: testJobData.ownerGithubUsername,
-          ownerGithub: testJobData.ownerGithubId,
-          branch: testJobData.instanceMasterBranch,
-          masterPod: testJobData.instanceMasterPod,
-          instanceName: testJobData.instanceName,
-          shortHash: testJobData.instanceShortHash
-        })
-
+        sinon.assert.callOrder(
+          Hosts.prototype.removeHostsForInstance,
+          Docker.prototype.stopContainer,
+          Docker.prototype.removeContainer
+        )
         done()
       })
     })
 
     it('should resolve if missing instanceMasterBranch', function (done) {
       delete testJob.instanceMasterBranch
-
-      Hosts.prototype.removeHostsForInstance.yieldsAsync()
-      Docker.prototype.stopContainer.yieldsAsync()
-      Docker.prototype.removeContainer.yieldsAsync()
-
       InstanceContainerDelete(testJob).asCallback(function (err) {
-        if (err) { return done(err) }
+        expect(err).to.not.exist()
 
-        sinon.assert.calledOnce(Hosts.prototype.removeHostsForInstance)
-        sinon.assert.calledWith(Hosts.prototype.removeHostsForInstance, {
-          ownerUsername: testJobData.ownerGithubUsername,
-          ownerGithub: testJobData.ownerGithubId,
-          branch: undefined,
-          masterPod: testJobData.instanceMasterPod,
-          instanceName: testJobData.instanceName,
-          shortHash: testJobData.instanceShortHash
-        })
-
+        sinon.assert.callOrder(
+          Hosts.prototype.removeHostsForInstance,
+          Docker.prototype.stopContainer,
+          Docker.prototype.removeContainer
+        )
         done()
       })
     })
