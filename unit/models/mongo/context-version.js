8d89e38fe553c17186421a2bda6aa5100a1c547d
Anandkumar Patel <anand@runnable.com>
2016-02-15 12:09:13 -0800

    add test for new cv fun

--- unit/models/mongo/context-version.js
+++ unit/models/mongo/context-version.js
@@ -1272,4 +1272,45 @@ describe('Context Version: ' + moduleName, function () {
       })
     })
   })
+
+  describe('setBuildStarting', function () {
+    beforeEach(function (done) {
+      sinon.stub(ContextVersion, 'updateAsync')
+      done()
+    })
+
+    afterEach(function (done) {
+      ContextVersion.updateAsync.restore()
+      done()
+    })
+
+    it('should call updateAsync with correct params', function (done) {
+      var buildId = 54353
+      var dockerHost = 'https://10.0.0.9:4242'
+      var testOut = 'bloom'
+      ContextVersion.updateAsync.returns(testOut)
+
+      var out = ContextVersion.setBuildStarting(buildId, dockerHost)
+      expect(out).to.equal(testOut)
+      sinon.assert.calledOnce(ContextVersion.updateAsync)
+      sinon.assert.calledWith(ContextVersion.updateAsync, {
+        'build._id': buildId,
+        'build.finished': {
+          $exists: false
+        },
+        'build.started': {
+          $exists: true
+        },
+        state: { $ne: ContextVersion.buildStarted }
+      }, {
+        $set: {
+          state: ContextVersion.states.buildStarting,
+          dockerHost: dockerHost
+        }
+      }, {
+        multi: true
+      })
+      done()
+    })
+  }) // end setBuildStarting
 })
