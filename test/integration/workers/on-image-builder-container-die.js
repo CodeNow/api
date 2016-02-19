eeb2dee935b06457bd73abf6b1157096ce3ff07f
Nathan219 <Nathan219@gmail.com>
2016-02-18 16:40:27 -0800

    Fix integration tests

--- test/integration/workers/on-image-builder-container-die.js
+++ test/integration/workers/on-image-builder-container-die.js
@@ -152,7 +152,7 @@ describe('OnImageBuilderContainerDie Integration Tests', function () {
         sinon.spy(Build, 'updateFailedByContextVersionIds')
         sinon.spy(Build, 'updateCompletedByContextVersionIds')
         sinon.spy(ContextVersion, 'updateBuildErrorByContainer')
-        sinon.stub(User.prototype, 'findGithubUserByGithubId').yieldsAsync(null, {
+        sinon.stub(User, 'anonymousFindGithubUserByGithubId').yieldsAsync(null, {
           login: 'nathan219',
           avatar_url: 'testingtesting123'
         })
@@ -172,7 +172,7 @@ describe('OnImageBuilderContainerDie Integration Tests', function () {
         Build.updateFailedByContextVersionIds.restore()
         Build.updateCompletedByContextVersionIds.restore()
         ContextVersion.updateBuildErrorByContainer.restore()
-        User.prototype.findGithubUserByGithubId.restore()
+        User.anonymousFindGithubUserByGithubId.restore()
         done()
       })
       describe('With a successful build', function () {
