5d6f3b2b299b62144d0ff661126fdd2693026a51
Anandkumar Patel <anand@runnable.com>
2016-03-03 16:01:08 -0800

    make expect better

--- test/functional/builds-id-actions-build/post/201.js
+++ test/functional/builds-id-actions-build/post/201.js
@@ -313,7 +313,7 @@ function itShouldBuildTheBuild (ctx) {
               if (err) { return done(err) }
 
               var expectedBindsLength = 1
-              var expectedBindsValues = [new RegExp('/var/run/docker.sock:/var/run/docker.sock')]
+              var expectedBindsValues = [new RegExp('^/var/run/docker.sock:/var/run/docker.sock$')]
               if (process.env.DOCKER_IMAGE_BUILDER_CACHE) {
                 expectedBindsLength++
                 expectedBindsValues.push(new RegExp(process.env.DOCKER_IMAGE_BUILDER_CACHE + ':/cache:rw'))
