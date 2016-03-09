838a4ee0bce006ba44b5074eb140cee9d1431dd5
Myztiq <kahn@runnable.com>
2016-03-09 14:35:27 -0800

    Added CAP_DROP env.

--- unit/models/apis/docker.js
+++ unit/models/apis/docker.js
@@ -394,7 +394,7 @@ describe('docker: ' + moduleName, function () {
             Image: process.env.DOCKER_IMAGE_BUILDER_NAME + ':' + process.env.DOCKER_IMAGE_BUILDER_VERSION,
             Env: ctx.mockEnv,
             HostConfig: {
-              CapDrop: ['MKNOD', 'NET_BIND_SERVICE', 'NET_RAW', 'FSETID'],
+              CapDrop: process.env.CAP_DROP.split(','),
               Binds: ['/var/run/docker.sock:/var/run/docker.sock']
             },
             Labels: ctx.mockLabels
@@ -445,7 +445,7 @@ describe('docker: ' + moduleName, function () {
             Image: process.env.DOCKER_IMAGE_BUILDER_NAME + ':' + process.env.DOCKER_IMAGE_BUILDER_VERSION,
             Env: ctx.mockEnv,
             HostConfig: {
-              CapDrop: ['MKNOD', 'NET_BIND_SERVICE', 'NET_RAW', 'FSETID'],
+              CapDrop: process.env.CAP_DROP.split(','),
               Binds: ['/var/run/docker.sock:/var/run/docker.sock']
             },
             Labels: ctx.mockLabels
@@ -491,7 +491,7 @@ describe('docker: ' + moduleName, function () {
               Image: process.env.DOCKER_IMAGE_BUILDER_NAME + ':' + process.env.DOCKER_IMAGE_BUILDER_VERSION,
               Env: ctx.mockEnv,
               HostConfig: {
-                CapDrop: ['MKNOD', 'NET_BIND_SERVICE', 'NET_RAW', 'FSETID'],
+                CapDrop: process.env.CAP_DROP.split(','),
                 Binds: [
                   '/var/run/docker.sock:/var/run/docker.sock',
                   process.env.DOCKER_IMAGE_BUILDER_CACHE + ':/cache:rw',
@@ -890,7 +890,7 @@ describe('docker: ' + moduleName, function () {
             ]),
             Image: ctx.mockContextVersion.build.dockerTag,
             HostConfig: {
-              CapDrop: ['MKNOD', 'NET_BIND_SERVICE', 'NET_RAW', 'FSETID'],
+              CapDrop: process.env.CAP_DROP.split(','),
               PublishAllPorts: true,
               Memory: 1234
             }
@@ -1014,7 +1014,7 @@ describe('docker: ' + moduleName, function () {
         sinon.assert.calledWith(model.startContainer,
           testId, {
             HostConfig: {
-              CapDrop: ['MKNOD', 'NET_BIND_SERVICE', 'NET_RAW', 'FSETID'],
+              CapDrop: process.env.CAP_DROP.split(','),
               PublishAllPorts: true,
               Memory: 1234
             }
@@ -1038,7 +1038,7 @@ describe('docker: ' + moduleName, function () {
         sinon.assert.calledWith(model.startContainer,
           testId, {
             HostConfig: {
-              CapDrop: ['MKNOD', 'NET_BIND_SERVICE', 'NET_RAW', 'FSETID'],
+              CapDrop: process.env.CAP_DROP.split(','),
               PublishAllPorts: true,
               Memory: 1234
             }
@@ -1063,7 +1063,7 @@ describe('docker: ' + moduleName, function () {
         sinon.assert.calledWith(model.startContainer,
           testId, {
             HostConfig: {
-              CapDrop: ['MKNOD', 'NET_BIND_SERVICE', 'NET_RAW', 'FSETID'],
+              CapDrop: process.env.CAP_DROP.split(','),
               PublishAllPorts: true,
               Memory: 1234
             }
