538851da47b3189eecd1defa56f9eb292f2f20ab
Nathan Meyers <nathan@runnable.com>
2016-03-15 13:58:11 -0700

    FInished the integration tests

--- test/integration/models/instance-service.js
+++ test/integration/models/instance-service.js
@@ -8,18 +8,10 @@ var before = lab.before
 var beforeEach = lab.beforeEach
 var after = lab.after
 var afterEach = lab.afterEach
-var Code = require('code')
-var expect = Code.expect
 var sinon = require('sinon')
 var Promise = require('bluebird')
 var pluck = require('101/pluck')
-var noop = require('101/noop')
 
-var async = require('async')
-var error = require('error')
-
-var Instance = require('models/mongo/instance')
-var ContextVersion = require('models/mongo/context-version')
 var mongoFactory = require('../fixtures/factory')
 var mongooseControl = require('models/mongo/mongoose-control.js')
 var IsolationService = require('models/services/isolation-service.js')
@@ -28,8 +20,11 @@ require('sinon-as-promised')(require('bluebird'))
 describe('Instance Services Integration Tests', function () {
   before(mongooseControl.start)
   var ctx
+  var forked = {}
   beforeEach(function (done) {
     ctx = {}
+    forked = {}
+
     done()
   })
 
@@ -59,7 +54,7 @@ describe('Instance Services Integration Tests', function () {
         name: name,
         username: ctx.mockUsername,
         isolated: isolated
-      }, function (err, instance, build, cv) {
+      }, function (err, instance) {
         if (err) {
           return done(err)
         }
@@ -106,18 +101,28 @@ describe('Instance Services Integration Tests', function () {
   }
 
   /**
-   * Gets the actual dependency instances linked to a master
+   * Gets the actual dependency instances linked to a master.  If the instance is not in the provided
+   * FROM map, then just get it from ctx.  This helps testing since anything connected to something
+   * not in forked should still be connected to the originals
    * @param from {Object} ctx of forked
    * @param master
    * @returns {*}
    */
-  function getDependencyInstances(from, master) {
+  function getDependencyInstances (from, master) {
+    if (!dependencyMap[master]) { return null }
     return dependencyMap[master].map(function (depName) {
       return from[depName] || ctx[depName]
     })
   }
 
-  function makeDependecies(master, dependents) {
+  /**
+   * This function creates connections in Neo4J to simulate what our API does
+   * @param {Instance} master
+   * @param {[Instance]} dependents
+   * @returns {Promise}
+   */
+  function makeDependecies (master, dependents) {
+    if (!dependents) { return null }
     return Promise.each(dependents, function (dependentInstance) {
       return master.addDependencyAsync(
         dependentInstance,
@@ -125,7 +130,15 @@ describe('Instance Services Integration Tests', function () {
       )
     })
   }
-  function checkDependencies(master, dependents) {
+
+  /**
+   * This function checks Neo4J to see if all of the given instances are actually connected
+   * @param {Instance} master
+   * @param {[Instance]} dependents
+   * @returns {Promise}
+   */
+  function checkDependencies (master, dependents) {
+    if (!master) { return null }
     return master.getDependenciesAsync()
       .then(function (nodeArray) {
         return nodeArray.map(pluck('name'))
@@ -139,17 +152,43 @@ describe('Instance Services Integration Tests', function () {
       })
       .then(function (missingDependencies) {
         if (missingDependencies) {
-          throw new Error('The dependencies for ' + master.name + ' were all wrong!' +
+          throw new Error('The dependencies for ' + master.name + ' were all wrong! Missing these: ' +
             JSON.stringify(missingDependencies.map(pluck('name')))
           )
         }
       })
   }
 
+  function createForks (masterName, childNameArray) {
+    function createInstance (instanceName, isMaster) {
+      var dashes = isMaster ? '-' : '--'
+      return createNewInstanceAsync(ctx.Frontend.shortHash + dashes + instanceName, 'asdasdasdas')
+        .then(function (instanceModel) {
+          forked[instanceName] = instanceModel
+          sinon.stub(instanceModel, 'getMainBranchName').returns('branch1')
+          return instanceModel
+        })
+    }
+    return createInstance(masterName, true)
+      .then(function () {
+        return Promise.map(childNameArray, function (instanceName) {
+          return createInstance(instanceName)
+        })
+      })
+      .then(function connectAllForkedDepsToOrginalUnforked(childInstanceArray) {
+        // This emulates when each of these instances get configured from their envs
+        return Promise.each(Object.keys(forked), function (instanceName) {
+          // It should take these newly forked instances, and bind them to the original master
+          // (ctx) branches.
+          return makeDependecies(forked[instanceName], getDependencyInstances(ctx, instanceName))
+        })
+          .return(childInstanceArray)
+      })
+  }
+
   describe('_updateDependenciesForInstanceWithChildren', function () {
     describe('Master\'s dependecies', function () {
       var toFork = ['Api', 'Link', 'MongoDB']
-      var forked = {}
       beforeEach(function (done) {
         Promise.each(toFork, function (instanceName) {
           return createNewInstanceAsync(ctx.Frontend.shortHash + '--' + instanceName, 'asdasdasdas')
@@ -160,7 +199,7 @@ describe('Instance Services Integration Tests', function () {
         })
           .asCallback(done)
       })
-      it('should ', function (done) {
+      it('should connect iFrontend to iApi', function (done) {
         IsolationService._updateDependenciesForInstanceWithChildren(ctx.Frontend, [forked.Api])
           .then(function () {
             return Promise.each(Object.keys(dependencyMap), function (instanceName) {
@@ -172,7 +211,7 @@ describe('Instance Services Integration Tests', function () {
           })
           .asCallback(done)
       })
-      it('should handle a lot of isolation', function (done) {
+      it('should connect iApi to iLink and iMongo', function (done) {
         // first fork Link, then master
         var children = [forked.Link, forked.MongoDB]
         IsolationService._updateDependenciesForInstanceWithChildren(ctx.Api, children)
@@ -187,59 +226,18 @@ describe('Instance Services Integration Tests', function () {
           .asCallback(done)
       })
     })
-    //describe('Has Dependencies, but not of the master', function () {
-    //  var forkedLink = null
-    //  beforeEach(function (done) {
-    //    createNewInstance(ctx.Frontend.shortHash + '--Link')(function (err, link) {
-    //      forkedLink = link
-    //      sinon.stub(forkedLink, 'getMainBranchName').returns('branch1')
-    //      done()
-    //    })
-    //  })
-    //  it('should handle when the master does not depend on any of the included isolated children', function (done) {
-    //    // first fork Link, then master
-    //
-    //    IsolationService._updateDependenciesForInstanceWithChildren(ctx.Frontend, [forkedLink])
-    //      .then(function () {
-    //
-    //      })
-    //      .asCallback(done)
-    //  })
-    //})
   })
   describe('updateDependenciesForIsolation', function () {
-    describe('Master\'s dependecies', function () {
-      var forked = {}
-      var forkedDependencyMap = {}
-      var instanceNameArray = ['Frontend', 'Api', 'Link', 'RabbitMQ', 'MongoDB']
-      beforeEach(function (done) {
-        // Fork Frontend
-        return Promise.each(instanceNameArray, function (instanceName) {
-          var doubleDashes = instanceName === 'Frontend' ? '-' : '--'
-          return createNewInstanceAsync(ctx.Frontend.shortHash + doubleDashes + instanceName, 'asdasdasdas')
-            .then(function (instanceModel) {
-              forked[instanceName] = instanceModel
-              sinon.stub(instanceModel, 'getMainBranchName').returns('branch1')
-            })
-          })
-          .asCallback(done)
-      })
-      beforeEach(function connectAllForkedDepsToOrginalUnforked (done) {
-        // This emulates when each of these instances get configured from their envs
-        Promise.each(Object.keys(dependencyMap), function (instanceName) {
-          // It should take these newly forked instances, and bind them to the original master
-          // (ctx) branches.
-          return makeDependecies(forked[instanceName], getDependencyInstances(ctx, instanceName))
-        })
-        .asCallback(done)
-      })
+    var forkedDependencyMap = {}
+    describe('Forking the top of the pod', function () {
       it('should connect Frontend to the new Api server', function (done) {
         forkedDependencyMap = {
           Frontend: ['Api']
         }
-        var forkedInstances = ['Frontend', ]
-        var isolatedChildren = [forked.Api, forked.Link, forked.RabbitMQ]
-        IsolationService.updateDependenciesForIsolation(forked.Frontend, [forked.Api])
+        createForks('Frontend', ['Api'])
+          .then(function (isolatedChildrenArray) {
+            return IsolationService.updateDependenciesForIsolation(forked.Frontend, isolatedChildrenArray)
+          })
           .then(function () {
             // Make sure the original mappings are still intact
             return Promise.each(Object.keys(dependencyMap), function (instanceName) {
@@ -253,7 +251,7 @@ describe('Instance Services Integration Tests', function () {
           .then(function () {
             // Check the forked Api (since we didn't send any of the other forks with
             // updateDependenciesForIsolation, none of it's
-            return checkDependencies(forked.Api, getDependencyInstances(ctx, 'Api'))
+            return checkDependencies(forked.Api, getDependencyInstances(forked, 'Api'))
           })
           .asCallback(done)
       })
@@ -264,9 +262,11 @@ describe('Instance Services Integration Tests', function () {
           Api: ['Link'], // Mongo isn't in here because it's going to be loaded from the unforked
           Link: ['RabbitMQ']
         }
-        var isolatedChildren = [forked.Api, forked.Link, forked.RabbitMQ]
         // first fork Link, then master
-        IsolationService.updateDependenciesForIsolation(forked.Frontend, isolatedChildren)
+        createForks('Frontend', ['Api', 'Link', 'RabbitMQ'])
+          .then(function (isolatedChildrenArray) {
+            return IsolationService.updateDependenciesForIsolation(forked.Frontend, isolatedChildrenArray)
+          })
           .then(function () {
             // Make sure the original mappings are still intact
             return Promise.each(Object.keys(dependencyMap), function (instanceName) {
@@ -274,19 +274,24 @@ describe('Instance Services Integration Tests', function () {
             })
           })
           .then(function () {
-            return Promise.each(Object.keys(forkedDependencyMap), function (instanceName) {
-              return checkDependencies(forked[instanceName], getDependencyInstances(forked, instanceName))
+            return Promise.each(Object.keys(forkedDependencyMap), function (isolatedInstanceName) {
+              return checkDependencies(
+                forked[isolatedInstanceName],
+                getDependencyInstances(forked, isolatedInstanceName)
+              )
             })
           })
           .asCallback(done)
       })
-      it('should connect links not connected to the isolation master', function (done) {
+      it('should connect iLink and iRabbit', function (done) {
         forkedDependencyMap = {
           Frontend: [/* Empty, since api isn't getting forked */],
           Link: ['RabbitMQ']
         }
-        // first fork Link, then master
-        IsolationService.updateDependenciesForIsolation(forked.Frontend, [forked.Link, forked.RabbitMQ])
+        createForks('Frontend', ['Link', 'RabbitMQ'])
+          .then(function (isolatedChildrenArray) {
+            return IsolationService.updateDependenciesForIsolation(forked.Frontend, isolatedChildrenArray)
+          })
           .then(function () {
             // Make sure the original mappings are still intact
             return Promise.each(Object.keys(dependencyMap), function (instanceName) {
@@ -294,31 +299,137 @@ describe('Instance Services Integration Tests', function () {
             })
           })
           .then(function () {
-            return Promise.each(Object.keys(forkedDependencyMap), function (instanceName) {
-              return checkDependencies(forked[instanceName], getDependencyInstances(forked, instanceName))
+            return Promise.each(Object.keys(forkedDependencyMap), function (isolatedInstanceName) {
+              return checkDependencies(
+                forked[isolatedInstanceName],
+                getDependencyInstances(forked, isolatedInstanceName)
+              )
+            })
+          })
+          .asCallback(done)
+      })
+      it('should connect nothing to iRabbit', function (done) {
+        forkedDependencyMap = {
+          Frontend: [/* Empty, since api isn't getting forked */],
+          Link: ['RabbitMQ']
+        }
+        createForks('Frontend', ['Link', 'RabbitMQ'])
+          .then(function (isolatedChildrenArray) {
+            return IsolationService.updateDependenciesForIsolation(forked.Frontend, isolatedChildrenArray)
+          })
+          .then(function () {
+            // Make sure the original mappings are still intact
+            return Promise.each(Object.keys(dependencyMap), function (instanceName) {
+              return checkDependencies(ctx[instanceName], getDependencyInstances(ctx, instanceName))
+            })
+          })
+          .then(function () {
+            return Promise.each(Object.keys(forkedDependencyMap), function (isolatedInstanceName) {
+              return checkDependencies(
+                forked[isolatedInstanceName],
+                getDependencyInstances(forked, isolatedInstanceName)
+              )
+            })
+          })
+          .asCallback(done)
+      })
+      it('should connect iApi to iMongo, but Link should stay with Mongo', function (done) {
+        forkedDependencyMap = {
+          Frontend: ['Api'],
+          Api: ['MongoDB']
+        }
+        createForks('Frontend', ['Api', 'MongoDB'])
+          .then(function (isolatedChildrenArray) {
+            return IsolationService.updateDependenciesForIsolation(forked.Frontend, isolatedChildrenArray)
+          })
+          .then(function () {
+            // Make sure the original mappings are still intact
+            return Promise.each(Object.keys(dependencyMap), function (instanceName) {
+              return checkDependencies(ctx[instanceName], getDependencyInstances(ctx, instanceName))
+            })
+          })
+          .then(function () {
+            return Promise.each(Object.keys(forkedDependencyMap), function (isolatedInstanceName) {
+              return checkDependencies(
+                forked[isolatedInstanceName],
+                getDependencyInstances(forked, isolatedInstanceName)
+              )
+            })
+          })
+          .then(function () {
+            // Just to be sure iApi is connected to iMongo
+            return checkDependencies(forked.Api, [ctx.Link, forked.MongoDB])
+          })
+          .then(function () {
+            // Just to be sure Link is still with Mongo
+            return checkDependencies(ctx.Link, [ctx.MongoDB])
+          })
+          .asCallback(done)
+      })
+      it('should connect iLink to iMongo, but Api should stay with Mongo', function (done) {
+        forkedDependencyMap = {
+          Frontend: [/* Empty, since api isn't getting forked */],
+          Link: ['MongoDB']
+        }
+        createForks('Frontend', ['Link', 'MongoDB'])
+          .then(function (isolatedChildrenArray) {
+            return IsolationService.updateDependenciesForIsolation(forked.Frontend, isolatedChildrenArray)
+          })
+          .then(function () {
+            // Make sure the original mappings are still intact
+            return Promise.each(Object.keys(dependencyMap), function (instanceName) {
+              return checkDependencies(ctx[instanceName], getDependencyInstances(ctx, instanceName))
+            })
+          })
+          .then(function () {
+            return Promise.each(Object.keys(forkedDependencyMap), function (isolatedInstanceName) {
+              return checkDependencies(
+                forked[isolatedInstanceName],
+                getDependencyInstances(forked, isolatedInstanceName)
+              )
+            })
+          })
+          .then(function () {
+            // Just to be sure Api is connected to Mongo
+            return checkDependencies(ctx.Api, [ctx.Link, ctx.MongoDB])
+          })
+          .then(function () {
+            // Just to be sure Link is still with Mongo
+            return checkDependencies(forked.Link, [forked.MongoDB])
+          })
+          .asCallback(done)
+      })
+    })
+
+    describe('Forking from the middle, Api', function () {
+      it('should connect all isolated instances except Mongo', function (done) {
+        // This is what the dependency map should look like for the forked instances
+        forkedDependencyMap = {
+          Api: ['Link'],
+          Link: ['RabbitMQ']
+        }
+        // first fork Link, then master
+
+        createForks('Api', ['Link', 'MongoDB', 'RabbitMQ'])
+          .then(function (isolatedChildrenArray) {
+            return IsolationService.updateDependenciesForIsolation(forked.Api, isolatedChildrenArray)
+          })
+          .then(function () {
+            // Make sure the original mappings are still intact
+            return Promise.each(Object.keys(dependencyMap), function (instanceName) {
+              return checkDependencies(ctx[instanceName], getDependencyInstances(ctx, instanceName))
+            })
+          })
+          .then(function () {
+            return Promise.each(Object.keys(forkedDependencyMap), function (isolatedInstanceName) {
+              return checkDependencies(
+                forked[isolatedInstanceName],
+                getDependencyInstances(forked, isolatedInstanceName)
+              )
             })
           })
           .asCallback(done)
       })
     })
-    //describe('Has Dependencies, but not of the master', function () {
-    //  var forkedLink = null
-    //  beforeEach(function (done) {
-    //    createNewInstance(ctx.Frontend.shortHash + '--Link')(function (err, link) {
-    //      forkedLink = link
-    //      sinon.stub(forkedLink, 'getMainBranchName').returns('branch1')
-    //      done()
-    //    })
-    //  })
-    //  it('should handle when the master does not depend on any of the included isolated children', function (done) {
-    //    // first fork Link, then master
-    //
-    //    IsolationService._updateDependenciesForInstanceWithChildren(ctx.Frontend, [forkedLink])
-    //      .then(function () {
-    //
-    //      })
-    //      .asCallback(done)
-    //  })
-    //})
   })
 })
