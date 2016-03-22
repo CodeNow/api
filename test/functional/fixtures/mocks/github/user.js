836ae1cc82683ef559015c417ecd0d896467365e
Anton Podviaznikov <podviaznikov@gmail.com>
2016-03-22 16:45:20 -0700

    minor

--- test/functional/fixtures/mocks/github/user.js
+++ test/functional/fixtures/mocks/github/user.js
@@ -50,7 +50,7 @@ module.exports = function (userId, username, token) {
       'company': '',
       'blog': 'http://twitter.com/tjmehta',
       'location': 'San Francisco',
-      'email': username + '@runnable.com',
+      'email': '',
       'emails': [
         {
           value: username + '@runnable.com',
