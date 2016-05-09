65b902c035f175abed8b78972816d71a2f113f07
thejsj <jorge.silva@thejsj.com>
2016-05-08 23:14:36 -0700

    Remove unsued instance method

--- unit/models/mongo/instances.js
+++ unit/models/mongo/instances.js
@@ -338,67 +338,6 @@ describe('Instance Model Tests ' + moduleName, function () {
     })
   }) // end findInstancesBuiltByDockerHost
 
-  describe('#setStoppingAsStoppedByDockerHost', function () {
-    var dockerHost = 'http://10.0.0.1:4242'
-    beforeEach(function (done) {
-      sinon.stub(Instance, 'update')
-      done()
-    })
-    afterEach(function (done) {
-      Instance.update.restore()
-      done()
-    })
-    describe('if mongo passes', function () {
-      beforeEach(function (done) {
-        Instance.update.yieldsAsync()
-        done()
-      })
-
-      it('should call update with the right parameters', function (done) {
-        Instance.setStoppingAsStoppedByDockerHost(dockerHost, function (err) {
-          expect(err).to.not.exist()
-          sinon.assert.calledOnce(Instance.update)
-          sinon.assert.calledWith(Instance.update,
-            {
-              'container.dockerHost': dockerHost,
-              'container.inspect.State.Stopping': true
-            }, {
-              $set: {
-                'container.inspect.State.Pid': 0,
-                'container.inspect.State.Running': false,
-                'container.inspect.State.Restarting': false,
-                'container.inspect.State.Paused': false,
-                'container.inspect.State.FinishedAt': sinon.match.string,
-                'container.inspect.State.ExitCode': 0
-              },
-              $unset: {
-                'container.inspect.State.Stopping': ''
-              }
-            }, {
-              multi: true
-            }
-          )
-          done()
-        })
-      })
-    })
-
-    describe('on mongoError', function () {
-      var error = 'heaters'
-      beforeEach(function (done) {
-        Instance.update.yieldsAsync(error)
-        done()
-      })
-      it('should propegate the error to the cb function', function (done) {
-        Instance.setStoppingAsStoppedByDockerHost(dockerHost, function (err) {
-          expect(err).to.equal(error)
-          sinon.assert.calledOnce(Instance.update)
-          done()
-        })
-      })
-    })
-  })
-
   describe('save', function () {
     it('should not save an instance with the same (lower) name and owner', function (done) {
       var instance = mongoFactory.createNewInstance('hello')
