f36d96e4cc3d1e1ec3114ecd09987b730521afbf
Nathan219 <Nathan219@gmail.com>
2016-05-12 12:57:15 -0700

    make the script be not shitty

--- scripts/migration-hostname/update-instances-with-hostname.js
+++ scripts/migration-hostname/update-instances-with-hostname.js
@@ -1,71 +1,63 @@
 'use strict'
-
+/**
+ * This script updates all instances to save their hostname (and elasticHostname) to the database.
+ * It also saves the name of the owner from github (if it wasn't already there)
+ */
 require('loadenv')()
 var Instances = require('models/mongo/instance')
 var keypather = require('keypather')()
-var async = require('async')
 var mongoose = require('mongoose')
 var Github = require('models/apis/github')
 mongoose.connect(process.env.MONGO)
+var Promise = require('bluebird')
 
 var dryRun = !process.env.ACTUALLY_RUN
 
 console.log('dryRun?', !!dryRun)
 
-async.waterfall([
-  function findAllInstances (cb) {
-    Instances.find({}, cb)
-  },
-  function addElasticHostname (instances, cb) {
-    console.log('updating instances:', instances.length)
-    async.eachLimit(
-      instances,
-      50,
-      function (i, eachCb) {
-        var id = i._id.toString()
-        if (dryRun) {
-          eachCb()
+Instances.findAsync({})
+  .each(function (i) {
+    return Promise.try(function () {
+      if (keypather.get(i, 'owner.username')) {
+        return i.owner.username
+      }
+      if (!keypather.get(i, 'owner.github')) {
+        return
+      }
+      Promise.promisifyAll(Github)
+      Promise.promisifyAll(Github.prototype)
+      var github = new Github()
+      return github.getUserByIdAsync(i.owner.github)
+        .get('login')
+        .tap(function (name) {
+          console.log('name', name);
+        })
+        .catch(function (err) {
+          return
+        })
+    })
+      .then(function (username) {
+        if (dryRun || !username) {
+          console.log('Skipped instance ' + i.name);
+          return
         } else {
-          if (keypather.get(i, 'owner.username')) {
-            var hostname = i.getElasticHostname(i.owner.username)
-            Instances.findByIdAndUpdate(
-              id,
-              {
-                $set: {
-                  hostname: hostname,
-                  elasticHostname: hostname
-                }
-              }, eachCb)
-          } else if (keypather.get(i, 'owner.github')) {
-            var github = new Github()
-            github.getUserById(i.owner.github, function (err, user) {
-              if (err || !user) {
-                console.log('Github query failed for ', i.owner.github)
-                return eachCb()
-              } else {
-                var hostname = i.getElasticHostname(user.login)
-                Instances.findByIdAndUpdate(
-                  id,
-                  {
-                    $set: {
-                      'owner.username': user.login,
-                      hostname: hostname,
-                      elasticHostname: hostname
-                    }
-                  }, eachCb)
-              }
-            })
-          } else {
-            eachCb()
-          }
+          var hostname = i.getElasticHostname(username)
+          var id = i._id.toString()
+          return Instances.findByIdAndUpdateAsync(id, {
+            $set: {
+              'owner.username': username,
+              hostname: hostname,
+              elasticHostname: hostname
+            }
+          })
         }
-      }, cb)
-  }
-], function (err) {
-  console.log('done.')
-  if (err) {
+      })
+  })
+  .then(function () {
+    console.log('done.')
+    process.exit(0)
+  })
+  .catch(function (err) {
     console.error('error happened', err)
     return process.exit(1)
-  }
-  process.exit(0)
-})
+  })
