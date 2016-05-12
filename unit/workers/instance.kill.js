a5389cedd5e3e88bac0c2c6b8aa966eb69e2d959
Myztiq <kahn@runnable.com>
2016-05-12 11:05:08 -0700

    Added instance.kill worker

---
+++ unit/workers/instance.kill.js
@@ -0,0 +1,184 @@
+/**
+ * @module unit/workers/instance.kill
+ */
+'use strict'
+
+var Lab = require('lab')
+var lab = exports.lab = Lab.script()
+
+var omit = require('101/omit')
+var Code = require('code')
+var sinon = require('sinon')
+require('sinon-as-promised')(require('bluebird'))
+
+var Docker = require('models/apis/docker')
+var Worker = require('workers/instance.kill')
+var Instance = require('models/mongo/instance')
+var InstanceService = require('models/services/instance-service')
+
+var TaskFatalError = require('ponos').TaskFatalError
+var afterEach = lab.afterEach
+var beforeEach = lab.beforeEach
+var describe = lab.describe
+var expect = Code.expect
+var it = lab.it
+
+describe('Workers: Instance Stop', function () {
+  var testInstanceId = '5633e9273e2b5b0c0077fd41'
+  var dockerContainer = '46080d6253c8db55b8bbb9408654896964b86c63e863f1b3b0301057d1ad92ba'
+  var testData = {
+    instanceId: testInstanceId,
+    containerId: dockerContainer
+  }
+  var testInstance = new Instance({
+    _id: testInstanceId,
+    name: 'name1',
+    shortHash: 'asd51a1',
+    masterPod: true,
+    owner: {
+      github: 124,
+      username: 'codenow',
+      gravatar: ''
+    },
+    createdBy: {
+      github: 125,
+      username: 'runnabear',
+      gravatar: ''
+    },
+    container: {
+      dockerContainer: dockerContainer
+    },
+    network: {
+      hostIp: '0.0.0.0'
+    },
+    build: '507f191e810c19729de860e2',
+    contextVersion: {
+      appCodeVersions: [
+        {
+          lowerBranch: 'develop',
+          additionalRepo: false
+        }
+      ]
+    }
+  })
+  beforeEach(function (done) {
+    sinon.stub(Instance, 'findOneStoppingAsync').resolves(testInstance)
+    sinon.stub(Docker.prototype, 'killContainerAsync').resolves()
+    sinon.stub(InstanceService, 'emitInstanceUpdate').resolves()
+    done()
+  })
+
+  afterEach(function (done) {
+    Instance.findOneStoppingAsync.restore()
+    Docker.prototype.killContainerAsync.restore()
+    InstanceService.emitInstanceUpdate.restore()
+    done()
+  })
+
+  describe('validation', function () {
+    it('should fatally fail if job is null', function (done) {
+      Worker(null).asCallback(function (err) {
+        expect(err).to.exist()
+        expect(err).to.be.an.instanceOf(TaskFatalError)
+        expect(err.message).to.equal('instance.kill: Invalid Job')
+        done()
+      })
+    })
+    it('should fatally fail if job is {}', function (done) {
+      Worker({}).asCallback(function (err) {
+        expect(err).to.exist()
+        expect(err).to.be.an.instanceOf(TaskFatalError)
+        expect(err.message).to.equal('instance.kill: Invalid Job')
+        done()
+      })
+    })
+    it('should fatally fail if job has no instanceId', function (done) {
+      var data = omit(testData, 'instanceId')
+      Worker(data).asCallback(function (err) {
+        expect(err).to.exist()
+        expect(err).to.be.an.instanceOf(TaskFatalError)
+        expect(err.message).to.equal('instance.kill: Invalid Job')
+        done()
+      })
+    })
+    it('should fatally fail if job has no containerId', function (done) {
+      var data = omit(testData, 'containerId')
+      Worker(data).asCallback(function (err) {
+        expect(err).to.exist()
+        expect(err).to.be.an.instanceOf(TaskFatalError)
+        expect(err.message).to.equal('instance.kill: Invalid Job')
+        done()
+      })
+    })
+  })
+  it('should fail if findOneStoppingAsync failed', function (done) {
+    var error = new Error('Mongo error')
+    Instance.findOneStoppingAsync.rejects(error)
+    Worker(testData).asCallback(function (err) {
+      expect(err).to.exist()
+      expect(err.message).to.equal(error.message)
+      done()
+    })
+  })
+  it('should fail fatally if findOneStoppingAsync returned no instance', function (done) {
+    Instance.findOneStoppingAsync.resolves(null)
+    Worker(testData).asCallback(function (err) {
+      expect(err).to.exist()
+      expect(err).to.be.instanceOf(TaskFatalError)
+      expect(err.message).to.equal('instance.kill: Instance not found')
+      done()
+    })
+  })
+  it('should fail if docker stopContainer failed', function (done) {
+    var error = new Error('Docker error')
+    Docker.prototype.killContainerAsync.rejects(error)
+    Worker(testData).asCallback(function (err) {
+      expect(err).to.exist()
+      expect(err.message).to.equal(error.message)
+      done()
+    })
+  })
+  it('should fail if sending events failed', function (done) {
+    var error = new Error('Primus error')
+    InstanceService.emitInstanceUpdate.rejects(error)
+    Worker(testData).asCallback(function (err) {
+      expect(err).to.exist()
+      expect(err.message).to.equal(error.message)
+      done()
+    })
+  })
+  it('should call findOneStoppingAsync', function (done) {
+    Worker(testData).asCallback(function (err) {
+      expect(err).to.not.exist()
+      sinon.assert.calledOnce(Instance.findOneStoppingAsync)
+      sinon.assert.calledWith(Instance.findOneStoppingAsync, testInstanceId, dockerContainer)
+      done()
+    })
+  })
+  it('should call killContainer', function (done) {
+    Worker(testData).asCallback(function (err) {
+      expect(err).to.not.exist()
+      sinon.assert.calledOnce(Docker.prototype.killContainerAsync)
+      sinon.assert.calledWith(Docker.prototype.killContainerAsync, dockerContainer)
+      done()
+    })
+  })
+  it('should call emitInstanceUpdate', function (done) {
+    Worker(testData).asCallback(function (err) {
+      expect(err).to.not.exist()
+      sinon.assert.calledOnce(InstanceService.emitInstanceUpdate)
+      sinon.assert.calledWith(InstanceService.emitInstanceUpdate, testInstance, null, 'stopping', true)
+      done()
+    })
+  })
+  it('should call out to various models and helper methods in the correct order', function (done) {
+    Worker(testData).asCallback(function (err) {
+      expect(err).to.not.exist()
+      sinon.assert.callOrder(
+        Instance.findOneStoppingAsync,
+        InstanceService.emitInstanceUpdate,
+        Docker.prototype.killContainerAsync)
+      done()
+    })
+  })
+})
