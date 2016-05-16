e61d1ca13e4a1871ad2f68bba2532dd2eaa86591
Myztiq <kahn@runnable.com>
2016-05-16 11:12:42 -0700

    Fixed typo!

--- unit/workers/isolation.redeploy.js
+++ unit/workers/isolation.redeploy.js
@@ -23,7 +23,7 @@ var describe = lab.describe
 var expect = Code.expect
 var it = lab.it
 
-describe('Workers: Isolation Kill', function () {
+describe('Workers: Isolation Redeploy', function () {
   var testIsolationId = '5633e9273e2b5b0c0077fd41'
   var testData = {
     isolationId: testIsolationId
