6075471bf5698d80c90ec6ecca7fdf7e5d05df39
Anton Podviaznikov <podviaznikov@gmail.com>
2016-05-26 16:20:09 -0700

    unit tests migrations

--- test/integration/models/mongo/schemas/user.js
+++ test/integration/models/mongo/schemas/user.js
@@ -5,18 +5,33 @@
 
 var Code = require('code')
 var Lab = require('lab')
-var path = require('path')
-
+var Faker = require('faker')
 var lab = exports.lab = Lab.script()
 
 var describe = lab.describe
 var it = lab.it
 var expect = Code.expect
+var before = lab.before
+var after = lab.after
+var afterEach = lab.afterEach
 
+var validation = require('../../../fixtures/validation')(lab)
+var mongooseControl = require('models/mongo/mongoose-control.js')
 var UserSchema = require('models/mongo/schemas/user')
+var User = require('models/mongo/user')
 
-var moduleName = path.relative(process.cwd(), __filename)
-describe('User Schema: ' + moduleName, function () {
+describe('User Schema Integration Tests', function () {
+  before(mongooseControl.start)
+
+  afterEach(function (done) {
+    User.remove({}, done)
+  })
+
+  after(function (done) {
+    User.remove({}, done)
+  })
+  after(mongooseControl.stop)
+
   describe('_transformToJSON', function () {
     it('should strip sensitive properties', function (done) {
       var ret = {
@@ -36,5 +51,41 @@ describe('User Schema: ' + moduleName, function () {
       expect(res.accounts.github.refreshToken).to.not.exist()
       done()
     })
+  })
+  function createNewUser () {
+    return new User({
+      email: Faker.Internet.email(),
+      accounts: {
+        github: {
+          username: 'test',
+          accessToken: 'test',
+          refreshToken: 'test',
+          id: 'test',
+          _json: {},
+          emails: [Faker.Internet.email(), Faker.Internet.email()]
+        }
+      },
+      show_email: false,
+      created: Date.now(),
+      context: validation.VALID_OBJECT_ID,
+      files: [{
+        Key: 'test',
+        ETag: 'test',
+        VersionId: validation.VALID_OBJECT_ID
+      }],
+      build: {
+        dockerImage: 'testing',
+        dockerTag: 'adsgasdfgasdf'
+      }
+    })
+  }
+
+  describe('Email Validation', function () {
+    validation.emailValidationChecking(createNewUser, 'email')
+    validation.requiredValidationChecking(createNewUser, 'email')
+  })
+
+  describe('Gravatar Validation', function () {
+    validation.urlValidationChecking(createNewUser, 'gravatar', 'gravatar')
   })
 })
