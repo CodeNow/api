0b35708ecf915d7a48fe2be2d03912162628c2ca
Anton Podviaznikov <podviaznikov@gmail.com>
2016-06-01 16:09:30 -0700

    linting

--- unit/models/services/context-service.js
+++ unit/models/services/context-service.js
@@ -10,36 +10,231 @@ var describe = lab.describe
 var expect = require('code').expect
 var it = lab.it
 
+var clone = require('101/clone')
 // external
 var sinon = require('sinon')
-
+require('sinon-as-promised')(require('bluebird'))
 // internal
 var Context = require('models/mongo/context')
 var ContextVersion = require('models/mongo/context-version')
 var Runnable = require('models/apis/runnable')
-
 // internal (being tested)
 var ContextService = require('models/services/context-service')
+var PermisionService = require('models/services/permission-service')
 
-var path = require('path')
-var moduleName = path.relative(process.cwd(), __filename)
-
-describe('ContextService: ' + moduleName, function () {
+describe('ContextService Unit Test', function () {
   var ctx = {}
   beforeEach(function (done) {
+    sinon.stub(PermisionService, 'isOwnerOf').returns({})
+    sinon.stub(Context, 'createAsync').returns()
     sinon.stub(Context.prototype, 'save').yieldsAsync()
     sinon.stub(ContextVersion, 'createDeepCopy').yieldsAsync()
     sinon.stub(Runnable.prototype, 'copyVersionIcvFiles').yieldsAsync()
     done()
   })
   afterEach(function (done) {
+    PermisionService.isOwnerOf.restore()
+    Context.createAsync.restore()
     Context.prototype.save.restore()
     ContextVersion.createDeepCopy.restore()
     Runnable.prototype.copyVersionIcvFiles.restore()
     done()
   })
 
-  describe('.handleVersionDeepCopy', function () {
+  describe('createNew', function () {
+    beforeEach(function (done) {
+      ctx.sessionUser = {
+        accounts: {
+          github: {
+            id: 1
+          }
+        }
+      }
+      done()
+    })
+
+    it('should fail if payload is null', function (done) {
+      ContextService.createNew(ctx.sessionUser, null)
+      .then(function () {
+        done(new Error('Should failed'))
+      })
+      .catch(function (err) {
+        expect(err.message).to.equal('"context" must be an object')
+        done()
+      })
+    })
+
+    it('should fail if name is not missing', function (done) {
+      ContextService.createNew(ctx.sessionUser, {})
+      .then(function () {
+        done(new Error('Should failed'))
+      })
+      .catch(function (err) {
+        expect(err.message).to.equal('"name" is required')
+        done()
+      })
+    })
+
+    it('should fail if name is not a string', function (done) {
+      ContextService.createNew(ctx.sessionUser, { name: {} })
+      .then(function () {
+        done(new Error('Should failed'))
+      })
+      .catch(function (err) {
+        expect(err.message).to.equal('"name" must be a string')
+        done()
+      })
+    })
+
+    it('should fail if owner.github is not a number', function (done) {
+      var payload = {
+        name: 'code',
+        owner: {
+          github: {}
+        }
+      }
+      ContextService.createNew(ctx.sessionUser, payload)
+      .then(function () {
+        done(new Error('Should failed'))
+      })
+      .catch(function (err) {
+        expect(err.message).to.equal('"owner.github" must be a number')
+        done()
+      })
+    })
+
+    it('should fail if isSource is not a boolean', function (done) {
+      var payload = {
+        name: 'code',
+        isSource: {}
+      }
+      ContextService.createNew(ctx.sessionUser, payload)
+      .then(function () {
+        done(new Error('Should failed'))
+      })
+      .catch(function (err) {
+        expect(err.message).to.equal('"isSource" must be a boolean')
+        done()
+      })
+    })
+
+    it('should fail if permission check failed', function (done) {
+      PermisionService.isOwnerOf.rejects(new Error('Not an owner'))
+      var payload = {
+        name: 'code'
+      }
+      ContextService.createNew(ctx.sessionUser, payload)
+      .then(function () {
+        done(new Error('Should failed'))
+      })
+      .catch(function (err) {
+        expect(err.message).to.equal('Not an owner')
+        done()
+      })
+    })
+
+    it('should fail if mongo save failed failed', function (done) {
+      Context.createAsync.rejects(new Error('Mongo error'))
+      var payload = {
+        name: 'code'
+      }
+      ContextService.createNew(ctx.sessionUser, payload)
+      .then(function () {
+        done(new Error('Should failed'))
+      })
+      .catch(function (err) {
+        expect(err.message).to.equal('Mongo error')
+        done()
+      })
+    })
+
+    it('should call isOwner check', function (done) {
+      var payload = {
+        name: 'code',
+        owner: {
+          github: 2
+        }
+      }
+      ContextService.createNew(ctx.sessionUser, clone(payload))
+      .then(function () {
+        sinon.assert.calledOnce(PermisionService.isOwnerOf)
+        sinon.assert.calledWith(PermisionService.isOwnerOf, ctx.sessionUser, payload)
+        done()
+      })
+      .catch(done)
+    })
+
+    it('should use sessionUser data for owner if not supplied', function (done) {
+      var payload = {
+        name: 'code'
+      }
+      ContextService.createNew(ctx.sessionUser, clone(payload))
+      .then(function () {
+        sinon.assert.calledOnce(PermisionService.isOwnerOf)
+        payload.owner = {
+          github: ctx.sessionUser.accounts.github.id
+        }
+        sinon.assert.calledWith(PermisionService.isOwnerOf, ctx.sessionUser, payload)
+        done()
+      })
+      .catch(done)
+    })
+
+    it('should call mongo with correct payload', function (done) {
+      var payload = {
+        name: 'code'
+      }
+      ContextService.createNew(ctx.sessionUser, clone(payload))
+      .then(function () {
+        sinon.assert.calledOnce(Context.createAsync)
+        payload.owner = {
+          github: ctx.sessionUser.accounts.github.id
+        }
+        sinon.assert.calledWith(Context.createAsync, payload)
+        done()
+      })
+      .catch(done)
+    })
+
+    it('should not include isSource for non moderator', function (done) {
+      var payload = {
+        name: 'code',
+        isSource: true
+      }
+      ContextService.createNew(ctx.sessionUser, clone(payload))
+      .then(function () {
+        sinon.assert.calledOnce(Context.createAsync)
+        payload.owner = {
+          github: ctx.sessionUser.accounts.github.id
+        }
+        delete payload.isSource
+        sinon.assert.calledWith(Context.createAsync, payload)
+        done()
+      })
+      .catch(done)
+    })
+
+    it('should include isSource for moderator', function (done) {
+      var payload = {
+        name: 'code',
+        isSource: true
+      }
+      var sessionUser = clone(ctx.sessionUser)
+      sessionUser.isModerator = true
+      ContextService.createNew(sessionUser, clone(payload))
+      .then(function () {
+        sinon.assert.calledOnce(Context.createAsync)
+        payload.owner = {
+          github: ctx.sessionUser.accounts.github.id
+        }
+        sinon.assert.calledWith(Context.createAsync, payload)
+        done()
+      })
+      .catch(done)
+    })
+  })
+
+  describe('handleVersionDeepCopy', function () {
     beforeEach(function (done) {
       ctx.mockContextVersion = {
         infraCodeVersion: 'pizza',
