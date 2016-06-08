8308b65f786d1c41d180ad468e37c7cf2a1f256b
thejsj <jorge.silva@thejsj.com>
2016-06-08 10:38:25 -0700

    Update integration tests

--- test/integration/models/services/instance-service.js
+++ test/integration/models/services/instance-service.js
@@ -69,101 +69,99 @@ describe('Instance Services Integration Tests', function () {
       }
       done()
     })
-    describe('flow validation', function () {
-      describe('built version', function () {
-        beforeEach(function (done) {
-          mongoFactory.createInstanceWithProps(ctx.mockOwner, {
-            masterPod: true
-          }, function (err, instance, build, cv) {
-            if (err) {
-              return done(err)
-            }
-            ctx.otherInstance = instance
-            ctx.otherBuild = build
-            ctx.otherCv = cv
-            done()
-          })
-        })
-        beforeEach(function (done) {
-          mongoFactory.createCompletedCv(1234, function (err, cv) {
-            if (err) {
-              return done(err)
-            }
-            ctx.completedCv = cv
-            done()
-          })
-        })
-        beforeEach(function (done) {
-          mongoFactory.createBuild(1234, ctx.completedCv, function (err, build) {
-            if (err) {
-              return done(err)
-            }
-            ctx.build = build
-            done()
-          })
-        })
-        it('should create an instance, create a connection, and fire both Rabbit events', function (done) {
-          var body = {
-            name: 'asdasdasd',
-            env: ['safdsdf=' + ctx.otherInstance.getElasticHostname('owner')],
-            build: ctx.build._id.toString(),
-            masterPod: true,
-            owner: {
-              github: ctx.ownerId
-            }
+    describe('create new instance', function () {
+      beforeEach(function (done) {
+        mongoFactory.createInstanceWithProps(ctx.mockOwner, {
+          masterPod: true
+        }, function (err, instance, build, cv) {
+          if (err) {
+            return done(err)
           }
-          InstanceService.createInstance(body, ctx.mockSessionUser)
-            .then(function (instance) {
-              expect(instance).to.exist()
-              return Instance.findByIdAsync(instance._id)
-            })
-            .then(function (instance) {
-              expect(instance).to.exist()
-              var jsoned = instance.toJSON()
-              // -----
-              expect(jsoned).to.deep.include({
-                createdBy: {
-                  github: 1234,
-                  gravatar: 'sdasdasdasdasdasd',
-                  username: 'user'
-                },
-                owner: {
-                  github: ctx.ownerId,
-                  gravatar: 'TEST-avatar_url',
-                  username: 'owner'
-                }
-              })
-              expect(jsoned).to.deep.include({
-                build: ctx.build._id,
-                name: body.name,
-                lowerName: body.name.toLowerCase(),
-                env: body.env
-              })
-              expect(instance.elasticHostname).to.exist()
-              expect(instance.contextVersion._id).to.deep.equal(ctx.completedCv._id)
-              // -----
-              sinon.assert.calledWith(rabbitMQ.instanceDeployed, {
-                cvId: ctx.completedCv._id.toString(),
-                instanceId: instance._id.toString()
-              })
-              sinon.assert.calledWith(rabbitMQ.createInstanceContainer, {
-                contextVersionId: ctx.completedCv._id.toString(),
-                instanceId: instance._id.toString(),
-                ownerUsername: 'owner',
-                sessionUserGithubId: 1234
-              })
-              sinon.assert.calledWith(
-                messenger.emitInstanceUpdate,
-                sinon.match.has('_id', instance._id),
-                'post'
-              )
-              return instance.getDependenciesAsync()
-            })
-            .then(function (deps) {
-              expect(deps.length).to.equal(1)
-            })
-            .asCallback(done)
+          ctx.otherInstance = instance
+          ctx.otherBuild = build
+          ctx.otherCv = cv
+          done()
         })
+      })
+      beforeEach(function (done) {
+        mongoFactory.createCompletedCv(1234, function (err, cv) {
+          if (err) {
+            return done(err)
+          }
+          ctx.completedCv = cv
+          done()
+        })
+      })
+      beforeEach(function (done) {
+        mongoFactory.createBuild(1234, ctx.completedCv, function (err, build) {
+          if (err) {
+            return done(err)
+          }
+          ctx.build = build
+          done()
+        })
+      })
+      it('should create an instance, create a connection, and fire both Rabbit events', function (done) {
+        var body = {
+          name: 'asdasdasd',
+          env: ['safdsdf=' + ctx.otherInstance.getElasticHostname('owner')],
+          build: ctx.build._id.toString(),
+          masterPod: true,
+          owner: {
+            github: ctx.ownerId
+          }
+        }
+        InstanceService.createInstance(body, ctx.mockSessionUser)
+          .then(function (instance) {
+            expect(instance).to.exist()
+            return Instance.findByIdAsync(instance._id)
+          })
+          .then(function (instance) {
+            expect(instance).to.exist()
+            var jsoned = instance.toJSON()
+            // -----
+            expect(jsoned).to.deep.include({
+              createdBy: {
+                github: 1234,
+                gravatar: 'sdasdasdasdasdasd',
+                username: 'user'
+              },
+              owner: {
+                github: ctx.ownerId,
+                gravatar: 'TEST-avatar_url',
+                username: 'owner'
+              }
+            })
+            expect(jsoned).to.deep.include({
+              build: ctx.build._id,
+              name: body.name,
+              lowerName: body.name.toLowerCase(),
+              env: body.env
+            })
+            expect(instance.elasticHostname).to.exist()
+            expect(instance.contextVersion._id).to.deep.equal(ctx.completedCv._id)
+            // -----
+            sinon.assert.calledWith(rabbitMQ.instanceDeployed, {
+              cvId: ctx.completedCv._id.toString(),
+              instanceId: instance._id.toString()
+            })
+            sinon.assert.calledWith(rabbitMQ.createInstanceContainer, {
+              contextVersionId: ctx.completedCv._id.toString(),
+              instanceId: instance._id.toString(),
+              ownerUsername: 'owner',
+              sessionUserGithubId: 1234
+            })
+            sinon.assert.calledWith(
+              messenger.emitInstanceUpdate,
+              sinon.match.has('_id', instance._id),
+              'post'
+            )
+            return instance.getDependenciesAsync()
+          })
+          .then(function (deps) {
+            expect(deps.length).to.equal(1)
+          })
+          .asCallback(done)
       })
     })
   })
@@ -213,85 +211,84 @@ describe('Instance Services Integration Tests', function () {
       }
       done()
     })
-    describe('flow validation', function () {
-      describe('built version', function () {
-        beforeEach(function (done) {
-          mongoFactory.createInstanceWithProps(ctx.mockOwner, {
-            masterPod: true
-          }, function (err, instance, build, cv) {
-            if (err) {
-              return done(err)
-            }
-            ctx.instance = instance
-            ctx.otherBuild = build
-            ctx.otherCv = cv
-            done()
-          })
-        })
-        beforeEach(function (done) {
-          mongoFactory.createCompletedCv(ctx.ownerId, function (err, cv) {
-            if (err) {
-              return done(err)
-            }
-            ctx.completedCv = cv
-            done()
-          })
-        })
-        beforeEach(function (done) {
-          mongoFactory.createBuild(ctx.ownerId, ctx.completedCv, function (err, build) {
-            if (err) {
-              return done(err)
-            }
-            ctx.build = build
-            done()
-          })
-        })
-        it('should create an instance, create a connection, and fire both Rabbit events', function (done) {
-          var body = {
-            env: ['safdsdf=sadasdas'],
-            build: ctx.build._id.toString()
+    describe('update instance with new build', function () {
+      beforeEach(function (done) {
+        mongoFactory.createInstanceWithProps(ctx.mockOwner, {
+          masterPod: true
+        }, function (err, instance, build, cv) {
+          if (err) {
+            return done(err)
           }
-          InstanceService.updateInstance(ctx.instance, body, ctx.mockSessionUser)
-            .then(function (instance) {
-              expect(instance).to.exist()
-              return Instance.findByIdAsync(instance._id)
-            })
-            .then(function (instance) {
-              expect(instance).to.exist()
-              var jsoned = instance.toJSON()
-
-              expect(jsoned).to.deep.include({
-                build: ctx.build._id,
-                env: body.env
-              })
-              expect(instance.elasticHostname).to.exist()
-              expect(instance.contextVersion._id).to.deep.equal(ctx.completedCv._id)
-              // -----
-              sinon.assert.calledWith(rabbitMQ.instanceDeployed, {
-                cvId: ctx.completedCv._id.toString(),
-                instanceId: instance._id.toString()
-              })
-              sinon.assert.calledWith(rabbitMQ.createInstanceContainer, {
-                contextVersionId: ctx.completedCv._id.toString(),
-                instanceId: instance._id.toString(),
-                ownerUsername: 'owner',
-                sessionUserGithubId: 1234
-              })
-              sinon.assert.calledWith(rabbitMQ.deleteContextVersion, {
-                contextVersionId: ctx.otherCv._id.toString()
-              })
-              sinon.assert.calledWith(
-                messenger.emitInstanceUpdate,
-                sinon.match.has('_id', instance._id),
-                'post'
-              )
-              return instance.getDependenciesAsync()
-            })
-            .then(function (deps) {
-              expect(deps.length).to.equal(0)
-            })
-            .asCallback(done)
+          ctx.instance = instance
+          ctx.otherBuild = build
+          ctx.otherCv = cv
+          done()
         })
+      })
+      beforeEach(function (done) {
+        mongoFactory.createCompletedCv(ctx.ownerId, function (err, cv) {
+          if (err) {
+            return done(err)
+          }
+          ctx.completedCv = cv
+          done()
+        })
+      })
+      beforeEach(function (done) {
+        mongoFactory.createBuild(ctx.ownerId, ctx.completedCv, function (err, build) {
+          if (err) {
+            return done(err)
+          }
+          ctx.build = build
+          done()
+        })
+      })
+      it('should update the instance, create a new container, delete the contextVersion, and emit an update', function (done) {
+        var body = {
+          env: ['safdsdf=sadasdas'],
+          build: ctx.build._id.toString()
+        }
+        InstanceService.updateInstance(ctx.instance, body, ctx.mockSessionUser)
+          .then(function (instance) {
+            expect(instance).to.exist()
+            return Instance.findByIdAsync(instance._id)
+          })
+          .then(function (instance) {
+            expect(instance).to.exist()
+            var jsoned = instance.toJSON()
+
+            expect(jsoned).to.deep.include({
+              build: ctx.build._id,
+              env: body.env,
+              contextVersion: ctx.completedCv.toJSON()
+            })
+            expect(instance.elasticHostname).to.exist()
+            expect(instance.contextVersion._id).to.deep.equal(ctx.completedCv._id)
+            // -----
+            sinon.assert.calledWith(rabbitMQ.instanceDeployed, {
+              cvId: ctx.completedCv._id.toString(),
+              instanceId: instance._id.toString()
+            })
+            sinon.assert.calledWith(rabbitMQ.createInstanceContainer, {
+              contextVersionId: ctx.completedCv._id.toString(),
+              instanceId: instance._id.toString(),
+              ownerUsername: 'owner',
+              sessionUserGithubId: 1234
+            })
+            sinon.assert.calledWith(rabbitMQ.deleteContextVersion, {
+              contextVersionId: ctx.otherCv._id.toString()
+            })
+            sinon.assert.calledWith(
+              messenger.emitInstanceUpdate,
+              sinon.match.has('_id', instance._id),
+              'post'
+            )
+            return instance.getDependenciesAsync()
+          })
+          .then(function (deps) {
+            expect(deps.length).to.equal(0)
+          })
+          .asCallback(done)
       })
     })
   })
