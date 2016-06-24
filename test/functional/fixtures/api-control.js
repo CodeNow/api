ae3fab5de6b3b85f7910c53cd094e9e2b906411d
Anton Podviaznikov <podviaznikov@gmail.com>
2016-06-24 16:44:41 -0700

    minors

--- test/functional/fixtures/api-control.js
+++ test/functional/fixtures/api-control.js
@@ -37,7 +37,7 @@ function ensureIndexes (cb) {
 var publishedEvents = [
   'container.network.attached',
   'dock.removed',
-  'docker.events-stream.connected'
+  'docker.events-stream.connected',
   'docker.events-stream.disconnected'
 ]
 
