9aa52148de3681be6de242eabc34c4f20a36e31f
Myztiq <kahn@runnable.com>
2016-06-28 15:51:55 -0700

    Updated tests, changed to search for errrs under 500.

--- unit/workers/instance.container.created.js
+++ unit/workers/instance.container.created.js
@@ -200,8 +200,8 @@ describe('InstanceContainerCreated: ' + moduleName, function () {
       })
     })
 
-    it('should throw task fatal if the delete container step gets a 500 error', function (done) {
-      var updateConflict = Boom.conflict("Container was not updated, instance's container has changed")
+    it('should throw task fatal if the delete container step gets a 400 error', function (done) {
+      var updateConflict = Boom.badRequest("Yup, this is a bad thing that should not be retried")
       InstanceService.updateContainerInspect.yieldsAsync(updateConflict)
       Docker.prototype.removeContainerAsync.rejects(Boom.badImplementation())
       InstanceContainerCreated(ctx.data).asCallback(function (err) {
