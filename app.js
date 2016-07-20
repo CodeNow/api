c2ced50ccf58e367ca0c060d5e011a6e12f1ac27
Anandkumar Patel <anand@runnable.com>
2016-07-20 14:49:43 -0700

    remove clusteman

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ if (process.env.NEW_RELIC_LICENSE_KEY) {
   require('newrelic')
 }
 
-var cluster = require('cluster')
 var createCount = require('callback-count')
 
 var ApiServer = require('server')
@@ -19,7 +18,6 @@ var error = require('error')
 var keyGen = require('key-generator')
 var logger = require('middlewares/logger')(__filename)
 var mongooseControl = require('models/mongo/mongoose-control')
-var noop = require('101/noop')
 var redisClient = require('models/redis')
 var redisPubSub = require('models/redis/pubsub')
 
@@ -31,10 +29,7 @@ var apiServer = new ApiServer()
 /**
  * @class
  */
-function Api () {
-  // bind `this` so it can be used directly as event handler
-  this._handleStopSignal = this._handleStopSignal.bind(this)
-}
+function Api () {}
 
 /**
  * - Listen to incoming HTTP requests
@@ -46,7 +41,6 @@ function Api () {
  */
 Api.prototype.start = function (cb) {
   cb = cb || error.logIfErr
-  var self = this
   var count = createCount(callback)
   log.trace('start')
   // start github ssh key generator
@@ -72,8 +66,6 @@ Api.prototype.start = function (cb) {
       return
     }
     log.trace('API started')
-    console.log('API started')
-    self.listenToSignals()
     cb()
   }
 }
@@ -120,79 +112,10 @@ Api.prototype.stop = function (cb) {
   }
 }
 
-/**
- * listen to process SIGINT
- */
-Api.prototype.listenToSignals = function () {
-  process.on('SIGINT', this._handleStopSignal)
-  process.on('SIGTERM', this._handleStopSignal)
-}
-
-/**
- * stop listening to process SIGINT
- */
-Api.prototype.stopListeningToSignals = function () {
-  process.removeListener('SIGINT', this._handleStopSignal)
-  process.removeListener('SIGTERM', this._handleStopSignal)
-}
-
-/**
- * SIGINT event handler
- */
-Api.prototype._handleStopSignal = function () {
-  log.info('STOP SIGNAL: recieved')
-  var self = this
-  process.removeAllListeners('uncaughtException')
-  this.stop(function (err) {
-    if (err) {
-      log.error({
-        err: err
-      }, 'STOP SIGNAL: stop failed')
-      return
-    }
-    log.info('STOP SIGNAL: stop succeeded, process should exit gracefully')
-    if (cluster.isWorker) {
-      // workers need to be exited manually
-      self._waitForActiveHandlesAndExit()
-    } else {
-      process.exit()
-    }
-  })
-}
-
-/**
- * wait for active handles to reach 2 or less
- */
-Api.prototype._waitForActiveHandlesAndExit = function (cb) {
-  cb = cb || noop
-  var poller = setInterval(function () {
-    var handles = process._getActiveHandles()
-    log.info({ count: handles.length, items: handles }, 'Active handles')
-    if (process._getActiveHandles().length <= 2) {
-      // there are 2 active handles
-      // 1 is the interval and 1 is the clustered process
-      clearInterval(poller)
-      log.info('Worker process exited gracefully')
-      process.exit() // clean exit
-    }
-  }, 1000)
-}
-
-/**
- * Returns PrimusSocket constructor function that can be used for
- * primus Client instantiation.
- * @return {Function} - PrimusSocket class
- */
-Api.prototype.getPrimusSocket = function () {
-  return apiServer.socketServer.primus.Socket
-}
-
 // we are exposing here apiServer as a singletond
 var api = module.exports = new Api()
 
-if (!module.parent) { // npm start
-  api.start()
-}
+api.start()
 
 // should not occur in practice, using domains to catch errors
 process.on('uncaughtException', function (err) {
