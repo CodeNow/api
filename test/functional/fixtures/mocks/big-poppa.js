742e3fb54af61539e45520b44e0739190d901e09
Nathan219 <Nathan219@gmail.com>
2016-08-11 17:40:08 -0700

    Add extra nocks for BigPoppa make function isUserPartOfOrg

--- test/functional/fixtures/mocks/big-poppa.js
+++ test/functional/fixtures/mocks/big-poppa.js
@@ -2,24 +2,24 @@
 var nock = require('nock')
 
 module.exports.whitelistUserOrgs = function (user, orgs) {
+  var userId = user.attrs.accounts.github.id
   nock('http://' + process.env.BIG_POPPA_HOST)
-    .get('/user/?githubId=' + user.attrs.accounts.github.id)
-    .reply(200, [{
-      organizations: orgs.map(function (org) {
-        return {
-          name: org.name,
-          githubId: org.githubId,
-          allowed: true
-        }
-      }),
-      githubId: user.attrs.accounts.github.id
-    }])
+    .get('/user/?githubId=' + userId)
+    .times(1000)
+    .reply(
+      200,
+      [{
+        organizations: orgs,
+        githubId: userId
+      }]
+    )
 }
 module.exports.whitelistOrgs = function (orgs) {
   var whitelistNock = nock('http://' + process.env.BIG_POPPA_HOST)
   orgs.forEach(function (org) {
     whitelistNock
       .get('/organization/?lowerName=' + org.name.toLowerCase())
+      .times(1000)
       .reply(
         200,
         [{
@@ -30,6 +30,7 @@ module.exports.whitelistOrgs = function (orgs) {
       )
     whitelistNock
       .get('/organization/?githubId=' + org.githubId)
+      .times(1000)
       .reply(
         200,
         [{
@@ -37,6 +38,15 @@ module.exports.whitelistOrgs = function (orgs) {
           githubId: org.githubId,
           allowed: true
         }]
+      )
+    whitelistNock
+      .post('/organization/:orgId/add')
+      .times(1000)
+      .filteringPath(/organization\/[0-9A-z]*\/add/, '')
+      .reply(
+        404, {
+          err: 'NO!'
+        }
       )
   })
 }
