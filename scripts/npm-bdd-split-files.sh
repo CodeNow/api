3ea1b7a1506f5660064eca080647c1a1cca90f5d
Nathan219 <Nathan219@gmail.com>
2016-08-23 18:52:15 -0700

    re-order tests again

--- scripts/npm-bdd-split-files.sh
+++ scripts/npm-bdd-split-files.sh
@@ -49,20 +49,20 @@ then
   else
     if [[ $CIRCLE_NODE_INDEX -eq 0 ]]
     then
-      len=60
+      len=55
       s=0
       n=1
-      e=60
+      e=55
     elif [[ $CIRCLE_NODE_INDEX -eq 1 ]]
     then
-      len=70
-      s=60
-      n=61
-      e=130
+      len=60
+      s=55
+      n=56
+      e=115
     else
       len=$(expr $(expr $numTests-300) / $CIRCLE_NODE_TOTAL)
-      s=130
-      n=131
+      s=115
+      n=116
       e=$(expr $numTests )
     fi
     if [[ $CIRCLE_NODE_TOTAL -eq $n ]]
