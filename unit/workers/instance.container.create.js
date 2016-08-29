78555ddeee8545e7453de7753e082c3f9403b71c
Anton Podviaznikov <podviaznikov@gmail.com>
2016-08-29 14:48:09 +0800

    fix tests

--- unit/workers/instance.container.create.js
+++ unit/workers/instance.container.create.js
@@ -6,6 +6,7 @@ var Lab = require('lab')
 var moment = require('moment')
 var Promise = require('bluebird')
 var sinon = require('sinon')
+require('sinon-as-promised')(Promise)
 var WorkerStopError = require('error-cat/errors/worker-stop-error')
 
 var ContextVersion = require('models/mongo/context-version')
@@ -73,12 +74,9 @@ describe('createInstanceContainer', function () {
           }
         }
       }
-      sinon.stub(ContextVersion, 'findById')
-        .yieldsAsync(null, ctx.contextVersion)
-      sinon.stub(InstanceService, 'createContainer')
-        .returns(Promise.resolve())
-      sinon.stub(PermissionService, 'checkOwnerAllowed')
-        .returns(Promise.resolve())
+      sinon.stub(ContextVersion, 'findById').yieldsAsync(null, ctx.contextVersion)
+      sinon.stub(InstanceService, 'createContainer').resolves()
+      sinon.stub(PermissionService, 'checkOwnerAllowed').resolves()
       done()
     })
 
@@ -90,11 +88,6 @@ describe('createInstanceContainer', function () {
     })
 
     describe('success', function () {
-      beforeEach(function (done) {
-        InstanceService.createContainer.yieldsAsync()
-        done()
-      })
-
       it('should call InstanceService.createContainer', function (done) {
         Worker.task(ctx.job)
           .asCallback(function (err) {
@@ -108,7 +101,7 @@ describe('createInstanceContainer', function () {
     describe('error', function () {
       beforeEach(function (done) {
         sinon.stub(rabbitmq, 'publishInstanceRebuild')
-        sinon.stub(Instance, 'setContainerCreateError').returns(Promise.resolve())
+        sinon.stub(Instance, 'setContainerCreateError').resolves()
         sinon.stub(error, 'log')
         done()
       })
@@ -172,14 +165,14 @@ describe('createInstanceContainer', function () {
       describe('unknown', function () {
         beforeEach(function (done) {
           ctx.err = new Error('boom')
-          InstanceService.createContainer.yieldsAsync(ctx.err)
+          InstanceService.createContainer.rejects(ctx.err)
           done()
         })
 
         it('should call InstanceService.createContainer', function (done) {
           Worker.task(ctx.job)
             .asCallback(function (err) {
-              expect(err.cause).to.equal(ctx.err)
+              expect(err.message).to.equal(ctx.err.message)
               done()
             })
         })
@@ -188,14 +181,14 @@ describe('createInstanceContainer', function () {
       describe('4XX', function () {
         beforeEach(function (done) {
           ctx.err = Boom.notFound('boom')
-          InstanceService.createContainer.yieldsAsync(ctx.err)
+          InstanceService.createContainer.rejects(ctx.err)
           done()
         })
 
         it('should call InstanceService.createContainer', function (done) {
           Worker.task(ctx.job)
             .asCallback(function (err) {
-              expect(err.data.originalError.cause).to.equal(ctx.err)
+              expect(err.data.originalError.message).to.equal(ctx.err.message)
               done()
             })
         })
@@ -210,14 +203,14 @@ describe('createInstanceContainer', function () {
             }
           }
           ContextVersion.findById.yieldsAsync(null, ctx.contextVersion)
-          InstanceService.createContainer.yieldsAsync(ctx.err)
+          InstanceService.createContainer.rejects(ctx.err)
           done()
         })
 
         it('should not trigger a re-build', function (done) {
           Worker.task(ctx.job)
             .asCallback(function (err) {
-              expect(err.cause).to.equal(ctx.err)
+              expect(err.message).to.equal(ctx.err.message)
               sinon.assert.notCalled(rabbitmq.publishInstanceRebuild)
               sinon.assert.notCalled(error.log)
               done()
@@ -228,7 +221,7 @@ describe('createInstanceContainer', function () {
       describe('image not found error', function () {
         beforeEach(function (done) {
           ctx.err = new Error('image 1234 not found')
-          InstanceService.createContainer.yieldsAsync(ctx.err)
+          InstanceService.createContainer.rejects(ctx.err)
           done()
         })
 
@@ -266,7 +259,7 @@ describe('createInstanceContainer', function () {
           it('should not trigger a re-build', function (done) {
             Worker.task(ctx.job)
               .asCallback(function (err) {
-                expect(err.cause).to.equal(ctx.err)
+                expect(err.message).to.equal(ctx.err.message)
                 sinon.assert.calledOnce(ContextVersion.findById)
                 sinon.assert.calledWith(
                   ContextVersion.findById,
