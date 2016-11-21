50f9aacd7068a35f7368a81fb93741d674178eba
Anton Podviaznikov <podviaznikov@gmail.com>
2016-11-21 15:30:40 -0800

    apply feedback from jorge

--- test/integration/models/mongo/docker-compose-cluser.js
+++ test/integration/models/mongo/docker-compose-cluser.js
@@ -17,7 +17,7 @@ const mongooseControl = require('models/mongo/mongoose-control')
 describe('DockerComposeCluster Model Integration Tests', function () {
   const data = {
     dockerComposeFilePath: '/config/compose.yml',
-    parentInstance: objectId('507f191e810c19729de860ea'),
+    parent: objectId('507f191e810c19729de860ea'),
     siblings: [
       objectId('607f191e810c19729de860eb'),
       objectId('707f191e810c19729de860ec')
@@ -28,19 +28,16 @@ describe('DockerComposeCluster Model Integration Tests', function () {
     DockerComposeCluster.remove({}, done)
   })
 
-  after(function (done) {
-    DockerComposeCluster.remove({}, done)
-  })
   after(mongooseControl.stop)
 
-  describe('find by parentInstance', function () {
+  describe('find by parent instance', function () {
     let savedDockerComposeCluster = null
     before(function (done) {
       const composeCluster = new DockerComposeCluster(data)
       composeCluster.saveAsync()
       .tap(function (saved) {
         expect(saved.dockerComposeFilePath).to.equal(data.dockerComposeFilePath)
-        expect(saved.parentInstance.toString()).to.equal(data.parentInstance.toString())
+        expect(saved.parent.toString()).to.equal(data.parent.toString())
         expect(saved.siblings.length).to.equal(data.siblings.length)
         expect(saved.siblings[0].toString()).to.equal(data.siblings[0].toString())
         expect(saved.siblings[1].toString()).to.equal(data.siblings[1].toString())
@@ -49,12 +46,12 @@ describe('DockerComposeCluster Model Integration Tests', function () {
       }).asCallback(done)
     })
 
-    it('should be possible to find settings by parentInstance id', function (done) {
-      DockerComposeCluster.findOneAsync({ 'parentInstance': objectId('507f191e810c19729de860ea') })
+    it('should be possible to find compose cluster by parent id', function (done) {
+      DockerComposeCluster.findOneAsync({ 'parent': objectId('507f191e810c19729de860ea') })
       .tap(function (composeCluster) {
         expect(String(composeCluster._id)).to.equal(String(savedDockerComposeCluster._id))
         expect(composeCluster.dockerComposeFilePath).to.equal(savedDockerComposeCluster.dockerComposeFilePath)
-        expect(composeCluster.parentInstance.toString()).to.equal(savedDockerComposeCluster.parentInstance.toString())
+        expect(composeCluster.parent.toString()).to.equal(savedDockerComposeCluster.parent.toString())
         expect(composeCluster.siblings.length).to.equal(savedDockerComposeCluster.siblings.length)
         expect(composeCluster.created).to.equal(savedDockerComposeCluster.created)
       })
@@ -66,7 +63,7 @@ describe('DockerComposeCluster Model Integration Tests', function () {
     it('should be possible to save compose cluster', function (done) {
       const data = {
         dockerComposeFilePath: '/config/compose.yml',
-        parentInstance: objectId('507f191e810c19729de860ea'),
+        parent: objectId('507f191e810c19729de860ea'),
         siblings: [
           objectId('607f191e810c19729de860eb'),
           objectId('707f191e810c19729de860ec')
@@ -76,7 +73,7 @@ describe('DockerComposeCluster Model Integration Tests', function () {
       composeCluster.saveAsync()
       .tap(function (saved) {
         expect(saved.dockerComposeFilePath).to.equal(data.dockerComposeFilePath)
-        expect(saved.parentInstance.toString()).to.equal(data.parentInstance.toString())
+        expect(saved.parent.toString()).to.equal(data.parent.toString())
         expect(saved.siblings.length).to.equal(data.siblings.length)
         expect(saved.siblings[0].toString()).to.equal(data.siblings[0].toString())
         expect(saved.siblings[1].toString()).to.equal(data.siblings[1].toString())
@@ -101,44 +98,44 @@ describe('DockerComposeCluster Model Integration Tests', function () {
       })
     })
 
-    it('should fail if parentInstance is not provided', function (done) {
+    it('should fail if parent id is not provided', function (done) {
       const invalidData = Object.assign({}, data)
-      invalidData.parentInstance = null
+      invalidData.parent = null
       const composeCluster = new DockerComposeCluster(invalidData)
       composeCluster.saveAsync()
       .then(function () {
         done(new Error('Should fail'))
       })
       .catch(function (err) {
-        expect(err.errors.parentInstance.message).to.equal('Docker Compose Cluser requires parent instance')
+        expect(err.errors.parent.message).to.equal('Docker Compose Cluser requires parent instance')
         done()
       })
     })
 
-    it('should fail if parentInstance is not valid object id', function (done) {
+    it('should fail if parent id is not valid object id', function (done) {
       const invalidData = Object.assign({}, data)
-      invalidData.parentInstance = 'some-invalid-id'
+      invalidData.parent = 'some-invalid-id'
       const composeCluster = new DockerComposeCluster(invalidData)
       composeCluster.saveAsync()
       .then(function () {
         done(new Error('Should fail'))
       })
       .catch(function (err) {
-        expect(err.message).to.equal('Cast to ObjectId failed for value "some-invalid-id" at path "parentInstance"')
+        expect(err.message).to.equal(`Cast to ObjectId failed for value "${invalidData.parent}" at path "parent"`)
         done()
       })
     })
 
     it('should fail if siblings are not valid objectid', function (done) {
       const invalidData = Object.assign({}, data)
-      invalidData.siblings.push('some-invalid-id')
+      invalidData.siblings = ['some-invalid-id']
       const composeCluster = new DockerComposeCluster(invalidData)
       composeCluster.saveAsync()
       .then(function () {
         done(new Error('Should fail'))
       })
       .catch(function (err) {
-        expect(err.message).to.equal('Cast to ObjectId failed for value "607f191e810c19729de860eb,707f191e810c19729de860ec,some-invalid-id" at path "siblings"')
+        expect(err.message).to.equal(`Cast to ObjectId failed for value "${invalidData.siblings[0]}" at path "siblings"`)
         done()
       })
     })
