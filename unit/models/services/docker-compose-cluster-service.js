fce650021333d15161a904b878490cc043a62435
Anton Podviaznikov <podviaznikov@gmail.com>
2016-12-09 15:48:02 -0800

    more code cleanups

--- unit/models/services/docker-compose-cluster-service.js
+++ unit/models/services/docker-compose-cluster-service.js
@@ -23,7 +23,6 @@ const ContextService = require('models/services/context-service')
 const ContextVersion = require('models/mongo/context-version')
 const InfraCodeVersionService = require('models/services/infracode-version-service')
 const InstanceService = require('models/services/instance-service')
-const OrganizationService = require('models/services/organization-service')
 
 require('sinon-as-promised')(Promise)
 
@@ -140,7 +139,6 @@ describe('Docker Compose Cluster Service Unit Tests', function () {
     beforeEach(function (done) {
       sinon.stub(DockerComposeCluster, 'createAsync').resolves(new DockerComposeCluster(clusterData))
       sinon.stub(GitHub.prototype, 'getRepoContentAsync').resolves(dockerComposeContent)
-      sinon.stub(OrganizationService, 'getByGithubUsername').resolves(testOrg)
       sinon.stub(octobear, 'parse').returns(testParsedContent)
       sinon.stub(rabbitMQ, 'clusterCreated').returns()
       done()
@@ -148,7 +146,6 @@ describe('Docker Compose Cluster Service Unit Tests', function () {
     afterEach(function (done) {
       DockerComposeCluster.createAsync.restore()
       GitHub.prototype.getRepoContentAsync.restore()
-      OrganizationService.getByGithubUsername.restore()
       octobear.parse.restore()
       rabbitMQ.clusterCreated.restore()
       done()
@@ -168,17 +165,6 @@ describe('Docker Compose Cluster Service Unit Tests', function () {
       it('should return error if octobear.parse failed', function (done) {
         const error = new Error('Some error')
         octobear.parse.throws(error)
-        DockerComposeClusterService.create(testSessionUser, triggeredAction, repoFullName, branchName, dockerComposeFilePath, newInstanceName)
-        .asCallback(function (err) {
-          expect(err).to.exist()
-          expect(err.message).to.equal(error.message)
-          done()
-        })
-      })
-
-      it('should return error if OrganizationService.getByGithubUsername failed', function (done) {
-        const error = new Error('Some error')
-        OrganizationService.getByGithubUsername.rejects(error)
         DockerComposeClusterService.create(testSessionUser, triggeredAction, repoFullName, branchName, dockerComposeFilePath, newInstanceName)
         .asCallback(function (err) {
           expect(err).to.exist()
@@ -238,15 +224,6 @@ describe('Docker Compose Cluster Service Unit Tests', function () {
         .asCallback(done)
       })
 
-      it('should call OrganizationService.getByGithubUsername with correct args', function (done) {
-        DockerComposeClusterService.create(testSessionUser, triggeredAction, repoFullName, branchName, dockerComposeFilePath, newInstanceName)
-        .tap(function () {
-          sinon.assert.calledOnce(OrganizationService.getByGithubUsername)
-          sinon.assert.calledWithExactly(OrganizationService.getByGithubUsername, ownerUsername)
-        })
-        .asCallback(done)
-      })
-
       it('should call createAsync with correct args', function (done) {
         DockerComposeClusterService.create(testSessionUser, triggeredAction, repoFullName, branchName, dockerComposeFilePath, newInstanceName)
         .tap(function () {
@@ -287,7 +264,6 @@ describe('Docker Compose Cluster Service Unit Tests', function () {
           sinon.assert.callOrder(
             GitHub.prototype.getRepoContentAsync,
             octobear.parse,
-            OrganizationService.getByGithubUsername,
             DockerComposeCluster.createAsync,
             rabbitMQ.clusterCreated)
         })
