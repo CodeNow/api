6e644fb22085cff102373c4f23cc1b8c5e80cfe3
Nathan219 <Nathan219@gmail.com>
2016-12-20 16:12:13 -0800

    Adding webhook script to add webhooks to all added instances

---
+++ scripts/reinstate-webhooks.js
@@ -0,0 +1,75 @@
+/*
+ * This script should be run whenever the database needs to be repopulated with
+ * the seed contexts
+ * `NODE_ENV=development NODE_PATH=./lib node scripts/seed-version.js`
+ *
+ * NOTE: This script will attempt to delete any existing source contexts, as well as their
+ * instances.  It should output what it's deleting, so be sure to verify nothing else was targeted
+ *
+ * NOTE 2: Must log in as HelloRunnable and populate user model in mongo before running this script
+ */
+
+'use strict'
+
+require('loadenv')()
+
+const OrgService = require('models/services/organization-service')
+const UserService = require('models/services/user-service')
+const Instance = require('models/mongo/instance')
+const RegEx = require('regexp-quote')
+const mongoose = require('mongoose')
+const Promise = require('bluebird')
+const User = require('models/mongo/user')
+const GitHub = require('models/apis/github')
+
+var args = process.argv.slice(2);
+/*
+ * START SCRIPT
+ */
+main(args[0])
+
+function main (orgName) {
+  return Promise.fromCallback(cb => {
+    mongoose.connect(process.env.MONGO, cb)
+  })
+    .then(() => {
+      return OrgService.getByGithubUsername(orgName)
+    })
+    .then((org) => {
+      return OrgService.getUsersByOrgName(orgName)
+        .map(user => {
+          const token = user.accounts.github.accessToken
+          const github = new GitHub({token})
+          return Instance.findAsync({
+            masterPod: true,
+            'owner.github': org.githubId,
+            'contextVersion.appCodeVersions': {
+              $elemMatch: {
+                $or: [
+                  {additionalRepo: false},
+                  {additionalRepo: {$exists: false}}
+                ]
+              }
+            }
+          }, {})
+            .map(function (instance) {
+              return github.createRepoHookIfNotAlreadyAsync(instance.getRepoName())
+                .catch(err => {
+                  console.error('user', user.accounts.github.username, 'couldn\'t add the hook', err)
+                })
+            })
+        })
+    })
+    .catch(err => {
+      console.error('hello runnable error', err)
+      throw err
+    })
+    .finally(() => {
+      return Promise.fromCallback(cb => {
+        mongoose.disconnect(cb)
+      })
+        .asCallback(err => {
+          return process.exit(err ? 1 : 0)
+        })
+    })
+}
