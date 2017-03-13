9b086b778e557cea95c78aed29509f2d73831ad1
Nathan219 <Nathan219@gmail.com>
2017-03-13 16:04:15 -0700

    This pulls all the configs, organizes them by instance id, finds the newest one, and deletes all the old ones

---
+++ scripts/remove-extra-autoisolation-config.js
@@ -0,0 +1,58 @@
+'use strict'
+/**
+ * This script updates all instances to save their hostname (and elasticHostname) to the database.
+ * It also saves the name of the owner from github (if it wasn't already there)
+ */
+require('loadenv')()
+var AutoIsolationConfigs = require('models/mongo/auto-isolation-config')
+var mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO)
+var Promise = require('bluebird')
+
+var dryRun = !process.env.ACTUALLY_RUN
+
+console.log('dryRun?', !!dryRun)
+
+AutoIsolationConfigs.findAsync({})
+  .then(function (configs) {
+    // map them by instanceId
+    const configsByInstanceId = configs.reduce((map, config) => {
+      if (config.instance) {
+        const instanceId = config.instance.toString()
+        if (!map[instanceId]) {
+          map[instanceId] = []
+        }
+        map[instanceId].push(config)
+      }
+      return map
+    }, {})
+    const allPromises = []
+    Object.keys(configsByInstanceId).forEach((instanceId) => {
+      let latestDateConfig
+      configsByInstanceId[instanceId].forEach((config) => {
+        if (!latestDateConfig) {
+          latestDateConfig = config
+        } else {
+          if (config.created > latestDateConfig.created) {
+            latestDateConfig = config
+          }
+        }
+      })
+      configsByInstanceId[instanceId].forEach((config) => {
+        if (config !== latestDateConfig) {
+          console.log('Removing Config', config);
+          if (!dryRun) {
+            allPromises.push(config.removeAsync())
+          }
+        }
+      })
+    })
+    return Promise.all(allPromises)
+  })
+  .then(function () {
+    console.log('done.')
+  })
+  .catch(function (err) {
+    console.error('error happened', err)
+    throw err
+  })
