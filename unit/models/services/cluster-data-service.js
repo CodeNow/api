258044cad7c523cbbf3c2f0eaf7a24cc09cd3775
Anton Podviaznikov <podviaznikov@gmail.com>
2017-03-22 20:02:39 -0700

    more tests

--- unit/models/services/cluster-data-service.js
+++ unit/models/services/cluster-data-service.js
@@ -116,10 +116,44 @@ describe('Cluster Data Service Unit Tests', function () {
   })
 
   describe('populateInstancesWithClusterInfo', () => {
+    const instance1Id = '507f1f77bcf86cd799439011'
+    const aig1Id = '107f1f77bcf86cd799439012'
+    let instance1
+    let aig1
+    let clusterConfig1
+    const instance2Id = '007f1f77bcf86cd799439019'
+    const aig2Id = '007f1f77bcf86cd799439019'
+    let instance2
+    let aig2
+    let clusterConfig2
+    let organization
+    let sessionUser
+    let ownerGitHubId
     beforeEach((done) => {
-      sinon.stub(UserService, 'getBpOrgInfoFromGitHubId').returns()
-      sinon.stub(AutoIsolationConfig, 'findAsync').resolves()
-      sinon.stub(InputClusterConfig, 'findAsync').resolves()
+      organization = { id: 11111}
+      sessionUser = {
+        _id: 2222
+      }
+      ownerGitHubId = 9999
+      instance1 = new Instance({ _id: instance1Id, owner: { github: ownerGitHubId } })
+      aig1 = new AutoIsolationConfig({
+        _id: objectId(aig1Id),
+        instance: objectId(instance1Id)
+      })
+      clusterConfig1 = new InputClusterConfig({
+        autoIsolationConfigId: objectId(aig1Id)
+      })
+      instance2 = new Instance({ _id: instance2Id, owner: { github: ownerGitHubId } })
+      aig2 = new AutoIsolationConfig({
+        _id: objectId(aig2Id),
+        instance: objectId(instance2Id)
+      })
+      clusterConfig2 = new InputClusterConfig({
+        autoIsolationConfigId: objectId(aig2Id)
+      })
+      sinon.stub(UserService, 'getBpOrgInfoFromGitHubId').returns(organization)
+      sinon.stub(AutoIsolationConfig, 'findAsync').resolves([aig1, aig2])
+      sinon.stub(InputClusterConfig, 'findAsync').resolves([clusterConfig1, clusterConfig2])
       done()
     })
     afterEach((done) => {
@@ -155,6 +189,49 @@ describe('Cluster Data Service Unit Tests', function () {
         expect(err).to.not.exist()
         expect(result).to.not.exist()
         sinon.assert.notCalled(UserService.getBpOrgInfoFromGitHubId)
+        done()
+      })
+    })
+
+    it('should call all the chain with correct args', (done) => {
+      ClusterDataService.populateInstancesWithClusterInfo([ instance1, instance2], sessionUser)
+      .asCallback((err, result) => {
+        expect(err).to.not.exist()
+        sinon.assert.calledOnce(UserService.getBpOrgInfoFromGitHubId)
+        sinon.assert.calledWithExactly(UserService.getBpOrgInfoFromGitHubId, sessionUser, ownerGitHubId)
+        sinon.assert.calledOnce(AutoIsolationConfig.findAsync)
+        sinon.assert.calledWithExactly(AutoIsolationConfig.findAsync, {
+          ownedByOrg: organization.id,
+          deleted: {
+            $exists: false
+          }
+        })
+        sinon.assert.calledOnce(InputClusterConfig.findAsync)
+        sinon.assert.calledWithExactly(InputClusterConfig.findAsync, {
+          autoIsolationConfigId: { $in: [ objectId(aig1Id), objectId(aig2Id) ] },
+          deleted: {
+            $exists: false
+          }
+        })
+        done()
+      })
+    })
+
+    it('should eturn correct result and modify input', (done) => {
+      const instances = [ instance1, instance2]
+      ClusterDataService.populateInstancesWithClusterInfo(instances, sessionUser)
+      .asCallback((err, result) => {
+        expect(err).to.not.exist()
+        expect(result).to.exist()
+        expect(result.length).to.equal(2)
+        expect(result[0]._doc.inputClusterConfig).to.exist()
+        expect(result[0]._doc.inputClusterConfig._id.toString()).to.equal(clusterConfig1._id.toString())
+        expect(result[1]._doc.inputClusterConfig).to.exist()
+        expect(result[1]._doc.inputClusterConfig._id.toString()).to.equal(clusterConfig2._id.toString())
+        expect(instances[0]._doc.inputClusterConfig).to.exist()
+        expect(instances[0]._doc.inputClusterConfig._id.toString()).to.equal(clusterConfig1._id.toString())
+        expect(instances[1]._doc.inputClusterConfig).to.exist()
+        expect(instances[1]._doc.inputClusterConfig._id.toString()).to.equal(clusterConfig2._id.toString())
         done()
       })
     })
