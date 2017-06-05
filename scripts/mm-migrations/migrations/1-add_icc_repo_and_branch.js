ae3d0eb73258d17ca40d55ec60a6255fc9810cdf
Nathan219 <Nathan219@gmail.com>
2017-06-05 16:53:08 -0700

    Add this template file for icc migration

---
+++ scripts/mm-migrations/migrations/1-add_icc_repo_and_branch.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports.id = "add_icc_repo_and_branch";
+
+module.exports.up = function (done) {
+  // use this.db for MongoDB communication, and this.log() for logging
+  done();
+};
+
+module.exports.down = function (done) {
+  // use this.db for MongoDB communication, and this.log() for logging
+  done();
+};
