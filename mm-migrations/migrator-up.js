aad5963139130fa6bc4474eafcf8f392860e5fb5
Nathan219 <Nathan219@gmail.com>
2017-06-09 14:05:40 -0700

    Fix config support for replicaSets set migration only for the worker

--- mm-migrations/migrator-up.js
+++ mm-migrations/migrator-up.js
@@ -2,16 +2,20 @@
 
 const migrator = require('./migrator').migrator
 const path = require('path')
+const Promise = require('bluebird')
 const logger = require('logger')
 const log = logger.child({
   module: 'Migrator-up'
 })
 const migrationPath = path.resolve('./mm-migrations/migrations')
-migrator.runFromDir(migrationPath, (error, results) => {
-  if (error) {
-    return log.error(error, { results })
-  }
-  log.trace('successfully migrated', results)
-}, (id, result) => {
+
+function traceEnd (id, result) {
   log.trace(`Finished id ${id}, migrating ${result}`)
-})
+}
+
+if (!process.env.IS_QUEUE_WORKER) {
+  Promise.fromCallback(cb => migrator.runFromDir(migrationPath, cb, traceEnd))
+    .tap(result => log.trace('successfully migrated', result))
+    .catch(err => log.error('failed to migrate', err))
+}
+
