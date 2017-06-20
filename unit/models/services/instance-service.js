fce49fb1719e11eeb898c37c9cc16d665296c771
Myztiq <kahn@runnable.com>
2017-06-20 10:21:39 -0700

    Fixed test

--- unit/models/services/instance-service.js
+++ unit/models/services/instance-service.js
@@ -231,7 +231,7 @@ describe('Instances Services Model', function () {
       }
       return InstanceService.fetchInstanceByContainerIdAndEnsureAccess(testDockerContainer, mockSessionUser)
         .then((isAllowed) => {
-          expect(isAllowed).to.equal(true)
+          expect(isAllowed).to.equal(mockInstance)
         })
     })
   })
